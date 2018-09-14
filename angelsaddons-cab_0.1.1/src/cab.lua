f142db557dce7fba0a1dbcfa44dba443bffb36d0
LovelySanta <laurens.scheldeman@gmail.com>
2018-09-14 12:56:07 +0200

    Added undeploy gun behaviour

--- angelsaddons-cab_0.1.1/src/cab.lua
+++ angelsaddons-cab_0.1.1/src/cab.lua
@@ -26,60 +26,17 @@ return {
     -- check if this vehicle is already deployed
     local identifier = string.format(global.vehicleData.positionIdentifier, surfaceIndex, position.y, position.x)
     if not global.vehicleData.deployedCabs then global.vehicleData.deployedCabs = {} end
-    if not global.vehicleData.deployedCabs[identifier] then -- not deployed yet
-
-      -- check if the vehicle is moving
-      if deployedCab["angels-cab"].speed ~= 0 then
-        -- inform player(s)
-        for _,entity in pairs{
-          deployedCab["angels-cab"].get_driver(),
-          deployedCab["angels-cab"].get_passenger(),
-        } do
-          if entity and entity.valid then
-            if entity.is_player() then
-              entity.print{"messages.angels-cab-deploy-driveSpeed"}
-            elseif entity.type == "player" and entity.player and entity.player.valid then
-              entity.player.print{"messages.angels-cab-deploy-driveSpeed"}
-            end
-          end
-        end
-
-        -- give unused charge back
-        deployedCab["angels-cab"].insert{
-          name = "angels-cab-deploy-charge",
-          count = 1,
-        }
-        return false
-      end
-
-      -- deploy the vehicle
-      local force = deployedCab["angels-cab"].force
-      for _, entityName in pairs{
-        "angels-cab-energy-interface",
-        "angels-cab-electric-pole",
-      } do
-        deployedCab[entityName] = surface.create_entity{
-          name = entityName,
-          position = position,
-          force = force,
-        }
-      end
-      global.vehicleData.deployedCabs[identifier] = deployedCab
-      return true
-
-    else -- already deployed
+    if global.vehicleData.deployedCabs[identifier] then
 
       -- inform player(s)
       for _,entity in pairs{
         deployedCab["angels-cab"].get_driver(),
         deployedCab["angels-cab"].get_passenger(),
       } do
-        if entity and entity.valid then
-          if entity.is_player() then
-            entity.print{"messages.angels-cab-deployspeed"}
-          elseif entity.type == "player" and entity.player and entity.player.valid then
-            entity.player.print{"messages.angels-cab-deploy-alreadyDeployed"}
-          end
+        if entity.is_player() then -- player in godmode
+          entity.print{"angels-cab-messages.deploy-alreadyDeployed"}
+        elseif entity.type == "player" and entity.player then -- player in character
+          entity.player.print{"angels-cab-messages.deploy-alreadyDeployed"}
         end
       end
 
@@ -90,6 +47,43 @@ return {
       }
       return false
     end
+
+    -- check if the vehicle is moving
+    if deployedCab["angels-cab"].speed ~= 0 then
+      -- inform player(s)
+      for _,entity in pairs{
+        deployedCab["angels-cab"].get_driver(),
+        deployedCab["angels-cab"].get_passenger(),
+      } do
+        if entity.is_player() then -- player in godmode
+          entity.print{"angels-cab-messages.deploy-driveSpeed"}
+        elseif entity.type == "player" and entity.player then -- player in character
+          entity.player.print{"angels-cab-messages.deploy-driveSpeed"}
+        end
+      end
+
+      -- give unused charge back
+      deployedCab["angels-cab"].insert{
+        name = "angels-cab-deploy-charge",
+        count = 1,
+      }
+      return false
+    end
+
+    -- deploy the vehicle
+    local force = deployedCab["angels-cab"].force
+    for _, entityName in pairs{
+      "angels-cab-energy-interface",
+      "angels-cab-electric-pole",
+    } do
+      deployedCab[entityName] = surface.create_entity{
+        name = entityName,
+        position = position,
+        force = force,
+      }
+    end
+    global.vehicleData.deployedCabs[identifier] = deployedCab
+    return true
   end,
 
   undeploy = function(entity)
@@ -101,8 +95,55 @@ return {
       y = math.floor(entity.position.y) + .5,
     }
     entity.destroy()
-    
-    return false
+
+    -- find vehicle that wants to get undeployed
+    local deployedCab = surface.find_entities_filtered{
+      name = "angels-cab",
+      type = "car",
+      area = {{x = position.x - 1, y = position.y - 1},{x = position.x + 1, y = position.y + 1}},
+      limit = 1,
+    }[1]
+    if (not deployedCab) or (not deployedCab.valid) then
+      game.print("Unknown error: Could not not find vehicle.")
+      return false
+    end
+
+    -- check if this vehicle is already deployed
+    local identifier = string.format(global.vehicleData.positionIdentifier, surfaceIndex, position.y, position.x)
+    if not global.vehicleData.deployedCabs then global.vehicleData.deployedCabs = {} end
+    if not global.vehicleData.deployedCabs[identifier] then -- not deployed yet
+      -- inform player(s)
+      for _,entity in pairs{
+        deployedCab.get_driver(),
+        deployedCab.get_passenger(),
+      } do
+        if entity.is_player() then -- player in godmode
+          entity.print{"angels-cab-messages.undeploy-notDeployed"}
+        elseif entity.type == "player" and entity.player then -- player in character
+          entity.player.print{"angels-cab-messages.undeploy-notDeployed"}
+        end
+      end
+
+      -- give unused charge back
+      deployedCab.insert{
+        name = "angels-cab-undeploy-charge",
+        count = 1,
+      }
+      return false
+    end
+
+    -- undeploy the vehicle
+    deployedCab = global.vehicleData.deployedCabs[identifier]
+    for _, entityName in pairs{
+      "angels-cab-energy-interface",
+      "angels-cab-electric-pole",
+    } do
+      if deployedCab[entityName] and deployedCab[entityName].valid then
+        deployedCab[entityName].destroy()
+      end
+    end
+    global.vehicleData.deployedCabs[identifier] = nil
+    return true
   end,
 
 }
