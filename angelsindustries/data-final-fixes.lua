8720a24af88a9e85b9f803bc332e4ecccd95c60c
tim vroom <tim@timvroom.nl>
2020-01-26 17:38:38 +0100

    Set proper icon size for loaders

--- angelsindustries/data-final-fixes.lua
+++ angelsindustries/data-final-fixes.lua
@@ -182,7 +182,9 @@ OV.execute()
 
 if not loaders_graphics then
 	data.raw["item"]["loader"].icon = "__angelsindustries__/graphics/icons/loader-yellow-ico.png"
+  data.raw["item"]["loader"].icon_size = 32
 	data.raw["loader"]["loader"].icon = "__angelsindustries__/graphics/icons/loader-yellow-ico.png"
+  data.raw["loader"]["loader"].icon_size = 32
 	data.raw["loader"]["loader"].structure =
 	{
 		direction_in =
@@ -209,7 +211,9 @@ if not loaders_graphics then
 	}
 
 	data.raw["item"]["fast-loader"].icon = "__angelsindustries__/graphics/icons/loader-red-ico.png"
+  data.raw["item"]["fast-loader"].icon_size = 32
 	data.raw["loader"]["fast-loader"].icon = "__angelsindustries__/graphics/icons/loader-red-ico.png"
+  data.raw["loader"]["fast-loader"].icon_size = 32
 	data.raw["loader"]["fast-loader"].structure =
 	{
 		direction_in =
@@ -236,7 +240,9 @@ if not loaders_graphics then
 	}
 
 	data.raw["item"]["express-loader"].icon = "__angelsindustries__/graphics/icons/loader-blue-ico.png"
+  data.raw["item"]["express-loader"].icon_size = 32
 	data.raw["loader"]["express-loader"].icon = "__angelsindustries__/graphics/icons/loader-blue-ico.png"
+  data.raw["loader"]["express-loader"].icon_size = 32
 	data.raw["loader"]["express-loader"].structure =
 	{
 		direction_in =
@@ -264,7 +270,9 @@ if not loaders_graphics then
 
 	if data.raw["item"]["faster-loader"] then
 		data.raw["item"]["faster-loader"].icon = "__angelsindustries__/graphics/icons/loader-green-ico.png"
+    data.raw["item"]["faster-loader"].icon_size = 32
 		data.raw["loader"]["faster-loader"].icon = "__angelsindustries__/graphics/icons/loader-green-ico.png"
+    data.raw["loader"]["faster-loader"].icon_size = 32
 		data.raw["loader"]["faster-loader"].structure =
 		{
 			direction_in =
@@ -292,7 +300,9 @@ if not loaders_graphics then
 	end
 	if data.raw["item"]["extremely-fast-loader"] then
 		data.raw["item"]["extremely-fast-loader"].icon = "__angelsindustries__/graphics/icons/loader-purple-ico.png"
+    data.raw["item"]["extremely-fast-loader"].icon_size = 32
 		data.raw["loader"]["extremely-fast-loader"].icon = "__angelsindustries__/graphics/icons/loader-purple-ico.png"
+    data.raw["loader"]["extremely-fast-loader"].icon_size = 32
 		data.raw["loader"]["extremely-fast-loader"].structure =
 		{
 			direction_in =
