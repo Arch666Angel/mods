4dfc5f26216ce8fe070cc191d307ebbdbe401943
tim vroom <tim@timvroom.nl>
2020-01-31 12:18:25 +0100

    Updated to 0.2.1
    - Fixed startup sounds
    - Added most of the translations (might have missed a few)

--- angelsaddons-liquidrobot/data-updates.lua
+++ angelsaddons-liquidrobot/data-updates.lua
@@ -1,13 +1,16 @@
+if mods["debugadapter"] then
+  require("__debugadapter__/debugadapter.lua")
+end
+
 -- if angelsmods.industries then
-	-- local OV = angelsmods.functions.OV
-	
-	-- if angelsmods.industries.components then
-		-- require("prototypes.recipes.cab-updates")
-	-- end
-	-- data.raw["item-subgroup"]["angels-cab"].group = "angels-logistics"
-	-- OV.remove_prereq("angels-cab", "automobilism")
-	-- OV.add_prereq("angels-cab", "angels-crawler")
+-- local OV = angelsmods.functions.OV
+
+-- if angelsmods.industries.components then
+-- require("prototypes.recipes.cab-updates")
+-- end
+-- data.raw["item-subgroup"]["angels-cab"].group = "angels-logistics"
+-- OV.remove_prereq("angels-cab", "automobilism")
+-- OV.add_prereq("angels-cab", "angels-crawler")
 -- end
 
 require("prototypes.entities.liquid-robot-generator")
-
