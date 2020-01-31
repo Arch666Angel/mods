e42b0e25d892882a1ea4a42d9aef0cee86706d44
LovelySanta <laurens.scheldeman@gmail.com>
2020-01-31 23:13:20 +0100

    glass

--- angelssmelting/prototypes/items/angels-glass.lua
+++ angelssmelting/prototypes/items/angels-glass.lua
@@ -1,5 +1,8 @@
 data:extend(
 {
+  -- ORE
+  -- SMELTING INTERMEDIATE
+  -- SMELTING RESULTS
   {
     type = "item",
     name = "solid-glass-mixture",
@@ -9,31 +12,14 @@ data:extend(
     order = "a",
     stack_size = 200
   },
-  {
-    type = "item",
-    name = "angels-plate-glass",
-    icon = "__angelssmelting__/graphics/icons/plate-glass.png",
-    icon_size = 32,
-    subgroup = "angels-glass",
-    order = "b",
-    stack_size = 200
-  },
-  {
-    type = "item",
-    name = "angels-coil-glass-fiber",
-    icon = "__angelssmelting__/graphics/icons/wire-coil-glass.png",
-    icon_size = 32,
-    subgroup = "angels-glass",
-    order = "c",
-    stack_size = 200
-  },
+  -- CASTING INTERMEDIATE
   {
     type = "fluid",
     name = "liquid-molten-glass",
     icon = "__angelssmelting__/graphics/icons/molten-glass.png",
     icon_size = 32,
     subgroup = "angels-glass-casting",
-    order = "d",
+    order = "b",
     default_temperature = 100,
     heat_capacity = "0KJ",
     base_color = {r = 130/255, g = 127/255, b = 127/255},
@@ -43,5 +29,25 @@ data:extend(
     flow_to_energy_ratio = 0.59,
     auto_barrel = false
   },
+  -- CASTING RESULT
+  {
+    type = "item",
+    name = "angels-plate-glass",
+    icon = "__angelssmelting__/graphics/icons/plate-glass.png",
+    icon_size = 32,
+    subgroup = "angels-glass-casting",
+    order = "c",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "angels-coil-glass-fiber",
+    icon = "__angelssmelting__/graphics/icons/wire-coil-glass.png",
+    icon_size = 32,
+    subgroup = "angels-glass-casting",
+    order = "d",
+    stack_size = 200
+  },
+  -- order e is reserved for glass fiber board
 }
 )
--- angelssmelting/prototypes/recipes/smelting-glass.lua
+++ angelssmelting/prototypes/recipes/smelting-glass.lua
@@ -2,304 +2,363 @@ local intermediatemulti = angelsmods.marathon.intermediatemulti
 
 data:extend(
 {
---GLAS
---INTERMEDIATE
-    {
+  --GLAS
+  -- SMELTING INTERMEDIATE
+  -- SMELTING RESULTS
+  {
     type = "recipe",
     name = "glass-mixture-1",
     category = "powder-mixing",
-	subgroup = "angels-glass",
+    subgroup = "angels-glass",
     energy_required = 4,
-	enabled = "false",
-    ingredients ={
+    enabled = "false",
+    ingredients =
+    {
       {type="item", name="quartz", amount=1},
-	},
-    results=
+    },
+    results =
     {
       {type="item", name="solid-glass-mixture", amount=1},
     },
-	icon_size = 32,
-    order = "a",
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/glass-mixture.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_1.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      },
     },
-    {
+    icon_size = 32,
+    order = "a[solid-glass-mixture]-a",
+  },
+  {
     type = "recipe",
     name = "glass-mixture-2",
     category = "powder-mixing",
-	subgroup = "angels-glass",
+    subgroup = "angels-glass",
     energy_required = 4,
-	enabled = "false",
-    ingredients ={
+    enabled = "false",
+    ingredients =
+    {
       {type="item", name="quartz", amount=1},
       {type="item", name="solid-lime", amount=1},
-	},
-    results=
+    },
+    results =
     {
       {type="item", name="solid-glass-mixture", amount=2},
     },
-	icon_size = 32,
-    order = "b",
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/glass-mixture.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_2.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      },
     },
-    {
+    icon_size = 32,
+    order = "a[solid-glass-mixture]-b",
+  },
+  {
     type = "recipe",
     name = "glass-mixture-3",
     category = "powder-mixing",
-	subgroup = "angels-glass",
+    subgroup = "angels-glass",
     energy_required = 4,
-	enabled = "false",
-    ingredients ={
+    enabled = "false",
+    ingredients =
+    {
       {type="item", name="quartz", amount=1},
       {type="item", name="solid-aluminium-oxide", amount=1},
       {type="item", name="solid-lime", amount=1},
-	},
-    results=
+    },
+    results =
     {
       {type="item", name="solid-glass-mixture", amount=3},
     },
-	icon_size = 32,
-    order = "c",
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/glass-mixture.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_3.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      },
     },
-    {
+    icon_size = 32,
+    order = "a[solid-glass-mixture]-c",
+  },
+  {
     type = "recipe",
     name = "glass-mixture-4",
     category = "powder-mixing",
-	subgroup = "angels-glass",
+    subgroup = "angels-glass",
     energy_required = 4,
-	enabled = "false",
-    ingredients ={
+    enabled = "false",
+    ingredients =
+    {
       {type="item", name="solid-sand", amount=1},
       {type="item", name="solid-sodium-carbonate", amount=1},
       {type="item", name="solid-lime", amount=1},
       {type="item", name="solid-sodium-sulfate", amount=1},
-	},
-    results=
+    },
+    results =
     {
       {type="item", name="solid-glass-mixture", amount=4},
     },
-	icon_size = 32,
-    order = "d",
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/glass-mixture.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_4.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      },
     },
---SMELTING
-    {
+    icon_size = 32,
+    order = "a[solid-glass-mixture]-d",
+  },
+  -- CASTING INTERMEDIATE
+  {
     type = "recipe",
     name = "molten-glass-smelting",
     category = "induction-smelting",
-	subgroup = "angels-glass-casting",
+    subgroup = "angels-glass-casting",
     energy_required = 4,
-	enabled = "false",
-    ingredients ={
+    enabled = "false",
+    ingredients =
+    {
       {type="item", name="solid-glass-mixture", amount=12},
-	},
-    results=
+    },
+    results =
     {
       {type="fluid", name="liquid-molten-glass", amount=120},
     },
-	icon_size = 32,
-    order = "e",
-    },
---CASTING
-    {
+    icon_size = 32,
+    order = "b[liquid-molten-glass]",
+  },
+  -- CASTING RESULT
+  {
     type = "recipe",
     name = "angels-plate-glass-1",
     category = "casting",
-	subgroup = "angels-glass-casting",
-	normal =
+    subgroup = "angels-glass-casting",
+    normal =
     {
-		enabled = "false",
-		energy_required = 4,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=40},
-		},
-		results=
-		{
-		  {type="item", name="angels-plate-glass", amount=3},
-		},
-	},
-    expensive =
-	{
-		enabled = "false",
-		energy_required = 4,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=50 * intermediatemulti},
-		},
-		results=
-		{
-		  {type="item", name="angels-plate-glass", amount=3},
-		},
-	},
-	icons = {
-		{
-			icon = "__angelssmelting__/graphics/icons/plate-glass.png",
-		},
-		{
-			icon = "__angelspetrochem__/graphics/icons/num_1.png",
-			tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
-			scale = 0.32,
-			shift = {-12, -12},
-		}
-	},	
-	icon_size = 32,
-    order = "f",
+      enabled = "false",
+      energy_required = 4,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=40},
+      },
+      results =
+      {
+        {type="item", name="angels-plate-glass", amount=3},
+      },
     },
+    expensive =
     {
+      enabled = "false",
+      energy_required = 4,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=50 * intermediatemulti},
+      },
+      results =
+      {
+        {type="item", name="angels-plate-glass", amount=3},
+      },
+    },
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/plate-glass.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_1.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      }
+    },
+    icon_size = 32,
+    order = "c[angels-plate-glass]-a",
+  },
+  {
     type = "recipe",
     name = "angels-plate-glass-2",
     category = "casting",
-	subgroup = "angels-glass-casting",
-	normal =
+    subgroup = "angels-glass-casting",
+    normal =
     {
-		enabled = "false",
-		energy_required = 3,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=40},
-		  {type="fluid", name="liquid-molten-lead", amount=20},
-		},
-		results=
-		{
-		  {type="item", name="angels-plate-glass", amount=4},
-		  {type="item", name="solid-lead-oxide", amount=2},
-		},
-		main_product= "angels-plate-glass",
-	},
-    expensive =
-	{
-		enabled = "false",
-		energy_required = 3,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=50 * intermediatemulti},
-		  {type="fluid", name="liquid-molten-lead", amount=20},
-		},
-		results=
-		{
-		  {type="item", name="angels-plate-glass", amount=4},
-		  {type="item", name="solid-lead-oxide", amount=2},
-		},
-		main_product= "angels-plate-glass",
-	},
-	icons = {
-		{
-			icon = "__angelssmelting__/graphics/icons/plate-glass.png",
-		},
-		{
-			icon = "__angelspetrochem__/graphics/icons/num_2.png",
-			tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
-			scale = 0.32,
-			shift = {-12, -12},
-		}
-	},	
-	icon_size = 32,
-    order = "g",
+      enabled = "false",
+      energy_required = 3,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=40},
+        {type="fluid", name="liquid-molten-lead", amount=20},
+      },
+      results =
+      {
+        {type="item", name="angels-plate-glass", amount=4},
+        {type="item", name="solid-lead-oxide", amount=2},
+      },
+      main_product= "angels-plate-glass",
     },
+    expensive =
     {
+      enabled = "false",
+      energy_required = 3,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=50 * intermediatemulti},
+        {type="fluid", name="liquid-molten-lead", amount=20},
+      },
+      results =
+      {
+        {type="item", name="angels-plate-glass", amount=4},
+        {type="item", name="solid-lead-oxide", amount=2},
+      },
+      main_product= "angels-plate-glass",
+    },
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/plate-glass.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_2.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      }
+    },
+    icon_size = 32,
+    order = "c[angels-plate-glass]-b",
+  },
+  {
     type = "recipe",
     name = "angels-plate-glass-3",
     category = "casting",
-	subgroup = "angels-glass-casting",
-	normal =
+    subgroup = "angels-glass-casting",
+    normal =
     {
-		enabled = "false",
-		energy_required = 2,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=40},
-		  {type="fluid", name="liquid-molten-tin", amount=20},
-		  {type="fluid", name="gas-nitrogen", amount=20},
-		},
-		results=
-		{
-		  {type="item", name="angels-plate-glass", amount=5},
-		  {type="item", name="ingot-tin", amount=2},
-		},
-		main_product = "angels-plate-glass",
-	},
-    expensive =
-	{
-		enabled = "false",
-		energy_required = 2,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=50 * intermediatemulti},
-		  {type="fluid", name="liquid-molten-tin", amount=20},
-		  {type="fluid", name="gas-nitrogen", amount=20},
-		},
-		results=
-		{
-		  {type="item", name="angels-plate-glass", amount=5},
-		  {type="item", name="ingot-tin", amount=2},
-		},
-		main_product = "angels-plate-glass",
-	},
-	icons = {
-		{
-			icon = "__angelssmelting__/graphics/icons/plate-glass.png",
-		},
-		{
-			icon = "__angelspetrochem__/graphics/icons/num_3.png",
-			tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
-			scale = 0.32,
-			shift = {-12, -12},
-		}
-	},	
-	icon_size = 32,
-    order = "h",
+      enabled = "false",
+      energy_required = 2,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=40},
+        {type="fluid", name="liquid-molten-tin", amount=20},
+        {type="fluid", name="gas-nitrogen", amount=20},
+      },
+      results =
+      {
+        {type="item", name="angels-plate-glass", amount=5},
+        {type="item", name="ingot-tin", amount=2},
+      },
+      main_product = "angels-plate-glass",
     },
+    expensive =
     {
+      enabled = "false",
+      energy_required = 2,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=50 * intermediatemulti},
+        {type="fluid", name="liquid-molten-tin", amount=20},
+        {type="fluid", name="gas-nitrogen", amount=20},
+      },
+      results =
+      {
+        {type="item", name="angels-plate-glass", amount=5},
+        {type="item", name="ingot-tin", amount=2},
+      },
+      main_product = "angels-plate-glass",
+    },
+    icons = {
+      {
+        icon = "__angelssmelting__/graphics/icons/plate-glass.png",
+      },
+      {
+        icon = "__angelspetrochem__/graphics/icons/num_3.png",
+        tint = {r = 1, g = 0.8, b = 0.0, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12},
+      }
+    },
+    icon_size = 32,
+    order = "c[angels-plate-glass]-c",
+  },
+  {
     type = "recipe",
     name = "angels-coil-glass-fiber",
     category = "strand-casting",
-	subgroup = "angels-glass-casting",
-	normal =
+    subgroup = "angels-glass-casting",
+    normal =
     {
-		enabled = "false",
-		energy_required = 4,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=80},
-		},
-		results=
-		{
-		  {type="item", name="angels-coil-glass-fiber", amount=4},
-		},
-	},
-    expensive =
-	{
-		enabled = "false",
-		energy_required = 4,
-		ingredients ={
-		  {type="fluid", name="liquid-molten-glass", amount=100 * intermediatemulti},
-		},
-		results=
-		{
-		  {type="item", name="angels-coil-glass-fiber", amount=4},
-		},
-	},
-	icon_size = 32,
-    order = "i",
+      enabled = "false",
+      energy_required = 4,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=80},
+      },
+      results =
+      {
+        {type="item", name="angels-coil-glass-fiber", amount=4},
+      },
     },
---CRAFTING
+    expensive =
     {
+      enabled = "false",
+      energy_required = 4,
+      ingredients =
+      {
+        {type="fluid", name="liquid-molten-glass", amount=100 * intermediatemulti},
+      },
+      results =
+      {
+        {type="item", name="angels-coil-glass-fiber", amount=4},
+      },
+    },
+    icon_size = 32,
+    order = "d[angels-coil-glass-fiber]",
+  },
+  --CRAFTING
+  {
     type = "recipe",
     name = "angels-glass-fiber-board",
     category = "advanced-crafting",
-	subgroup = "angels-glass-casting",
+    subgroup = "angels-glass-casting",
     energy_required = 1,
-	enabled = "false",
-    ingredients ={
-		{type="item", name="angels-coil-glass-fiber", amount=1},
-		{type="fluid", name="liquid-resin", amount=40},
-	},
-    results=
+    enabled = "false",
+    ingredients =
     {
-		{type="item", name="angels-void", amount=1},
+      {type="item", name="angels-coil-glass-fiber", amount=1},
+      {type="fluid", name="liquid-resin", amount=40},
     },
-	icons = {
-		{
-			icon = "__angelsrefining__/graphics/icons/void.png",
-		},
-		{
-			icon = "__angelssmelting__/graphics/icons/wire-coil-glass.png",
-			scale = 0.4375,
-			shift = { -10, -10},
-		}
-	},	
-	icon_size = 32,
-    order = "j",
+    results =
+    {
+      {type="item", name="angels-void", amount=1},
     },
+    icons = {
+      {
+        icon = "__angelsrefining__/graphics/icons/void.png",
+      },
+      {
+        icon = "__angelssmelting__/graphics/icons/wire-coil-glass.png",
+        scale = 0.4375,
+        shift = { -10, -10},
+      }
+    },
+    icon_size = 32,
+    order = "e[angels-glass-fiber-board]",
+  },
 }
 )
