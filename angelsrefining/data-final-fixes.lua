822410e4e883090d41c41d528c0a0c8866bfa753
tim vroom <tim@timvroom.nl>
2020-02-01 12:11:41 +0100

    Added reorganisation of barrels/fill/empty. in data-final-fixes
    Also some re-alignments

--- angelsrefining/data-final-fixes.lua
+++ angelsrefining/data-final-fixes.lua
@@ -1,3 +1,7 @@
 -- if angelsmods.refining then
 	-- require("prototypes.generation.angels-override")
 -- end
+
+if (angelsmods.industries and angelsmods.industries.overhaul) or mods['bobplates'] then
+    angelsmods.functions.create_barreling_fluid_subgroup()
+end
