fd41eb89b01ad1f336317b99008da9b57ac838ec
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-02-03 20:51:43 +1100

    enforce auto-barrel rules for all fluids
    forced check of auto-barrelling before modifying barrels to cylinder/canister and fluid bots

--- angelsrefining/data-updates.lua
+++ angelsrefining/data-updates.lua
@@ -13,11 +13,13 @@ if mods['bobplates'] and data.raw["item-subgroup"]["bob-gas-bottle"] then
 	data.raw.technology["gas-canisters"].enabled = true
 	--local fluid_n=data.raw["fluid"]
 	for _, fluid_n in pairs(data.raw.fluid) do
-		if string.sub(fluid_n.name,1,3)=="gas" then
-			barrel_overrides(fluid_n.name,"gas")
-		elseif not string.find(fluid_n.name,"acid")==nil or string.sub(fluid_n.name,-4)=="acid" then
-			local acid=string.find(fluid_n.name,"acid")
-			barrel_overrides(fluid_n.name,"acid")
+		if not fluid_n.auto_barrel==false then
+			if string.sub(fluid_n.name,1,3)=="gas" then
+				barrel_overrides(fluid_n.name,"gas")
+			elseif not string.find(fluid_n.name,"acid")==nil or string.sub(fluid_n.name,-4)=="acid" then
+				local acid=string.find(fluid_n.name,"acid")
+				barrel_overrides(fluid_n.name,"acid")
+			end
 		end
 	end
 	--insert custom barrel replacements
