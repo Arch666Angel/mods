e3d67ef716e58df76fc58e60a0154376863783eb
tim vroom <tim@timvroom.nl>
2020-02-04 00:25:03 +0100

    incomplete - Updates for tier trains
    include numbers
    configurable tiers
    todo: recipes and tech and speed multiply/calcs

--- angelsaddons-smeltingtrain/data.lua
+++ angelsaddons-smeltingtrain/data.lua
@@ -1,6 +1,11 @@
-if not angelsmods then angelsmods = {} end
-if not angelsmods.addons then angelsmods.addons = {} end
-if not angelsmods.addons.smeltingtrain then angelsmods.addons.smeltingtrain = {} end
+local __DebugAdapter = mods["debugadapter"] and require("__debugadapter__/debugadapter.lua")
+
+angelsmods = angelsmods or {}
+angelsmods.addons = angelsmods.addons or {}
+angelsmods.addons.smeltingtrain = angelsmods.addons.smeltingtrain or {}
+angelsmods.addons.smeltingtrain.enable_tiers = settings.startup["angels-smeltingtrain-tier-enable"].value
+angelsmods.addons.smeltingtrain.tier_amount = settings.startup["angels-smeltingtrain-tier-amount"].value
+angelsmods.addons.smeltingtrain.number_tint = {r = 1, g = 0.38, b = 0, a = 1}
 
 require("prototypes.smelting-category")
 
@@ -9,4 +14,3 @@ require("prototypes.entities.smelting-train")
 require("prototypes.recipes.smelting-recipe")
 
 require("prototypes.technology.smelting-technology")
-
