437523481335fb9d9c3afa3e07707cce3402c1f2
LovelySanta <laurens.scheldeman@gmail.com>
2020-02-05 19:34:38 +0100

    bugfix

--- angelsexploration/prototypes/exploration-override.lua
+++ angelsexploration/prototypes/exploration-override.lua
@@ -3,7 +3,7 @@ if not (angelsmods.industries and angelsmods.industries.overhaul) then
   data.raw["item-with-entity-data"]["car"].order = "a[car]"
 end
 data.raw["item-with-entity-data"]["tank"].subgroup = "angels-exploration-vehicles"
-data.raw["item-with-entity-data"]["tank"].subgroup = "b[tank]-a[vanilla]"
+data.raw["item-with-entity-data"]["tank"].order = "b[tank]-a[vanilla]"
 
 table.insert(data.raw.technology["military-3"].prerequisites, "angels-cannon-turret")
 table.insert(data.raw.technology["military-3"].effects, { type = "unlock-recipe", recipe = "cannon-turret-shell-2" })
