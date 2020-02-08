c98f6404c6ba78d26f026abd652016f9df8783af
LovelySanta <laurens.scheldeman@gmail.com>
2020-02-08 22:20:46 +0100

    added warning sign next to settings

--- angelsindustries/settings.lua
+++ angelsindustries/settings.lua
@@ -11,14 +11,14 @@ data:extend(
   },
   {
     type = "bool-setting",
-    name = "angels-enable-tech",
+    name = "angels-enable-components",
     setting_type = "startup",
     default_value = false,
     order = "b",
   },
   {
     type = "bool-setting",
-    name = "angels-enable-components",
+    name = "angels-enable-tech",
     setting_type = "startup",
     default_value = false,
     order = "c",
