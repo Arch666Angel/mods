a2c2b4387f30aaad962198624fec510927af2961
tim vroom <tim@timvroom.nl>
2020-02-15 14:44:35 +0100

    Removed debugadapter again...

--- angelsbioprocessing/data-updates.lua
+++ angelsbioprocessing/data-updates.lua
@@ -1,6 +1,3 @@
-if mods["debugadapter"] then
-	require("__debugadapter__/debugadapter.lua")
-end
 if angelsmods.bioprocessing then
 	require("prototypes.bio-processing-override")
 	require("prototypes.bio-processing-generate")
