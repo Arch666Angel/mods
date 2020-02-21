0a16246069fe6cbc6616f9eee5495b30d49be384
LovelySanta <laurens.scheldeman@gmail.com>
2020-02-21 18:58:03 +0100

    rubyte gives nickel again

--- angelsrefining/prototypes/override/refining-override-sorting.lua
+++ angelsrefining/prototypes/override/refining-override-sorting.lua
@@ -258,8 +258,7 @@ OV.patch_recipes(merge_table_of_tables{
   create_sorting_recipes("rubyte", "angelsore5%s", {
     ["!!"           ] = (not special_vanilla) and {false, false, false, true},
     ["lead-ore"     ] = (not special_vanilla) and {2, 2, 3, 3},
-    ["manganese-ore"] = (not special_vanilla) and {1, 1, 1, 2},
-    ["nickel-ore"   ] = (not special_vanilla) and (not ore_exists("manganese-ore")) and {1, 1, 1, 2},
+    ["nickel-ore"   ] = (not special_vanilla) and {1, 1, 1, 2},
     ["bauxite-ore"  ] = (not special_vanilla) and {0, 1, 1, 1},
     ["gold-ore"     ] = (not special_vanilla) and {0, 1, 1, 1},
     ["rutile-ore"   ] = (not special_vanilla) and {0, 0, 1, 1},
