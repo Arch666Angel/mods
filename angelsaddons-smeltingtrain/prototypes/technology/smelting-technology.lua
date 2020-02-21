2311dc20d1fd6318411b5260b068f2352d62fbc9
Tim Vroom <tim@timvroom.nl>
2020-02-21 10:42:05 +0100

    AngelAddons Train tiering (#34)
    * incomplete - Updates for tier trains
    
    include numbers
    configurable tiers
    todo: recipes and tech and speed multiply/calcs
    
    * Added proper recipes and all the items...
    
    * Added tiers of trains and slightly easier editable options

--- angelsaddons-smeltingtrain/prototypes/technology/smelting-technology.lua
+++ angelsaddons-smeltingtrain/prototypes/technology/smelting-technology.lua
@@ -1,41 +1,38 @@
+local function get_unlocks(tech, base_effects)
+  if tech_unlocks[tech] then
+    for name, _ in pairs(tech_unlocks[tech]) do
+      table.insert(
+        base_effects,
+        {
+          type = "unlock-recipe",
+          recipe = name
+        }
+      )
+    end
+  end
+
+  return base_effects
+end
 data:extend(
-{
+  {
     {
-    type = "technology",
-    name = "angels-smelting-train",
-    icon = "__angelsaddons-smeltingtrain__/graphics/technology/smelting-loco-1-tech.png",
-	icon_size = 128,
-	prerequisites =
-    {
-	"railway",
-    },
-    effects =
-    {
-	  {
-        type = "unlock-recipe",
-        recipe = "smelting-locomotive-1"
+      type = "technology",
+      name = "angels-smelting-train",
+      icon = "__angelsaddons-smeltingtrain__/graphics/technology/smelting-loco-1-tech.png",
+      icon_size = 128,
+      prerequisites = {
+        "railway"
       },
-	  {
-        type = "unlock-recipe",
-        recipe = "smelting-locomotive-tender"
+      effects = get_unlocks("angels-smelting-train", {}),
+      unit = {
+        count = 60,
+        ingredients = {
+          {"automation-science-pack", 1},
+          {"logistic-science-pack", 1}
+        },
+        time = 15
       },
-	  {
-        type = "unlock-recipe",
-        recipe = "smelting-wagon-1"
-      },
-    },
-    unit =
-    {
-      count = 60,
-      ingredients = {
-	  {"automation-science-pack", 1},
-	  {"logistic-science-pack", 1},
-	  },
-      time = 15
-    },
-    order = "c-a"
-    },
-}
+      order = "c-a"
+    }
+  }
 )
-
-
