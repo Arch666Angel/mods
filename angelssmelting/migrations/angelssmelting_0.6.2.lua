70290348fbae60a227abe7ed0916bb5def68500e
Evan <evandy@gmail.com>
2020-02-27 20:07:50 -0600

    Fix upgrade script

--- angelssmelting/migrations/angelssmelting_0.6.2.lua
+++ angelssmelting/migrations/angelssmelting_0.6.2.lua
@@ -1,6 +1,19 @@
+local techs = {"angels-platinum-smelting-1",
+      	       "angels-platinum-smelting-2",
+	       "angels-platinum-smelting-3"}
+
 game.reload_script()
 
 for index, force in pairs(game.forces) do
-	force.reset_recipes()
-	force.reset_technology_effects()
+    force.reset_recipes()
+    force.reset_technology_effects()
+
+    for _, tech in ipairs(techs) do
+    	if force.technologies[tech] then
+	   force.technologies[tech].enabled = true
+	end
+    end
 end
+
+
+
