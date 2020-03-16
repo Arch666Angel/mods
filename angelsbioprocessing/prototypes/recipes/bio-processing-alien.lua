5274998e6831bfe1b8c348f3911a0c5e5584ad43
LovelySanta <laurens.scheldeman@gmail.com>
2020-03-16 01:56:10 +0100

    fix duplicate recipe

--- angelsbioprocessing/prototypes/recipes/bio-processing-alien.lua
+++ angelsbioprocessing/prototypes/recipes/bio-processing-alien.lua
@@ -231,32 +231,6 @@ data:extend(
       results = {
         {type = "fluid", name = "crystal-slurry", amount = 1}
       },
-      icons = {
-        {
-          icon = "__angelsrefining__/graphics/icons/liquify-blank.png"
-        },
-        {
-          icon = "__angelsbioprocessing__/graphics/icons/crystal-powder.png",
-          scale = 0.4375,
-          shift = {-10, -10}
-        }
-      },
-      icon_size = 32,
-      order = "d[slurry]"
-    },
-    {
-      type = "recipe",
-      name = "crystal-powder-slurry",
-      category = "crafting-with-fluid",
-      subgroup = "bio-biter-processing-cutting",
-      enabled = "false",
-      energy_required = 2,
-      ingredients = {
-        {type = "item", name = "crystal-powder", amount = 1}
-      },
-      results = {
-        {type = "fluid", name = "crystal-slurry", amount = 1}
-      },
       main_product = "crystal-slurry",
       always_show_products = "true",
       icons = angelsmods.functions.create_liquid_recipe_icon(nil, { {184,231,043}, {080,172,033}, {055,126,017} }, { "crystal-powder" }),
