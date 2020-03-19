ba3088cdd125db4be4613c91279d0b0dff47e737
LovelySanta <laurens.scheldeman@gmail.com>
2020-03-19 12:54:00 +0100

    Added shred deco compatibility with angel industry technology overhaul

---
+++ angelsaddons-shred/data-updates.lua
@@ -0,0 +1,15 @@
+
+if mods["angelsindustries"] and angelsmods.industries.overhaul and angelsmods.industries.tech then
+  for _,decoName in pairs({
+    "shred",
+    "santa",
+    "inter",
+    "voske",
+    "east",
+  }) do
+    local itemName = "offering-"..decoName.."-1"
+    data.raw.item[itemName].subgroup = "angels-pack-components"
+    data.raw.item[itemName].order = "e"
+    data.raw.item[itemName].rocket_launch_product = {"science-gravitational-analyzer", 1000}
+  end
+end
