a55f6ef4ff86b34a856b355c7d50749d4ceebe74
Tim Vroom <800147+TimVroom@users.noreply.github.com>
2020-03-21 10:22:22 +0100

    Fixed electronic blocks in assemblers (#110)

--- angelsindustries/prototypes/overrides/components-bobs-entity-update.lua
+++ angelsindustries/prototypes/overrides/components-bobs-entity-update.lua
@@ -1,17 +1,22 @@
 local OV = angelsmods.functions.OV
 -- CUSTOM FIXES FOR BOBS
 
-if mods['bobassembly'] then
+if mods["bobassembly"] then
   -- burner assemblers
-  OV.patch_recipes({
+  OV.patch_recipes(
     {
-      name = "assembling-machine-2",
-      ingredients =
       {
-        {"block-electronics-1", "block-electronics-2"},
+        name = "assembling-machine-2",
+        ingredients = {
+          {"block-electronics-1", "block-electronics-2"}
+        },
+        minable = {
+          results = {
+            {"block-electronics-1", "block-electronics-2"}
+          }
+        }
       }
-    },
-  })
+    }
+  )
   OV.add_prereq("automation-2", "angels-components-construction-2")
 end
-
