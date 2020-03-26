0c694d0570b552b7e24fc5c0533353ac6974cf33
LovelySanta <laurens.scheldeman@gmail.com>
2020-03-26 20:24:45 +0100

    Fixed recipes would use the unobtainable advanced processing blocks

--- angelsindustries/data-final-fixes.lua
+++ angelsindustries/data-final-fixes.lua
@@ -18,6 +18,24 @@ if angelsmods.industries.overhaul and angelsmods.industries.components then
   data.raw.recipe["block-bprocessing-3"].hidden = true
   data.raw.recipe["block-bprocessing-4"].hidden = true
   data.raw.recipe["block-bprocessing-5"].hidden = true
+  OV.global_replace_item({
+    "block-extraction-3",
+    "block-mprocessing-3",
+    "block-cprocessing-3",
+    "block-bprocessing-3"
+  }, "block-production-3")
+  OV.global_replace_item({
+    "block-extraction-4",
+    "block-mprocessing-4",
+    "block-cprocessing-4",
+    "block-bprocessing-4"
+  }, "block-production-4")
+  OV.global_replace_item({
+    "block-extraction-5",
+    "block-mprocessing-5",
+    "block-cprocessing-5",
+    "block-bprocessing-5"
+  }, "block-production-5")
 
   OV.execute()
 end
