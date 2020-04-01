bbf66ee59ffbff2491d498964bcb3e5840a2e832
Tim Vroom <800147+TimVroom@users.noreply.github.com>
2020-04-01 15:20:02 +0200

    Fixes startup error

--- angelspetrochem/prototypes/global-override/bobrevamp.lua
+++ angelspetrochem/prototypes/global-override/bobrevamp.lua
@@ -3,23 +3,35 @@ if bobmods then
 
   if mods.bobrevamp then
     -- Move solid fuels arround
-    OV.remove_unlock("angels-oil-processing", "liquid-fuel")
-    OV.remove_unlock("angels-oil-processing", "solid-fuel-from-hydrogen")
+    OV.disable_technology({"solid-fuel"})
+
     OV.remove_unlock("angels-oil-processing", "solid-fuel-fuel-oil")
     OV.remove_unlock("angels-oil-processing", "solid-fuel-naphtha")
     OV.remove_unlock("gas-processing", "solid-fuel-methane")
-    OV.add_unlock("flammables", "solid-fuel-from-hydrogen")
-    data.raw.recipe["solid-fuel-from-hydrogen"].icons = angelsmods.functions.create_solid_recipe_icon({
-      { "__angelspetrochem__/graphics/icons/molecules/hydrogen.png", 72 }
-    }, "solid-fuel"),
+
     OV.add_unlock("flammables", "solid-fuel-naphtha")
     OV.add_unlock("flammables", "solid-fuel-fuel-oil")
-    OV.add_unlock("flammables", "liquid-fuel")
     OV.add_unlock("angels-advanced-gas-processing", "solid-fuel-methane")
+
     OV.add_prereq("angels-advanced-gas-processing", "flammables")
-    OV.add_prereq("angels-nitrogen-processing-3", "flammables")
-    OV.add_prereq("chemical-processing-3", "flammables")
     OV.add_prereq("gas-synthesis", "flammables")
-    OV.disable_technology({"solid-fuel"})
+    OV.add_prereq("angels-nitrogen-processing-3", "flammables")
+
+    if mods.bobplates then
+      OV.remove_unlock("angels-oil-processing", "liquid-fuel")
+      OV.remove_unlock("angels-oil-processing", "solid-fuel-from-hydrogen")
+
+      OV.add_unlock("flammables", "liquid-fuel")
+      OV.add_unlock("flammables", "solid-fuel-from-hydrogen")
+
+      OV.add_prereq("chemical-processing-3", "flammables")
+      data.raw.recipe["solid-fuel-from-hydrogen"].icons =
+        angelsmods.functions.create_solid_recipe_icon(
+        {
+          {"__angelspetrochem__/graphics/icons/molecules/hydrogen.png", 72}
+        },
+        "solid-fuel"
+      )
+    end
   end
 end
