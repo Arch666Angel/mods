44a922796d80e17f560776629cfe1b4f8dbc8942
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-04-04 15:14:24 +1100

    Fixed liquid-fuel-oil fuel value typo
    Also "rebalanced" the enriched fuel block and liquid-fuel values

--- angelspetrochem/prototypes/petrochem-override.lua
+++ angelspetrochem/prototypes/petrochem-override.lua
@@ -394,7 +394,7 @@ if mods.bobplates then
   data.raw.fluid["liquid-naphtha"].fuel_value = "244.7kJ"
   data.raw.fluid["liquid-naphtha"].emissions_multiplier = 3
   --liquid Fuel oil (light oil), bobs value 1.5MJ (light fuel oil 39 MJ/L)(40.6 MJ/kg)
-  data.raw.fluid["liquid-fuel-oil"].fuel_value = "24.99kJ"
+  data.raw.fluid["liquid-fuel-oil"].fuel_value = "249.9kJ" -- was 24.99kJ
   data.raw.fluid["liquid-fuel-oil"].emissions_multiplier = 2
   --gas methane (petrogas), bobs value 2.3MJ (methane 35.9 MJ/L)(49.85 MJ/kg)
   data.raw.fluid["gas-methane"].fuel_value = "230.0kJ"
@@ -420,6 +420,9 @@ if mods.bobplates then
   --gas hydrazine (), bobs value is 340kJ (hydrazine 19.8 MJ/L)(19.4 MJ/kg)
   data.raw.fluid["gas-hydrazine"].fuel_value = "126.9kJ"
   data.raw.fluid["gas-hydrazine"].emissions_multiplier = 0.1
+  --fuel oil balancing
+  data.raw.fluid["liquid-fuel"].fuel_value= "300kJ" --down from 2.3MJ
+  data.raw.recipe["enriched-fuel-from-liquid-fuel"].ingredients={{type = "fluid", name = "liquid-fuel", amount = 100}}--up from 20
   if mods["angelsbioprocessing"] then
     --liquid ethanol (), - (ethanol(L) 21.1 MJ/L)(26.7 MJ/kg)
     data.raw.fluid["gas-ethanol"].fuel_value = "135.2kJ"
