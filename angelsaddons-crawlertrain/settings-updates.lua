1482ae54c67e4d939932675717530ccea689141b
LovelySanta <laurens.scheldeman@gmail.com>
2020-04-09 09:14:03 +0200

    When bob trains are present, update the tier defaults to 3

---
+++ angelsaddons-crawlertrain/settings-updates.lua
@@ -0,0 +1,3 @@
+if mods.boblogistics then
+  data.raw["int-setting"]["angels-crawlertrain-tier-amount"].default_value = 3
+end
---
+++ angelsaddons-smeltingtrain/settings-updates.lua
@@ -0,0 +1,3 @@
+if mods.boblogistics then
+  data.raw["int-setting"]["angels-smeltingtrain-tier-amount"].default_value = 3
+end
