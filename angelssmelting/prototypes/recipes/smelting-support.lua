f83954e211f24dc581905bd6fe961ff139669a83
LovelySanta <laurens.scheldeman@gmail.com>
2020-04-12 10:16:46 +0200

    Fixed molding updates were in industries instead of smelting

--- angelssmelting/prototypes/recipes/smelting-support.lua
+++ angelssmelting/prototypes/recipes/smelting-support.lua
@@ -190,39 +190,87 @@ data:extend(
   {
     type = "recipe",
     name = "mold-expendable",
-    category = "smelting",
+    category = "sintering",
     subgroup = "angels-mold-casting",
     energy_required = 4,
     enabled = "false",
     ingredients =
     {
-      {type="item", name="clay-brick", amount=8},
+      {type = "item", name = "solid-sand", amount = 40},
     },
     results =
     {
-      {type="item", name="mold-expendable", amount=2},
+      {type="item", name="mold-expendable", amount=1},
+      {type = "item", name = "slag", amount = 2},
     },
+    main_product = "mold-expendable",
     icon_size = 32,
     order = "a[mold-expendable]",
   },
   {
     type = "recipe",
     name = "mold-non-expendable",
-    category = "blast-smelting",
+    category = "sintering",
     subgroup = "angels-mold-casting",
     energy_required = 4,
     enabled = "false",
     ingredients =
     {
-      {type="item", name="concrete-brick", amount=8},
+      {type = "item", name = "solid-clay", amount = 50}
     },
     results =
     {
-      {type="item", name="mold-non-expendable", amount=2},
+      {type = "item", name = "mold-non-expendable", amount = 1},
+      {type = "item", name = "solid-iron-hydroxide", amount = 4}
+    },
+    main_product = "mold-non-expendable",
+    icons =
+    {
+      {
+        icon = "__angelssmelting__/graphics/icons/non-expendable-mold.png"
+      },
+      {
+        icon = "__angelsrefining__/graphics/icons/num_1.png",
+        tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12}
+      }
     },
     icon_size = 32,
-    order = "b[mold-non-expendable]",
+    order = "b[mold-non-expendable]-a[creation]",
     stack_size = 200
+  },
+  {
+    type = "recipe",
+    name = "mold-non-expendable-wash",
+    category = "crafting-with-fluid",
+    subgroup = "angels-mold-casting",
+    energy_required = 3,
+    enabled = "false",
+    ingredients =
+    {
+      {type = "item", name = "spent-mold-non-expendable", amount = 1},
+      {type = "fluid", name = "liquid-nitric-acid", amount = 20}
+    },
+    results =
+    {
+      {type = "item", name = "mold-non-expendable", amount = 1, probability = 0.90, catalyst_amount = 1},
+      {type = "fluid", name = "water-red-waste", amount = 20}
+    },
+    icons =
+    {
+      {
+        icon = "__angelssmelting__/graphics/icons/non-expendable-mold.png"
+      },
+      {
+        icon = "__angelsrefining__/graphics/icons/num_2.png",
+        tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
+        scale = 0.32,
+        shift = {-12, -12}
+      }
+    },
+    icon_size = 32,
+    order = "b[mold-non-expendable]-b[cleaning]",
   },
 }
 )
