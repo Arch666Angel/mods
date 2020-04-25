6c9306fae6eb490fb3f0789ae30177f183d7f269
Tim Vroom <800147+TimVroom@users.noreply.github.com>
2020-04-25 16:59:36 +0200

    Merge fail from dev

--- angelssmelting/prototypes/technology/smelting-alloy-steel.lua
+++ angelssmelting/prototypes/technology/smelting-alloy-steel.lua
@@ -124,10 +124,10 @@ data:extend(
       unit = {
         count = 50,
         ingredients = {
-          {"automation-science-pack", 1},
-          {"logistic-science-pack", 1},
-          {"chemical-science-pack", 1},
-          {"production-science-pack", 1}
+          {type = "item", name = "automation-science-pack", amount = 1},
+          {type = "item", name = "logistic-science-pack", amount = 1},
+          {type = "item", name = "chemical-science-pack", amount = 1},
+          {type = "item", name = "production-science-pack", amount = 1}
         },
         time = 30
       },
