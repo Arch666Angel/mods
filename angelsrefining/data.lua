cc3959c9623d3a9241c69986fb39cea28fe603e8
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-05-03 21:01:07 +1000

    forgot to save a file

--- angelsrefining/data.lua
+++ angelsrefining/data.lua
@@ -60,59 +60,54 @@ angelsmods.functions.store = angelsmods.functions.store or {}
 angelsmods.functions.store.update = angelsmods.functions.store.update or {}
 angelsmods.functions.store.make = angelsmods.functions.store.make or {}
 
-if angelsmods.functions then
-  --Helper Functions
-  require("prototypes.angels-functions")
-  --Function set for overrides (items, recipes, techs)
-  angelsmods.functions.OV = require("prototypes.override-functions")
-  --Function set to build recipes and normalize them
-  angelsmods.functions.RB = require("prototypes.recipe-builder")
-  --Function set to build resources
-  require("prototypes.generation.resource-builder")
-end
+--Helper Functions
+require("prototypes.angels-functions")
+--Function set for overrides (items, recipes, techs)
+angelsmods.functions.OV = require("prototypes.override-functions")
+--Function set to build recipes and normalize them
+angelsmods.functions.RB = require("prototypes.recipe-builder")
+--Function set to build resources
+require("prototypes.generation.resource-builder")
 
 --LOAD PROTOTYPES
-if angelsmods.refining then
-  require("prototypes.refining-category")
+require("prototypes.refining-category")
 
-  require("prototypes.generation.angels-ore")
-  require("prototypes.generation.bob-ore-update")
-  require("prototypes.generation.angels-crystal-rock")
+require("prototypes.generation.angels-ore")
+require("prototypes.generation.bob-ore-update")
+require("prototypes.generation.angels-crystal-rock")
 
-  require("prototypes.buildings.pipes-overlay")
-  require("prototypes.buildings.ore-crusher")
-  require("prototypes.buildings.ore-sorting-facility")
-  require("prototypes.buildings.ore-floatation-cell")
-  require("prototypes.buildings.ore-leaching-plant")
-  require("prototypes.buildings.ore-refinery")
-  require("prototypes.buildings.powderizer")
-  require("prototypes.buildings.electro-whinning-cell")
-  require("prototypes.buildings.crystallizer")
-  require("prototypes.buildings.filtration-unit")
-  require("prototypes.buildings.liquifier")
-  require("prototypes.buildings.thermal-extractor")
+require("prototypes.buildings.pipes-overlay")
+require("prototypes.buildings.ore-crusher")
+require("prototypes.buildings.ore-sorting-facility")
+require("prototypes.buildings.ore-floatation-cell")
+require("prototypes.buildings.ore-leaching-plant")
+require("prototypes.buildings.ore-refinery")
+require("prototypes.buildings.powderizer")
+require("prototypes.buildings.electro-whinning-cell")
+require("prototypes.buildings.crystallizer")
+require("prototypes.buildings.filtration-unit")
+require("prototypes.buildings.liquifier")
+require("prototypes.buildings.thermal-extractor")
 
-  require("prototypes.buildings.hydro-plant")
-  require("prototypes.buildings.salination-plant")
-  require("prototypes.buildings.seafloor-pump")
-  require("prototypes.buildings.washing-plant")
-  require("prototypes.buildings.clarifier")
-  require("prototypes.buildings.barreling-pump")
+require("prototypes.buildings.hydro-plant")
+require("prototypes.buildings.salination-plant")
+require("prototypes.buildings.seafloor-pump")
+require("prototypes.buildings.washing-plant")
+require("prototypes.buildings.clarifier")
+require("prototypes.buildings.barreling-pump")
 
-  require("prototypes.items.refining-ores")
-  require("prototypes.items.refining-items")
-  require("prototypes.items.water-treatment-items")
+require("prototypes.items.refining-ores")
+require("prototypes.items.refining-items")
+require("prototypes.items.water-treatment-items")
 
-  require("prototypes.recipes.refining-static")
-  require("prototypes.recipes.refining-dynamic")
-  require("prototypes.recipes.refining-entity")
+require("prototypes.recipes.refining-static")
+require("prototypes.recipes.refining-dynamic")
+require("prototypes.recipes.refining-entity")
 
-  require("prototypes.recipes.water-treatment-recipes")
+require("prototypes.recipes.water-treatment-recipes")
 
-  require("prototypes.technology.refining-technology")
+require("prototypes.technology.refining-technology")
 
-  require("prototypes.technology.water-treatment-technology")
-end
-
+require("prototypes.technology.water-treatment-technology")
 -- EXECUTE FUNCTIONS
 angelsmods.functions.make_resource()
