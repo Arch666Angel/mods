0e1b7a9aa9de1a98f41c334c479969a512a2ee79
LovelySanta <laurens.scheldeman@gmail.com>
2020-05-18 11:03:51 +0200

    added migration for garden mutations (#231)

--- angelsbioprocessing/migrations/angelsbioprocessing_0.7.9.lua
+++ angelsbioprocessing/migrations/angelsbioprocessing_0.7.9.lua
@@ -5,4 +5,24 @@ for index, force in pairs(game.forces) do
   force.reset_recipes()
   force.reset_technologies()
   force.reset_technology_effects()
+
+  local researched = false
+  for _, techName in pairs{
+    "bio-arboretum-2",
+    "bio-arboretum-desert-2",
+    "bio-arboretum-temperate-2",
+    "bio-arboretum-swamp-2",
+    "bio-desert-farm",
+    "bio-temperate-farm",
+    "bio-swamp-farm",
+  } do
+    if (force.technologies[techName] or {}).researched then
+      researched = true
+      break
+    end
+  end
+  if researched then
+    force.technologies["bio-farm-alien"].researched = true
+    force.technologies["gardens-2"].researched = true
+  end
 end
