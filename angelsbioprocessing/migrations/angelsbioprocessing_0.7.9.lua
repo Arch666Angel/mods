cf4ae075baf18125f1ce46a22cd85bd15682a29c
LovelySanta <laurens.scheldeman@gmail.com>
2020-05-22 22:32:58 +0200

    some more migrations

--- angelsbioprocessing/migrations/angelsbioprocessing_0.7.9.lua
+++ angelsbioprocessing/migrations/angelsbioprocessing_0.7.9.lua
@@ -26,14 +26,26 @@ for index, force in pairs(game.forces) do
     force.technologies["bio-farm-alien"].researched = true
     force.technologies["gardens-2"].researched = true
 
-    if force.technologies["bio-temperate-farming-2"] then
-      force.technologies["bio-temperate-farming-2"].researched = (force.technologies["bio-temperate-farming-1"] or {}).researched or false
+    for techToResearch, techToCheck in pairs{
+      ["bio-temperate-farming-2"] = "bio-temperate-farming-1",
+      ["bio-desert-farming-2"] = "bio-desert-farming-1",
+      ["bio-desert-farming-2"] = "bio-desert-farming-1",
+    } do
+      if force.technologies[techToResearch] and force.technologies[techToCheck] then
+        force.technologies[techToResearch].researched = force.technologies[techToCheck].researched or force.technologies[techToResearch].researched
+      end
     end
-    if force.technologies["bio-desert-farming-2"] then
-      force.technologies["bio-desert-farming-2"].researched = (force.technologies["bio-desert-farming-1"] or {}).researched or false
-    end
-    if force.technologies["bio-swamp-farming-2"] then
-      force.technologies["bio-swamp-farming-2"].researched = (force.technologies["bio-swamp-farming-1"] or {}).researched or false
+  end
+
+  for techToResearch, techToCheck in pairs{
+    ["bio-processing-crystal-splinter-2"] = "bio-processing-crystal-splinter-1",
+    ["bio-processing-crystal-splinter-3"] = "bio-processing-crystal-splinter-1",
+    ["bio-processing-crystal-shard-2"] = "bio-processing-crystal-shard-1",
+    ["bio-processing-crystal-shard-3"] = "bio-processing-crystal-shard-1",
+    ["bio-processing-alien-2"] = "bio-refugium-biter-2",
+  } do
+    if force.technologies[techToResearch] and force.technologies[techToCheck] then
+      force.technologies[techToResearch].researched = force.technologies[techToCheck].researched or force.technologies[techToResearch].researched
     end
   end
 end
