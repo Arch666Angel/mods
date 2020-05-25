8baca2ff6c3169ee6ed027a8d3bd584f19b48d51
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-05-25 16:53:01 +1000

    ensure token-bio does not get added if already in the lab list

--- angelsbioprocessing/prototypes/bio-processing-override.lua
+++ angelsbioprocessing/prototypes/bio-processing-override.lua
@@ -35,8 +35,18 @@ end
 
 --UPDATE LABS INPUT
 for i, labs in pairs(data.raw["lab"]) do
-  if not lab_ignore[labs.name] then
-    table.insert(labs.inputs, "token-bio")
+  --first check it does not exist
+  local found = false
+  for i, chk in ipairs(labs.inputs) do
+    for chk == "token-bio" then
+      found = true
+    end
+  end
+  --add it to all the labs
+  if not lab_ignore[labs.name] and not found then
+    if not labs.inputs["token-bio"] then 
+      table.insert(labs.inputs, "token-bio")
+    end
   end
 end
 
