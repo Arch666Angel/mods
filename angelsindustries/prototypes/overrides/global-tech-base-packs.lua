1f5b46069b625fb8131430ffe669b584492255c8
LovelySanta <laurens.scheldeman@gmail.com>
2020-06-01 12:06:29 +0200

    small change

--- angelsindustries/prototypes/overrides/global-tech-base-packs.lua
+++ angelsindustries/prototypes/overrides/global-tech-base-packs.lua
@@ -114,7 +114,7 @@ if angelsmods.industries.tech then
   --REFINING
   pack_replace("water-treatment-3", "green", "orange")
   pack_replace("ore-leaching", "blue", "orange")
-  pack_replace("geode-processing-2", "green", "orange")
+  --pack_replace("geode-processing-2", "green", "orange")
   pack_replace("advanced-ore-refining-2", "green", "orange")
   --SMELTING
   pack_replace("angels-stone-smelting-3", "blue", "orange")
