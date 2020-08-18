c4af401325f164662f15fc7acd278ccc49a3519e
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-08-18 23:42:56 +1000

    Removed CAB from draft

--- angelsaddons-mobility/prototypes/categories.lua
+++ angelsaddons-mobility/prototypes/categories.lua
@@ -1,6 +1,3 @@
---------------------------------------------------------------------------------------------------
--- CAB categories --------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------
 if not data.raw["equipment-category"]["angels-void"] then
   data:extend(
   {
@@ -10,31 +7,6 @@ if not data.raw["equipment-category"]["angels-void"] then
     },
   })
 end
-
-data:extend(
-{
---EQUIPMENT CATEGORY
-{
-  type = "equipment-category",
-  name = "angels-cab"
-  },
---AMMO CATEGORY
-{
-  type = "ammo-category",
-  name = "angels-cab-deploy",
-},
-{
-  type = "ammo-category",
-  name = "angels-cab-undeploy",
-},
---SUBGROUPS
-{
-  type = "item-subgroup",
-  name = "angels-cab",
-  group = mods["angelsindustries"] and "angels-vehicles" or "logistics",
-  order = mods["angelsindustries"] and "da" or "zc",
-},
-})
 --------------------------------------------------------------------------------------------------
 -- Train categories ------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------
--- angelsaddons-mobility/prototypes/recipes/recipe-updates.lua
+++ angelsaddons-mobility/prototypes/recipes/recipe-updates.lua
@@ -1,29 +1,5 @@
 local RB = angelsmods.functions.RB
---------------------------------------------------------------------------------------------------
--- CAB recipes -----------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------
 if angelsmods.industries and angelsmods.industries.components then
-  if angelsmods.addons.mobility.cab then
-    angelsmods.functions.RB.build(
-      {
-        {
-          type = "recipe",
-          name = "angels-cab",
-          energy_required = 10,
-          enabled = false,
-          ingredients = {
-            {type = "item", name = "motor-3", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 10},
-            {type = "item", name = "construction-frame-3", amount = 5},
-            {type = "item", name = "construction-components", amount = 20},
-            {type = "item", name = "circuit-orange-loaded", amount = 2},
-            {type = "item", name = "angels-crawler", amount = 1}
-          },
-          result = "angels-cab"
-        }
-      }
-    )
-  end
 --------------------------------------------------------------------------------------------------
 -- TRAIN recipes ---------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------
