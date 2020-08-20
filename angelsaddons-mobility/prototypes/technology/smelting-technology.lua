2a53c119e6a8b4eec36f4adf4321d7134689c67f
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-08-20 12:14:49 +1000

    Train consolidation (#361)
    * Initial Combination
    
    Cab
    Smelting Train
    Petro Train
    Crawler Train
    All functional including "off settings"
    
    * Removed CAB from draft
    
    * missed one
    
    * Added AngelsStorage
    
    * added thumbnails
    
    * added initial changelog and set version number
    
    Co-authored-by: LaurensScheldeman <laurens.scheldeman@gmail.com>

---
+++ angelsaddons-mobility/prototypes/technology/smelting-technology.lua
@@ -0,0 +1,74 @@
+local funcs = require("prototypes/train-functions")
+if angelsmods.addons.mobility.smeltingtrain.enabled then
+local tiers = {
+  {
+    count = 60,
+    ingredients = {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1}
+    },
+    time = 30
+  },
+  {
+    count = 100,
+    ingredients = {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1}
+    },
+    time = 30
+  },
+  {
+    count = 100,
+    ingredients = {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1},
+      {"chemical-science-pack", 1}
+    },
+    time = 30
+  },
+  {
+    count = 100,
+    ingredients = {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1},
+      {"chemical-science-pack", 1},
+      {"production-science-pack", 1}
+    },
+    time = 30
+  },
+  {
+    count = 100,
+    ingredients = {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1},
+      {"chemical-science-pack", 1},
+      {"production-science-pack", 1},
+      {"utility-science-pack", 1}
+    },
+    time = 30
+  }
+}
+
+local extra_prereq = {
+  nil,
+  {"speed-module", "effectivity-module"},
+  {"speed-module-2", "effectivity-module-2", "low-density-structure", "electric-engine"},
+  {"speed-module-3", "effectivity-module-3"},
+  {"rocket-control-unit"}
+}
+
+funcs.generate_train_technology(
+  {
+    type = "technology",
+    name = "angels-smelting-train",
+    icon = "__angelsaddons-mobility__/graphics/technology/smelting-loco-1-tech.png",
+    icon_size = 128,
+    prerequisites = {
+      "railway"
+    },
+    order = "c-a"
+  },
+  tiers,
+  extra_prereq
+)
+end
---
+++ angelsaddons-mobility/settings.lua
@@ -0,0 +1,31 @@
+data:extend(
+  {
+    {
+      type = "int-setting",
+      name = "angels-crawlertrain-tier-amount",
+      setting_type = "startup",
+      default_value = 1,
+      minimum_value = 0, --0 disables
+      maximum_value = 5,
+      order = "a[trains]-a"
+    },
+    {
+      type = "int-setting",
+      name = "angels-petrotrain-tier-amount",
+      setting_type = "startup",
+      default_value = 1,
+      minimum_value = 0, --0 disables
+      maximum_value = 5,
+      order = "a[trains]-b"
+    },
+    {
+      type = "int-setting",
+      name = "angels-smeltingtrain-tier-amount",
+      setting_type = "startup",
+      default_value = 1,
+      minimum_value = 0, --0 disables
+      maximum_value = 5,
+      order = "a[trains]-c"
+    },
+  }
+)
