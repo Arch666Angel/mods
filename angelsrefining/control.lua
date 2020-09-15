90b764f7b5f411e2c9878fcd9a638a6871c0c7cf
LaurensScheldeman <laurens.scheldeman@gmail.com>
2020-09-16 01:47:49 +0200

    some more gui style work

--- angelsrefining/control.lua
+++ angelsrefining/control.lua
@@ -15,176 +15,161 @@ end)
 )]]
 
 script.on_event(defines.events.on_cutscene_cancelled, function(event)
+  if game.active_mods['angelsexploration'] then return end
 
-if not game.active_mods['angelsexploration'] then
+  local player = game.players[event.player_index]
 
-local player = game.players[event.player_index]
+  local welcomeFrame = player.gui.screen.add(
+    {
+      type = 'frame',
+      name = 'angels_welcome_screen',
+      direction = 'vertical'
+    }
+  )
+  welcomeFrame.style.maximal_width = 400
 
-local welcome = player.gui.screen.add(
-  {
-    type = 'frame',
-    name = 'welcome_screen',
-    direction = 'vertical',
-  }
-)
+  local welcomeFrameHeader = welcomeFrame.add(
+    {
+      type = 'flow',
+      name = 'header',
+    }
+  )
+  welcomeFrameHeader.drag_target = welcomeFrame
+  welcomeFrameHeader.style.horizontally_stretchable = true
 
-welcome.style.maximal_width = 400
+  welcomeFrameHeader.add(
+    {
+      type = 'label',
+      name = 'header_label',
+      caption = 'Enemy settings',
+      style = 'frame_title',
+      ignored_by_interaction = true
+    }
+  ).drag_target = welcomeFrame
 
-welcome.add(
-  {
-    type = 'flow',
-    name = 'header',
-  }
-)
+  local headerSpace = welcomeFrameHeader.add(
+    {
+      type = 'empty-widget',
+      name = 'space',
+      style = 'draggable_space_header',
+      ignored_by_interaction = true
+    }
+  )
+  headerSpace.style.horizontally_stretchable = true
+  headerSpace.style.height = 24
+  headerSpace.style.right_margin = 4
 
-welcome.header.drag_target = welcome
+  welcomeFrameHeader.add(
+    {
+      type = "sprite-button",
+      name = 'welcome_close_button',
+      sprite = "utility/close_white",
+      hovered_sprite = "utility/close_black",
+      clicked_sprite = "utility/close_black",
+      style = 'frame_action_button'
+    }
+  )
 
-welcome.header.add(
-  {
-    type = 'label',
-    name = 'header_label',
-    caption = 'Enemy settings'
-  }
-)
+  welcomeFrame.force_auto_center()
 
-welcome.header.header_label.drag_target = welcome
+  local welcomeFrameContent = welcomeFrame.add(
+    {
+      type = 'frame',
+      name = 'angels_welcome_screen_content',
+      direction = 'vertical',
+      style = 'inside_shallow_frame_with_padding',
+      ignored_by_interaction = true
+    }
+  )
 
-welcome.header.add(
-  {
-    type = 'empty-widget',
-    name = 'space',
-  }
-)
+  local pollutionSetting = game.map_settings.pollution.enabled and "enabled" or "disabled"
+  welcomeFrameContent.add(
+    {
+      type = 'label',
+      name = 'pollution_message',
+      caption = {'angels_welcome_accept_button.pollution-check', {'angels_welcome_accept_button.' .. pollutionSetting}},
+      ignored_by_interaction = true
+    }
+  )
 
-welcome.header.space.drag_target = welcome
+  local enemySizeSetting = player.surface.map_gen_settings['autoplace_controls']['angels-biter-slider'].size > 1 and "enabled" or "disabled"
+  welcomeFrameContent.add(
+    {
+      type = 'label',
+      name = 'enemy_size_message',
+      caption = {'angels_welcome_accept_button.enemy-check', {'angels_welcome_accept_button.' .. enemySizeSetting}},
+      ignored_by_interaction = true
+    }
+  )
 
-welcome.header.space.style.horizontally_stretchable = true
+  local enemyEvolutionSetting = game.map_settings.enemy_evolution.enabled and "enabled" or "disabled"
+  welcomeFrameContent.add(
+    {
+      type = 'label',
+      name = 'enemy_evolution_message',
+      caption = {'angels_welcome_accept_button.evo-check',  {'angels_welcome_accept_button.' .. enemyEvolutionSetting}},
+      ignored_by_interaction = true
+    }
+  )
 
-welcome.header.add(
-  {
-    type = "sprite-button",
-    name = 'welcome_close_button',
-    sprite = "utility/close_fat"
-  }
-)
+  local enemyExpansionSetting = game.map_settings.enemy_expansion.enabled and "enabled" or "disabled"
+  welcomeFrameContent.add(
+    {
+      type = 'label',
+      name = 'expansion_message',
+      caption = {'angels_welcome_accept_button.exp-check',  {'angels_welcome_accept_button.' .. enemyExpansionSetting}},
+      ignored_by_interaction = true
+    }
+  )
 
-welcome.header.style.horizontal_align = 'right'
+  welcomeFrameContent.add(
+    {
+      type = 'label',
+      name = 'faq',
+      caption = {'angels_welcome_accept_button.faq'},
+      ignored_by_interaction = true
+    }
+  ).style.single_line = false
 
-welcome.force_auto_center()
+  local welcomeFrameFooter = welcomeFrame.add(
+    {
+      type = 'flow',
+      name = 'footer',
+      style = 'dialog_buttons_horizontal_flow'
+    }
+  )
+  welcomeFrameFooter.style.horizontally_stretchable = true
 
-local pollution = ''
-if game.map_settings.pollution.enabled == true then
-  pollution = {'angels_welcome_accept_button.enabled'}
-else
-  pollution = {'angels_welcome_accept_button.disabled'}
-end
+  local footerSpace = welcomeFrameFooter.add(
+    {
+      type = 'empty-widget',
+      name = 'bottom_space',
+      --style = 'draggable_space'
+      style = 'draggable_space_with_no_left_margin',
+      ignored_by_interaction = true
+    }
+  )
+  footerSpace.style.horizontally_stretchable = true
+  footerSpace.style.height = 32
 
---local string = ({'angels_welcome_accept_button.pollution-check', pollution})
-
-welcome.add(
-  {
-    type = 'label',
-    name = 'pollution_message',
-    caption = {'angels_welcome_accept_button.pollution-check', pollution}
-  }
-)
-
-local enemy = ''
-if player.surface.map_gen_settings['autoplace_controls']['angels-biter-slider'].size > 1 then
-  enemy = {'angels_welcome_accept_button.enabled'}
-else
-  enemy = {'angels_welcome_accept_button.disabled'}
-end
-
-welcome.add(
-  {
-    type = 'label',
-    name = 'enemy_message',
-    caption = {'angels_welcome_accept_button.enemy-check', enemy}
-  }
-)
-
-local evo = ''
-if game.map_settings.enemy_evolution.enabled == true then
-  evo = {'angels_welcome_accept_button.enabled'}
-else
-  evo = {'angels_welcome_accept_button.disabled'}
-end
-
-welcome.add(
-  {
-    type = 'label',
-    name = 'evolution_message',
-    caption = {'angels_welcome_accept_button.evo-check', evo}
-  }
-)
-
-local exp = ''
-if game.map_settings.enemy_expansion.enabled == true then
-  exp = {'angels_welcome_accept_button.enabled'}
-else
-  exp = {'angels_welcome_accept_button.disabled'}
-end
-
-welcome.add(
-  {
-    type = 'label',
-    name = 'expansion_message',
-    caption = {'angels_welcome_accept_button.exp-check', exp}
-  }
-)
-
-welcome.add(
-  {
-    type = 'label',
-    name = 'faq',
-    caption = {'angels_welcome_accept_button.faq'}
-  }
-)
-
-welcome.faq.style.single_line = false
-
-welcome.add(
-  {
-    type = 'flow',
-    name = 'bottom',
-  }
-)
-
-welcome.bottom.style.horizontal_align = 'right'
-welcome.bottom.add(
-  {
-    type = 'empty-widget',
-    name = 'bottom_space',
-  }
-)
-
-welcome.bottom.bottom_space.style.horizontally_stretchable = true
-
-welcome.bottom.add(
-  {
-    type = 'button',
-    name = 'welcome_accept_button',
-    caption = 'OK'
-  }
-)
-
-welcome.bottom.welcome_accept_button.style = 'confirm_button'
-
-end
+  welcomeFrameFooter.add(
+    {
+      type = 'button',
+      name = 'welcome_accept_button',
+      caption = 'OK',
+      style = 'confirm_button'
+    }
+  )
 
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
-
-  local player = game.players[event.player_index]
-
-  if event.element.name == 'welcome_close_button' and player.gui.screen.welcome_screen ~= nil then
-    player.gui.screen.welcome_screen.destroy()
-  elseif event.element.name == 'welcome_accept_button' and player.gui.screen.welcome_screen ~= nil then
-    player.gui.screen.welcome_screen.destroy()
+  if event.element.name == 'welcome_close_button' or event.element.name == 'welcome_accept_button' then
+    local player = game.players[event.player_index]
+    if player.gui.screen.angels_welcome_screen then
+      player.gui.screen.angels_welcome_screen.destroy()
+    end
   end
-
 end)
 
 script.on_event(defines.events.on_player_rotated_entity, function(event)
