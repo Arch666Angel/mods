6db240c4480c772b7b1f09e45dd81ce9907a16ba
lovely_santa <laurens.scheldeman@gmail.com>
2020-10-04 17:46:28 +0200

    Fixed angels smelting train recipes

--- angelsaddons-mobility/prototypes/recipes/recipe-updates.lua
+++ angelsaddons-mobility/prototypes/recipes/recipe-updates.lua
@@ -61,6 +61,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.petrotrain.enabled then
     RB.build(
       {
@@ -106,7 +107,8 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
-  if angelsmods.addons.mobility.petrotrain.enabled then
+
+  if angelsmods.addons.mobility.smeltingtrain.enabled then
     RB.build(
       {
         {
@@ -151,6 +153,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.crawlertrain.tier_amount >= 2 then
     RB.build(
       {
@@ -213,6 +216,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.petrotrain.tier_amount >= 2 then
     RB.build(
       {
@@ -261,6 +265,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.smeltingtrain.tier_amount >= 2 then
     RB.build(
       {
@@ -273,7 +278,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-2", amount = 15},
             {type = "item", name = "circuit-green-loaded", amount = 15},
             {type = "item", name = "construction-frame-2", amount = 10},
-            {type = "item", name = "mechanical-parts", amount = 10}
+            {type = "item", name = "mechanical-parts", amount = 10},
+            {type = "item", name = "smelting-locomotive-1", amount = 1}
           },
           result = "smelting-locomotive-1-2"
         },
@@ -286,7 +292,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-2", amount = 15},
             {type = "item", name = "circuit-green-loaded", amount = 10},
             {type = "item", name = "construction-frame-2", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-locomotive-tender", amount = 1}
           },
           result = "smelting-locomotive-tender-2"
         },
@@ -299,13 +306,15 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "angels-servo-motor-2", amount = 15},
             {type = "item", name = "circuit-green-loaded", amount = 10},
             {type = "item", name = "construction-frame-2", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-wagon-1", amount = 1}
           },
           result = "smelting-wagon-1-2"
         }
       }
     )
   end
+
   if angelsmods.addons.mobility.crawlertrain.tier_amount >= 3 then
     RB.build(
       {
@@ -368,6 +377,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.petrotrain.tier_amount >= 3 then
     RB.build(
       {
@@ -416,6 +426,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+  
   if angelsmods.addons.mobility.smeltingtrain.tier_amount >= 3 then
     RB.build(
       {
@@ -428,7 +439,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-3", amount = 15},
             {type = "item", name = "circuit-orange-loaded", amount = 15},
             {type = "item", name = "construction-frame-3", amount = 10},
-            {type = "item", name = "mechanical-parts", amount = 10}
+            {type = "item", name = "mechanical-parts", amount = 10},
+            {type = "item", name = "smelting-locomotive-1-2", amount = 1}
           },
           result = "smelting-locomotive-1-3"
         },
@@ -441,7 +453,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-3", amount = 15},
             {type = "item", name = "circuit-orange-loaded", amount = 10},
             {type = "item", name = "construction-frame-3", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-locomotive-tender-2", amount = 1}
           },
           result = "smelting-locomotive-tender-3"
         },
@@ -454,13 +467,15 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "angels-servo-motor-3", amount = 15},
             {type = "item", name = "circuit-orange-loaded", amount = 10},
             {type = "item", name = "construction-frame-3", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-wagon-1-2", amount = 1}
           },
           result = "smelting-wagon-1-3"
         }
       }
     )
   end
+
   if angelsmods.addons.mobility.crawlertrain.tier_amount >= 4 then
     RB.build(
       {
@@ -523,6 +538,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.petrotrain.tier_amount >= 4 then
     RB.build(
       {
@@ -571,6 +587,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.smeltingtrain.tier_amount >= 4 then
     RB.build(
       {
@@ -583,7 +600,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-4", amount = 15},
             {type = "item", name = "circuit-blue-loaded", amount = 15},
             {type = "item", name = "construction-frame-4", amount = 10},
-            {type = "item", name = "mechanical-parts", amount = 10}
+            {type = "item", name = "mechanical-parts", amount = 10},
+            {type = "item", name = "smelting-locomotive-1-3", amount = 1}
           },
           result = "smelting-locomotive-1-4"
         },
@@ -596,7 +614,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-4", amount = 15},
             {type = "item", name = "circuit-blue-loaded", amount = 10},
             {type = "item", name = "construction-frame-4", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-locomotive-tender-3", amount = 1}
           },
           result = "smelting-locomotive-tender-4"
         },
@@ -609,13 +628,15 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "angels-servo-motor-4", amount = 15},
             {type = "item", name = "circuit-blue-loaded", amount = 10},
             {type = "item", name = "construction-frame-4", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-wagon-1-3", amount = 1}
           },
           result = "smelting-wagon-1-4"
         }
       }
     )
   end
+
   if angelsmods.addons.mobility.crawlertrain.tier_amount >= 5 then
     RB.build(
       {
@@ -678,6 +699,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.petrotrain.tier_amount >= 5 then
     RB.build(
       {
@@ -726,6 +748,7 @@ if angelsmods.industries and angelsmods.industries.components then
       }
     )
   end
+
   if angelsmods.addons.mobility.smeltingtrain.tier_amount >= 5 then
     RB.build(
       {
@@ -738,7 +761,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-5", amount = 15},
             {type = "item", name = "circuit-yellow-loaded", amount = 15},
             {type = "item", name = "construction-frame-5", amount = 10},
-            {type = "item", name = "mechanical-parts", amount = 10}
+            {type = "item", name = "mechanical-parts", amount = 10},
+            {type = "item", name = "smelting-locomotive-1-4", amount = 1}
           },
           result = "smelting-locomotive-1-5"
         },
@@ -751,7 +775,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "motor-5", amount = 15},
             {type = "item", name = "circuit-yellow-loaded", amount = 10},
             {type = "item", name = "construction-frame-5", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-locomotive-tender-4", amount = 1}
           },
           result = "smelting-locomotive-tender-5"
         },
@@ -764,7 +789,8 @@ if angelsmods.industries and angelsmods.industries.components then
             {type = "item", name = "angels-servo-motor-5", amount = 15},
             {type = "item", name = "circuit-yellow-loaded", amount = 10},
             {type = "item", name = "construction-frame-5", amount = 5},
-            {type = "item", name = "mechanical-parts", amount = 5}
+            {type = "item", name = "mechanical-parts", amount = 5},
+            {type = "item", name = "smelting-wagon-1-4", amount = 1}
           },
           result = "smelting-wagon-1-5"
         }
