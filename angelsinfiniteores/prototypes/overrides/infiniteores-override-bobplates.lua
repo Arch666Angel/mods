9c0546caacf8c1550718ab16a5897687abfd286f
lovely_santa <laurens.scheldeman@gmail.com>
2020-10-07 19:06:35 +0200

    Removed bob's coal from wood when playing with angels infinite ores (#379)

---
+++ angelsinfiniteores/prototypes/overrides/infiniteores-override-bobplates.lua
@@ -0,0 +1,7 @@
+if mods["bobplates"] and mods["angelsbioprocessing"] then
+  -- there is an infinite wood production available, coal should
+  -- be obtained from infinite coal patches instead
+  local OV = angelsmods.functions.OV
+  OV.disable_recipe({"bob-coal-from-wood"})
+  OV.execute()
+end
