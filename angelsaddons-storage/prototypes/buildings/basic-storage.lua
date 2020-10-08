249bf010c16e61fd82943f18e4683e6220ad5db1
lovely_santa <laurens.scheldeman@gmail.com>
2020-10-08 11:17:41 +0200

    Added Inline storage tank as an option when playing without Angels petrochem (#387)

--- angelsaddons-storage/prototypes/buildings/basic-storage.lua
+++ angelsaddons-storage/prototypes/buildings/basic-storage.lua
@@ -100,6 +100,119 @@ if angelsmods.addons.storage.warehouses then
     }
   )
 end
+if angelsmods.addons.storage.inlinetank and (not mods["angelspetrochem"]) then
+  data:extend(
+    {
+      {
+        type = "item",
+        name = "angels-storage-tank-3",
+        icon = "__angelsaddons-storage__/graphics/icons/storage-tank-3.png",
+        icon_size = 32,
+        subgroup = "storage",
+        order = "b[fluid]-b[inline-storage-tank]",
+        place_result = "angels-storage-tank-3",
+        stack_size = 10,
+      },  
+      {
+        type = "storage-tank",
+        name = "angels-storage-tank-3",
+        icon = "__angelsaddons-storage__/graphics/icons/storage-tank-3.png",
+        icon_size = 32,
+        flags = {"placeable-player", "player-creation"},
+        minable = {hardness = 0.2, mining_time = 3, result = "angels-storage-tank-3"},
+        max_health = 500,
+        corpse = "medium-remnants",
+        collision_box = {{-0.9, -0.9}, {0.9, 0.9}},
+        selection_box = {{-1, -1}, {1, 1}},
+        two_direction_only = false,
+        fluid_box =
+        {
+          base_area = 200,
+          pipe_covers = pipecoverspictures(),
+          pipe_connections =
+          {
+            { position = {0.5, -1.5} },
+            { position = {0.5, 1.5} },
+          },
+        },
+        window_bounding_box = {{-0.125, 0.6875}, {0.1875, 1.1875}},
+        pictures =
+        {
+          picture =
+          {
+            sheet =
+            {
+              filename = "__angelsaddons-storage__/graphics/entity/storage-tank-3.png",
+              priority = "extra-high",
+              frames = 4,
+              width = 256,
+              height = 320,
+              scale = 0.495,
+              shift = {0, -0.5}
+            }
+          },
+          fluid_background =
+          {
+            filename = "__angelsaddons-storage__/graphics/entity/blank.png",
+            priority = "extra-high",
+            width = 1,
+            height = 1
+          },
+          window_background =
+          {
+            filename = "__angelsaddons-storage__/graphics/entity/blank.png",
+            priority = "extra-high",
+            width = 1,
+            height = 1
+          },
+          flow_sprite =
+          {
+            filename = "__angelsaddons-storage__/graphics/entity/blank.png",
+            priority = "extra-high",
+            width = 1,
+            height = 1
+          },
+          gas_flow =
+          {
+            filename = "__angelsaddons-storage__/graphics/entity/blank.png",
+            priority = "extra-high",
+            width = 1,
+            height = 1,
+            frame_count = 1,
+            axially_symmetrical = false,
+            direction_count = 1,
+            animation_speed = 0.25,
+            hr_version =
+            {
+              filename = "__angelsaddons-storage__/graphics/entity/blank.png",
+              priority = "extra-high",
+              width = 1,
+              height = 1,
+              frame_count = 1,
+              axially_symmetrical = false,
+              direction_count = 1,
+              animation_speed = 0.25,
+            }
+          }
+        },
+        flow_length_in_ticks = 360,
+        vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+        working_sound =
+        {
+          sound = {
+            filename = "__base__/sound/storage-tank.ogg",
+            volume = 0.8
+          },
+          apparent_volume = 1.5,
+          max_sounds_per_type = 3
+        },
+        circuit_wire_connection_points = circuit_connector_definitions["storage-tank"].points,
+        circuit_connector_sprites = circuit_connector_definitions["storage-tank"].sprites,
+        circuit_wire_max_distance = default_circuit_wire_max_distance
+      }
+    }
+  )
+end
 if angelsmods.addons.storage.pressuretank then
   data:extend(
     {
---
+++ angelsaddons-storage/settings-updates.lua
@@ -0,0 +1,12 @@
+local function hide_setting(setting_type, setting_name, setting_default)
+  if data.raw[setting_type] and data.raw[setting_type][setting_name] then
+    data.raw[setting_type][setting_name].hidden = true
+    if setting_default ~= nil then
+      data.raw[setting_type][setting_name].default_value = setting_default
+    end
+  end
+end
+
+if mods["angelspetrochem"] then
+  hide_setting("bool-setting", "angels-enable-inline-tank")
+end
