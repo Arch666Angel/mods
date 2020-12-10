eb39d9e28b0eeb9e19aad6b996aef7d7cd07c282
lovely_santa <laurens.scheldeman@gmail.com>
2020-12-10 15:23:27 +0100

    some error fixes

--- angelsrefining/migrations/angelsrefining_0.11.20.lua
+++ angelsrefining/migrations/angelsrefining_0.11.20.lua
@@ -1,6 +1,6 @@
 for _, force in pairs(game.forces) do
   if force.technologies["geode-crystallization-2"] and force.technologies["geode-processing-3"] then
-    force.technologies["geode-crystallization-2"].researched = force.technologies["geode-processing-3"].researched
+    force.technologies["geode-processing-3"].researched = force.technologies["geode-crystallization-2"].researched
     force.technologies["geode-crystallization-2"].researched = false
   end
 
