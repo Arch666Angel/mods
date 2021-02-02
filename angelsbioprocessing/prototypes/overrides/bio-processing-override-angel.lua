5829cc1786062d7f892a05f982c23b7a8af02dda
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2021-02-02 22:12:34 +1100

    Added a recipe/tech-unlock for the bio-tile

--- angelsbioprocessing/prototypes/overrides/bio-processing-override-angel.lua
+++ angelsbioprocessing/prototypes/overrides/bio-processing-override-angel.lua
@@ -3,7 +3,7 @@ if angelsmods.industries and angelsmods.industries.overhaul then
 
   OV.modify_output("algae-brown-burning", {"solid-lithium", 1})
   -- OV.remove_unlock("bio-paper-1", "circuit-paper-board")
-
+  OV.modify_input("bio-tile",{type = "item", name = "solid-glass-mixture", amount = 1})
   -- MODULES
   OV.modify_input("speed-module", {"crystal-splinter-blue"})
   OV.modify_input("speed-module-2", {"crystal-shard-blue"})
@@ -28,7 +28,10 @@ if angelsmods.industries and angelsmods.industries.overhaul then
   OV.modify_input("beacon", {"crystal-full-harmonic"})
   OV.add_prereq("effect-transmission", "bio-processing-crystal-full")
 elseif mods["bobplates"] then
+  OV.modify_input("bio-tile",{type = "item", name = "solid-glass-mixture", amount = 1})
   -- nothing to change
 else
+  OV.modify_input("bio-tile",{type = "item", name = "steel-plate", amount = 1})
   OV.disable_recipe("algae-brown-burning")
 end
+OV.add_unlock("bio-farm-2","bio-tile")
