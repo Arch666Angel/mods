bc6239146708cec1da0f70b70af45e086572b0fe
lovely_santa <laurens.scheldeman@gmail.com>
2021-02-16 22:26:40 +0100

    Tweaked the lib to show icons next to the autoplace controls

--- angelspetrochem/prototypes/generation/angels-natural-gas.lua
+++ angelspetrochem/prototypes/generation/angels-natural-gas.lua
@@ -17,9 +17,9 @@ angelsmods.functions.add_resource(
     output_name = "gas-natural-1",
     output_min = 20,
     output_max = 20,
-    icon = "__base__/graphics/icons/crude-oil-resource.png",
+    icon = "__angelspetrochem__/graphics/icons/gas.png",
     icon_size = 64,
-    icon_mipmaps = 4,
+    icon_mipmaps = 1,
     stage_counts = {0},
     autoplace = {
       starting_area = false,
--- angelsrefining/prototypes/generation/angels-fissure.lua
+++ angelsrefining/prototypes/generation/angels-fissure.lua
@@ -7,6 +7,7 @@ data:extend(
     {
       type = "autoplace-control",
       name = "angels-fissure",
+      localised_name = {"", "[entity=angels-fissure] ", {"entity-name.angels-fissure"}},
       richness = true,
       order = "b-e",
       category = "resource"
@@ -14,9 +15,9 @@ data:extend(
     {
       type = "resource",
       name = "angels-fissure",
-      icon = "__base__/graphics/icons/crude-oil-resource.png",
+      icon = "__angelsrefining__/graphics/icons/fissure.png",
       icon_size = 64,
-      icon_mipmaps = 4,
+      icon_mipmaps = 1,
       flags = {"placeable-neutral"},
       category = "angels-fissure",
       order = "a-b-a",
