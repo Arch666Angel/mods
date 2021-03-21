c49063eb37845d2eff37729db3dd9f1881aa5215
Anton Sakharov <ya.antonsakharov@yandex.ru>
2021-03-21 21:18:33 +0500

    Disable resins, rubber(s). Cement w/o resins

--- angelspetrochem/prototypes/override/bobelectronics.lua
+++ angelspetrochem/prototypes/override/bobelectronics.lua
@@ -1,5 +1,14 @@
 local OV = angelsmods.functions.OV
 
 if not mods["bobelectronics"] then
+	--disable empty resin technologies
 	OV.disable_technology({"resin-1", "resin-2", "resin-3"})
+	--disable resins and remove from cement-processing-2
+	OV.disable_technology("resins")
+	OV.remove_prereq("angels-stone-smelting-2","resins")
+	angelsmods.functions.disable_barreling_recipes("liquid-resin")	
+	----
+	--rubbers
+	OV.disable_technology({"rubber", "rubbers"})
+	angelsmods.functions.disable_barreling_recipes("liquid-rubber")
 end
