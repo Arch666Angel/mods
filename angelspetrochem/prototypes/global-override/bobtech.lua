0645e18b2073c4c9adf1a29d03f23da1bf6857f6
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-03-21 01:39:03 -0400

    removed electrolysis 1 prereq from fluid control as electrolysis 1 is disabled

--- angelspetrochem/prototypes/global-override/bobtech.lua
+++ angelspetrochem/prototypes/global-override/bobtech.lua
@@ -7,7 +7,4 @@ if mods["bobtech"] and settings.startup["bobmods-burnerphase"].value then
   OV.add_prereq("basic-chemistry", "ore-crushing")
 
   OV.add_prereq("angels-fluid-control", "steel-processing")
-  if mods["bobplates"] then
-    OV.add_prereq("angels-fluid-control", "electrolysis-1")
-  end
 end
