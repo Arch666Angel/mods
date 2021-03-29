2171391908d14f37bfae533e83a41670db3e13f2
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2021-03-29 23:19:18 +1100

    setting tweak patch 2

--- angelssmelting/settings-updates.lua
+++ angelssmelting/settings-updates.lua
@@ -1,4 +1,3 @@
 if mods["bobplates"] then
-  data.raw["bool-setting"]["bobmods-plates-convert-recipes"].default_value=false
-  angelsmods.settings.hide_setting("bool-setting", "bobmods-plates-convert-recipes")
+  angelsmods.settings.hide_setting("bool-setting", "bobmods-plates-convert-recipes", false)
 end
