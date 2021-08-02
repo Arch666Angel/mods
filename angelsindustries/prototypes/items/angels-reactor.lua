1a532d3f147a8472ce02221bd271d88f28d2a6d3
Kira <35714562+kirazy@users.noreply.github.com>
2021-08-02 06:28:38 -0700

    Adjust reactor dimensions and provide completed sprites

--- angelsindustries/prototypes/items/angels-reactor.lua
+++ angelsindustries/prototypes/items/angels-reactor.lua
@@ -2,14 +2,14 @@
 if angelsmods.industries.overhaul then
     data:extend({
         {
-            type = 'item',
-            name = 'angels-burner-reactor',
-            icon = '__base__/graphics/icons/nuclear-reactor.png',
+            type = "item",
+            name = "angels-burner-reactor",
+            icon = "__angelsindustries__/graphics/icons/fast-reactor-icon.png",
             icon_size = 64,
             icon_mipmaps = 4,
-            subgroup = 'angels-power-nuclear',
-            order = 'a[reactor]-b',
-            place_result = 'angels-burner-reactor',
+            subgroup = "angels-power-nuclear",
+            order = "a[reactor]-b",
+            place_result = "angels-burner-reactor",
             stack_size = 10
         }
     })
