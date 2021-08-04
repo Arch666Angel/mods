d62a031f380126bac1655e9d6d5e925749f871b5
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2021-08-04 13:26:29 +1000

    RB error failed with bobs

--- angelsindustries/prototypes/recipes/angels-reactor.lua
+++ angelsindustries/prototypes/recipes/angels-reactor.lua
@@ -1,4 +1,6 @@
 local OV = angelsmods.functions.OV
+local buildingmulti = angelsmods.marathon.buildingmulti
+local buildingtime = angelsmods.marathon.buildingtime
 
 if angelsmods.industries.overhaul then
     angelsmods.functions.RB.build({
@@ -16,7 +18,19 @@ if angelsmods.industries.overhaul then
                     {type = 'item', name = 'angels-plate-lead', amount = 350}
                 },
                 result = 'angels-burner-reactor'
-            }
+            },
+            expensive = {
+                energy_required = 8 * (buildingtime),
+                enabled = false,
+                ingredients = {
+                    {type = 'item', name = 't4-brick', amount = 100 * buildingmulti},
+                    {type = 'item', name = 't4-plate', amount = 100 * buildingmulti},
+                    {type = 'item', name = 't4-circuit', amount = 500 * buildingmulti},
+                    {type = 'item', name = 't5-plate', amount = 100 * buildingmulti},
+                    {type = 'item', name = 'angels-plate-lead', amount = 350 * buildingmulti}
+                },
+                result = 'angels-burner-reactor'
+            },
         }
     })
 
