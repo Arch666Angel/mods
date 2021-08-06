6568f6ee28843ab5fd3ca4d9d97d1d07d837f290
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2021-08-06 10:04:25 +1000

    made heavy water electrolysis temp dependant

--- angelspetrochem/prototypes/recipes/petrochem-basics.lua
+++ angelspetrochem/prototypes/recipes/petrochem-basics.lua
@@ -868,7 +868,7 @@ data:extend(
       energy_required = 4,
       enabled = false,
       ingredients = {
-        {type = "fluid", name = "liquid-water-heavy", amount = 100}
+        {type = "fluid", name = "liquid-water-heavy", amount = 100, maximum_temperature = 30}
       },
       results = {
         {type = "fluid", name = "gas-oxygen", amount = 40},
