Arch666Angel/mods

commit cb196ac4502d17cf503695bd75093c807b858017

battery 3 spec vanila fix

angelsindustries/prototypes/overrides/components-base-recipe-update.lua
@@ -31,6 +31,7 @@ if angelsmods.industries.components then
   AI.replace_recipe_ing("battery-equipment","battery-1","battery-3")
   OV.remove_prereq("battery-equipment", "battery")
   OV.add_prereq("battery-equipment", "angels-components-batteries-3")
+  table.insert(data.raw.technology["battery-equipment"].unit.ingredients,{type = "item", name = "chemical-science-pack", amount = 1})
   AI.replace_recipe_ing("battery-mk2-equipment","battery-equipment","battery-4")
   OV.add_prereq("battery-mk2-equipment", "angels-components-batteries-4")
 

angelsindustries/prototypes/technology/components-batteries-technology.lua
@@ -147,6 +147,7 @@ if angelsmods.industries.components then
           ingredients = {
             {type = "item", name = "automation-science-pack", amount = 1},
             {type = "item", name = "logistic-science-pack", amount = 1},
+            {type = "item", name = "chemical-science-pack", amount = 1},
           },
           time = 60
         },

commit 1af822a3028d944386233b22a60face5ac724b9c

fixed unit test bugs introduced in https://github.com/Arch666Angel/mods/commit/cb196ac4502d17cf503695bd75093c807b858017

angelsindustries/prototypes/overrides/components-base-recipe-update.lua
@@ -31,7 +31,7 @@ if angelsmods.industries.components then
   AI.replace_recipe_ing("battery-equipment","battery-1","battery-3")
   OV.remove_prereq("battery-equipment", "battery")
   OV.add_prereq("battery-equipment", "angels-components-batteries-3")
-  table.insert(data.raw.technology["battery-equipment"].unit.ingredients,{type = "item", name = "chemical-science-pack", amount = 1})
+  OV.set_science_pack("battery-equipment", "chemical-science-pack", 1)
   AI.replace_recipe_ing("battery-mk2-equipment","battery-equipment","battery-4")
   OV.add_prereq("battery-mk2-equipment", "angels-components-batteries-4")
 

angelsindustries/prototypes/overrides/global-tech-base-packs.lua
@@ -98,7 +98,7 @@ if angelsmods.industries.tech then
   AI.pack_replace("electric-energy-accumulators", "green", "orange")
   AI.pack_replace("energy-shield-equipment", "green", "orange")
   AI.pack_replace("night-vision-equipment", "green", "orange")
-  AI.pack_replace("battery-equipment", "green", "orange")
+  AI.pack_replace("battery-equipment", "blue", "orange")
   AI.pack_replace("solar-panel-equipment", "green", "orange")
   AI.pack_replace("sulfur-processing", "green", "orange")
   --AI.pack_replace("plastics","green","orange")
@@ -176,7 +176,7 @@ if angelsmods.industries.tech then
   AI.pack_replace("angels-components-cabling-4", "blue", "orange")
   AI.pack_replace("cargo-robots-2", "green", "orange")
   AI.pack_replace("angels-construction-robots-2", "green", "orange")
-  AI.pack_replace("angels-components-batteries-3", "green", "orange")
+  AI.pack_replace("angels-components-batteries-3", "blue", "orange")
 
   -------------------------------------------------------------------------------
   -- BLUE SCIENCE PACKS ---------------------------------------------------------