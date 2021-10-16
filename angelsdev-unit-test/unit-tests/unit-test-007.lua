18e6dbd482350f8e1f12cc50133edc9423409517
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2021-10-17 08:39:33 +1300

    Updating variable names for clarity #709

--- angelsdev-unit-test/unit-tests/unit-test-007.lua
+++ angelsdev-unit-test/unit-tests/unit-test-007.lua
@@ -6,25 +6,26 @@ local unit_test_result = unit_test_functions.test_successful
 local unit_test_007 = function()
   local unit_test_result = unit_test_functions.test_successful
 
-  local filters = {}
-  table.insert(filters, {filter = "tool", invert = true, mode = "and"})
-  table.insert(filters, {filter = "selection-tool", invert = true, mode = "and"})
-  table.insert(filters, {filter = "fuel", invert = true, mode = "and", flag = "hidden"})
-  table.insert(filters, {filter = "place-result", invert = true, mode = "and"})
-  table.insert(filters, {filter = "place-as-tile", invert = true, mode = "and"})
-  table.insert(filters, {filter = "placed-as-equipment-result", invert = true, mode = "and"})
-  table.insert(filters, {filter = "flag", invert = true, mode = "and", flag = "hidden"})
-  table.insert(filters, {filter = "type", invert = false, mode = "and", type = "item"})
-  table.insert(filters, {filter = "has-rocket-launch-products", invert = true, mode = "and"})
-  table.insert(filters, {filter = "wire-count", invert = false, mode = "and", comparison = "=", value = 0})
+  local item_filters = {}
+  table.insert(item_filters, {filter = "tool", invert = true, mode = "and"})
+  table.insert(item_filters, {filter = "selection-tool", invert = true, mode = "and"})
+  table.insert(item_filters, {filter = "fuel", invert = true, mode = "and", flag = "hidden"})
+  table.insert(item_filters, {filter = "place-result", invert = true, mode = "and"})
+  table.insert(item_filters, {filter = "place-as-tile", invert = true, mode = "and"})
+  table.insert(item_filters, {filter = "placed-as-equipment-result", invert = true, mode = "and"})
+  table.insert(item_filters, {filter = "flag", invert = true, mode = "and", flag = "hidden"})
+  table.insert(item_filters, {filter = "type", invert = false, mode = "and", type = "item"})
+  table.insert(item_filters, {filter = "has-rocket-launch-products", invert = true, mode = "and"})
+  table.insert(item_filters, {filter = "wire-count", invert = false, mode = "and", comparison = "=", value = 0})
 
-  local prototypes = game.get_filtered_item_prototypes(filters)
-  for item_name, item in pairs(prototypes) do
-    filters = {}
-    table.insert(filters, {filter = "hidden", invert = true, mode = "and"})
-    table.insert(filters, {filter = "has-ingredient-item", invert = false, mode = "and", elem_filters = {{filter = "name", name = item_name}}})
-    prototypes = game.get_filtered_recipe_prototypes(filters)
-    if #prototypes == 0 then
+  local item_prototypes = game.get_filtered_item_prototypes(item_filters)
+
+  for item_name, item in pairs(item_prototypes) do
+    local recipe_filters = {}
+    table.insert(recipe_filters, {filter = "hidden", invert = true, mode = "and"})
+    table.insert(recipe_filters, {filter = "has-ingredient-item", invert = false, mode = "and", elem_filters = {{filter = "name", name = item_name}}})
+    local recipe_prototypes = game.get_filtered_recipe_prototypes(recipe_filters)
+    if #recipe_prototypes == 0 then
       unit_test_functions.print_msg(string.format("Item %q is unused.", item_name))
       unit_test_result = unit_test_functions.test_failed
     end
