cf545954649941e417dfcf6716967e19e57fa934
lovely_santa <laurens.scheldeman@gmail.com>
2021-10-23 12:16:06 +0200

    Added HQ graphics to the petrochem gas and oil tank

--- angelspetrochem/prototypes/buildings/storage-tanks.lua
+++ angelspetrochem/prototypes/buildings/storage-tanks.lua
@@ -1,10 +1,13 @@
 data:extend(
   {
+    ---------------------------------------------------------------------------
+    -- GAS TANK ---------------------------------------------------------------
+    ---------------------------------------------------------------------------
     {
       type = "item",
       name = "angels-storage-tank-1",
-      icon = "__angelspetrochem__/graphics/icons/storage-tank-1.png",
-      icon_size = 32,
+      icon = "__angelspetrochem__/graphics/icons/petrochem-gas-tank.png",
+      icon_size = 64, icon_mipmaps = 4,
       subgroup = "angels-fluid-tanks",
       order = "b[angels-storage-tank-1]",
       place_result = "angels-storage-tank-1",
@@ -13,14 +16,15 @@ data:extend(
     {
       type = "storage-tank",
       name = "angels-storage-tank-1",
-      icon = "__angelspetrochem__/graphics/icons/storage-tank-1.png",
-      icon_size = 32,
-      flags = {"placeable-player", "player-creation"},
+      icon = "__angelspetrochem__/graphics/icons/petrochem-gas-tank.png",
+      icon_size = 64, icon_mipmaps = 4,
+      flags = {"placeable-player", "player-creation", "not-rotatable"},
       minable = {mining_time = 3, result = "angels-storage-tank-1"},
       max_health = 500,
       corpse = "medium-remnants",
       collision_box = {{-2.3, -2.3}, {2.3, 2.3}},
       selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
+      drawing_box = {{-2.5, -3.25}, {2.5, 2.5}},
       two_direction_only = false,
       fluid_box =
       {
@@ -39,14 +43,44 @@ data:extend(
       {
         picture =
         {
-          sheet =
+          sheets =
           {
-            filename = "__angelspetrochem__/graphics/entity/storage-tanks/storage-tank-1.png",
-            priority = "extra-high",
-            frames = 1,
-            width = 224,
-            height = 224,
-            shift = {0, 0}
+            {
+              filename = "__angelspetrochem__/graphics/entity/petrochem-gas-tank/petrochem-gas-tank.png",
+              priority = "extra-high",
+              frames = 1,
+              width = 167,
+              height = 192,
+              shift = util.by_pixel(-1, -7),
+              hr_version = angelsmods.trigger.enable_hq_graphics and {
+                filename = "__angelspetrochem__/graphics/entity/petrochem-gas-tank/hr-petrochem-gas-tank.png",
+                priority = "extra-high",
+                frames = 1,
+                width = 334,
+                height = 387,
+                shift = util.by_pixel(-0.5, -6),
+                scale = 0.5
+              } or nil
+            },
+            {
+              filename = "__angelspetrochem__/graphics/entity/petrochem-gas-tank/petrochem-gas-tank-shadow.png",
+              priority = "extra-high",
+              frames = 1,
+              width = 220,
+              height = 120,
+              shift = util.by_pixel(26, 31),
+              draw_as_shadow = true,
+              hr_version = angelsmods.trigger.enable_hq_graphics and {
+                filename = "__angelspetrochem__/graphics/entity/petrochem-gas-tank/hr-petrochem-gas-tank-shadow.png",
+                priority = "extra-high",
+                frames = 1,
+                width = 437,
+                height = 237,
+                shift = util.by_pixel(26, 32),
+                draw_as_shadow = true,
+                scale = 0.5
+              } or nil
+            }
           }
         },
         fluid_background =
@@ -61,7 +95,14 @@ data:extend(
           filename = "__base__/graphics/entity/storage-tank/window-background.png",
           priority = "extra-high",
           width = 17,
-          height = 24
+          height = 24,
+          hr_version = angelsmods.trigger.enable_hq_graphics and {
+            filename = "__base__/graphics/entity/storage-tank/hr-window-background.png",
+            priority = "extra-high",
+            width = 34,
+            height = 48,
+            scale = 0.5
+          } or nil
         },
         flow_sprite =
         {
@@ -81,8 +122,7 @@ data:extend(
           axially_symmetrical = false,
           direction_count = 1,
           animation_speed = 0.25,
-          hr_version =
-          {
+          hr_version = angelsmods.trigger.enable_hq_graphics and {
             filename = "__base__/graphics/entity/pipe/hr-steam.png",
             priority = "extra-high",
             line_length = 10,
@@ -91,47 +131,53 @@ data:extend(
             frame_count = 60,
             axially_symmetrical = false,
             animation_speed = 0.25,
-            direction_count = 1
-          }
-        }
+            direction_count = 1,
+            scale = 0.5
+          } or nil
+        },
       },
       flow_length_in_ticks = 360,
       vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
       working_sound =
       {
-        sound = {
-          filename = "__base__/sound/storage-tank.ogg",
-          volume = 0.8
+        sound =
+        {
+            filename = "__base__/sound/storage-tank.ogg",
+            volume = 0.6
         },
-        apparent_volume = 1.5,
+        match_volume_to_activity = true,
+        audible_distance_modifier = 0.5,
         max_sounds_per_type = 3
       },
       circuit_wire_connection_points = circuit_connector_definitions["storage-tank"].points,
       circuit_connector_sprites = circuit_connector_definitions["storage-tank"].sprites,
       circuit_wire_max_distance = default_circuit_wire_max_distance
     },
+    ---------------------------------------------------------------------------
+    -- OIL TANK ---------------------------------------------------------------
+    ---------------------------------------------------------------------------
     {
       type = "item",
       name = "angels-storage-tank-2",
-      icon = "__angelspetrochem__/graphics/icons/storage-tank-2.png",
-      icon_size = 32,
+      icon = "__angelspetrochem__/graphics/icons/petrochem-oil-tank.png",
+      icon_size = 64, icon_mipmaps = 4,
       subgroup = "angels-fluid-tanks",
       order = "c[angels-storage-tank-2]",
       place_result = "angels-storage-tank-2",
       stack_size = 10,
-    },  
+    },
     {
       type = "storage-tank",
       name = "angels-storage-tank-2",
-      icon = "__angelspetrochem__/graphics/icons/storage-tank-2.png",
-      icon_size = 32,
+      icon = "__angelspetrochem__/graphics/icons/petrochem-oil-tank.png",
+      icon_size = 64, icon_mipmaps = 4,
       flags = {"placeable-player", "player-creation"},
       minable = {mining_time = 3, result = "angels-storage-tank-2"},
       max_health = 500,
       corpse = "medium-remnants",
       collision_box = {{-1.9, -1.9}, {1.9, 1.9}},
       selection_box = {{-2, -2}, {2, 2}},
-      two_direction_only = false,
+      two_direction_only = true,
       fluid_box =
       {
         base_area = 800,
@@ -149,14 +195,44 @@ data:extend(
       {
         picture =
         {
-          sheet =
+          sheets =
           {
-            filename = "__angelspetrochem__/graphics/entity/storage-tanks/storage-tank-2.png",
-            priority = "extra-high",
-            frames = 2,
-            width = 192,
-            height = 192,
-            shift = {0, 0}
+            {
+              filename = "__angelspetrochem__/graphics/entity/petrochem-oil-tank/petrochem-oil-tank.png",
+              priority = "extra-high",
+              frames = 2,
+              width = 135,
+              height = 154,
+              shift = util.by_pixel(-1, -1),
+              hr_version = angelsmods.trigger.enable_hq_graphics and {
+                filename = "__angelspetrochem__/graphics/entity/petrochem-oil-tank/hr-petrochem-oil-tank.png",
+                priority = "extra-high",
+                frames = 2,
+                width = 273,
+                height = 307,
+                shift = util.by_pixel(0, -2),
+                scale = 0.5
+              } or nil
+            },
+            {
+              filename = "__angelspetrochem__/graphics/entity/petrochem-oil-tank/petrochem-oil-tank-shadow.png",
+              priority = "extra-high",
+              frames = 2,
+              width = 167,
+              height = 165,
+              shift = util.by_pixel(15, 8.5),
+              draw_as_shadow = true,
+              hr_version = angelsmods.trigger.enable_hq_graphics and {
+                filename = "__angelspetrochem__/graphics/entity/petrochem-oil-tank/hr-petrochem-oil-tank-shadow.png",
+                priority = "extra-high",
+                frames = 2,
+                width = 335,
+                height = 328,
+                shift = util.by_pixel(16.5, 9.5),
+                draw_as_shadow = true,
+                scale = 0.5
+              } or nil
+            }
           }
         },
         fluid_background =
@@ -171,7 +247,14 @@ data:extend(
           filename = "__base__/graphics/entity/storage-tank/window-background.png",
           priority = "extra-high",
           width = 17,
-          height = 24
+          height = 24,
+          hr_version = angelsmods.trigger.enable_hq_graphics and {
+            filename = "__base__/graphics/entity/storage-tank/hr-window-background.png",
+            priority = "extra-high",
+            width = 34,
+            height = 48,
+            scale = 0.5
+          } or nil
         },
         flow_sprite =
         {
@@ -191,8 +274,7 @@ data:extend(
           axially_symmetrical = false,
           direction_count = 1,
           animation_speed = 0.25,
-          hr_version =
-          {
+          hr_version = angelsmods.trigger.enable_hq_graphics and {
             filename = "__base__/graphics/entity/pipe/hr-steam.png",
             priority = "extra-high",
             line_length = 10,
@@ -201,9 +283,10 @@ data:extend(
             frame_count = 60,
             axially_symmetrical = false,
             animation_speed = 0.25,
-            direction_count = 1
-          }
-        }
+            direction_count = 1,
+            scale = 0.5
+          } or nil
+        },
       },
       flow_length_in_ticks = 360,
       vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
@@ -220,6 +303,9 @@ data:extend(
       circuit_connector_sprites = circuit_connector_definitions["storage-tank"].sprites,
       circuit_wire_max_distance = default_circuit_wire_max_distance
     },
+    ---------------------------------------------------------------------------
+    -- SMALL INLINE TANK ------------------------------------------------------
+    ---------------------------------------------------------------------------
     {
       type = "item",
       name = "angels-storage-tank-3",
