16078894968d0c5ebdbfa5f33f69c1afe03fcd97
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2022-02-16 23:11:51 +1100

    remove logs

--- angelspetrochem/data-updates.lua
+++ angelspetrochem/data-updates.lua
@@ -3,5 +3,3 @@ require("prototypes.petrochem-override")
 
 -- EXECUTE OVERRIDES
 angelsmods.functions.OV.execute()
-log(serpent.block(data.raw.fluid["gas-vinyl-chloride"].icons))
-log(serpent.block(data.raw.fluid["liquid-plastic"].icons))
