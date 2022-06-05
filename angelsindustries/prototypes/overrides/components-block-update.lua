e8ba997294a05f9d402ee86fbb4555b4354efb25
maxreader <60635199+maxreader@users.noreply.github.com>
2022-06-05 05:43:02 -0500

    Steel smelting tier adjustments (#779, #780)
    Co-authored-by: lovely_santa <laurens.scheldeman@gmail.com>

--- angelsindustries/prototypes/overrides/components-block-update.lua
+++ angelsindustries/prototypes/overrides/components-block-update.lua
@@ -23,15 +23,6 @@ if angelsmods.industries.components then
   OV.remove_prereq("angels-components-mechanical-2", "angels-stone-smelting-2")
   OV.add_prereq("angels-components-mechanical-2", "angels-stone-smelting-1")
 
-  -- move steel smelting 1 from green to red science
-  OV.remove_science_pack("angels-steel-smelting-1", "logistic-science-pack")
-  OV.remove_prereq("angels-steel-smelting-1", "angels-metallurgy-2")
-  OV.add_prereq("angels-steel-smelting-1", "angels-metallurgy-1")
-  -- update depending technologies accordingly
-  if angelsmods.trigger.smelting_products["invar"].plate and mods["bobplates"] then
-    OV.add_prereq("angels-invar-smelting-1", "logistic-science-pack")
-  end
-
   -----------------------------------------------------------------------------
   -- ADD ELECTRONIC RECIPES TO ELECTRONIC ASSMBLY -----------------------------
   -----------------------------------------------------------------------------
