f81c098a7bc28435c4eca52b7eb98755485d654b
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-06-16 13:54:51 +1200

    Fix Science overhaul tech costs #674

--- angelsindustries/prototypes/overrides/global-tech-base-packs.lua
+++ angelsindustries/prototypes/overrides/global-tech-base-packs.lua
@@ -114,11 +114,9 @@ if angelsmods.industries.tech then
   AI.pack_replace("construction-robotics", "blue", "orange")
   AI.pack_replace("electric-energy-distribution-2", "blue", "orange")
   --REFINING
-  AI.pack_replace("water-treatment-3", "green", "orange")
   AI.pack_replace("ore-leaching", "blue", "orange")
   OV.remove_prereq("ore-leaching", "tech-blue-packs")
   AI.pack_replace("geode-processing-2", "green", "orange")
-  AI.pack_replace("advanced-ore-refining-2", "green", "orange")
   --SMELTING
   AI.pack_replace("angels-metallurgy-3", "blue", "orange")
   OV.remove_prereq("angels-metallurgy-3", "tech-blue-packs")
@@ -134,7 +132,7 @@ if angelsmods.industries.tech then
   AI.pack_replace("angels-gold-smelting-1", "blue", "orange")
   AI.pack_replace("angels-manganese-smelting-2", "blue", "orange")
   AI.pack_replace("angels-silver-smelting-2", "blue", "orange")
-  AI.pack_replace("angels-steel-smelting-2", "blue", "orange")
+  AI.pack_replace("angels-steel-smelting-2", "green", "orange")
   AI.pack_replace("angels-zinc-smelting-2", "blue", "orange")
   AI.pack_replace("angels-copper-smelting-3", "blue", "orange")
   AI.pack_replace("angels-lead-smelting-3", "blue", "orange")
@@ -155,14 +153,10 @@ if angelsmods.industries.tech then
   AI.pack_replace("bio-processing-crystal-splinter-3", "green", "orange")
   AI.pack_replace("bio-arboretum-2","green","orange")
   --PETROCHEM
-  AI.pack_replace("angels-advanced-chemistry-2", "green", "orange")
   AI.pack_replace("plastic-1", "green", "orange")
   --AI.pack_replace("resin-2", "blue","orange") -- needs melamine, which is quite deep in the tree, leave as blue
-  AI.pack_replace("rubber", "green", "orange")
   --AI.pack_replace("rocket-booster-1", "green", "orange")
   AI.pack_replace("rocket-explosives-1", "green", "orange")
-  AI.pack_replace("angels-advanced-chemistry-2", "green", "orange")
-  AI.pack_replace("angels-advanced-chemistry-2", "green", "orange")
   --INDUSTRIES
   AI.pack_replace("tech-blue-circuit", "blue", "orange")
   OV.remove_science_pack("tech-blue-circuit", "tech-blue-packs")
