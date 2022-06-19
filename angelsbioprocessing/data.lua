a6fecb7b8fa4c215454238bf424c62ae57ef1947
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2022-06-19 21:22:55 +1000

    tweaks to pass more spec_vanilla configs

--- angelsbioprocessing/data.lua
+++ angelsbioprocessing/data.lua
@@ -54,6 +54,7 @@ end
 angelsmods.triggers.bio_rubber = angelsmods.triggers.bio_rubber or angelsmods.trigger.rubber or false
 angelsmods.triggers.bio_resin = angelsmods.triggers.bio_resin or angelsmods.trigger.resin or false
 angelsmods.triggers.bio_plastic = angelsmods.triggers.bio_plastic or (angelsmods.trigger.plastic and (angelsmods.triggers.bio_resin or angelsmods.triggers.bio_rubber)) or false
+angelsmods.triggers.paper = not angelsmods.functions.is_special_vanilla()
 
 -- set triggers for other angel mods
 require("prototypes.bio-processing-triggers")
