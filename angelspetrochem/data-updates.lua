2f3f48365081b98a2365f56cf01e738efcccc952
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-07-03 14:22:23 +1200

    Fixed red waste water trigger #811

--- angelspetrochem/data-updates.lua
+++ angelspetrochem/data-updates.lua
@@ -1,3 +1,5 @@
+angelsmods.trigger.water_red_waste = angelsmods.trigger.enableacids and angelsmods.trigger.refinery_products["rubyte"]
+
 require("prototypes.petrochem-generate")
 require("prototypes.petrochem-override")
 
