9cdeeec65b4c55dace6e59b44eeff19d2d3f9413
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-07-04 18:02:31 +1200

    New Unit Test - Unlocking Hidden / Disabled recipes #814

--- angelsdev-unit-test/unit-tests.lua
+++ angelsdev-unit-test/unit-tests.lua
@@ -10,6 +10,7 @@ local unit_tests =
   require("unit-tests.unit-test-008"),
   require("unit-tests.unit-test-009"),
   require("unit-tests.unit-test-010"),
+  require("unit-tests.unit-test-011"),
 }
 
 local unit_test_functions = require("unit-test-functions")
