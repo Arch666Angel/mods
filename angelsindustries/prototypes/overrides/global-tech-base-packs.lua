44f3f1eea51f8485f1ba96539e0cd996387c5028
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2022-09-06 00:26:40 +1200

    Green science prerequisites - bio tokens
    Remove bio tokens from techs that are prerequisites for green science

--- angelsindustries/prototypes/overrides/global-tech-base-packs.lua
+++ angelsindustries/prototypes/overrides/global-tech-base-packs.lua
@@ -52,8 +52,10 @@ if angelsmods.industries.tech then
   AI.pack_count_update("bio-temperate-farming", "angels-science-pack-red", 4)
   AI.pack_replace("bio-fermentation", "green", "red")
   OV.remove_prereq("bio-fermentation", "tech-green-packs")
+  OV.remove_science_pack("bio-fermentation", "token-bio")
   AI.pack_replace("bio-arboretum-temperate-1", "green", "red")
   OV.remove_prereq("bio-arboretum-temperate-1", "resins")
+  OV.remove_science_pack("bio-arboretum-temperate-1", "token-bio")
   OV.add_prereq("bio-arboretum-temperate-2", "resins")
   -- INDUSTRIES
   AI.pack_replace("tech-green-circuit", "green", "red")
