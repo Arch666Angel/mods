5bc5a3c884ec8e99449bd0ed9f9aaf576379a149
Duchesko <atburghardt@gmail.com>
2022-11-04 22:32:25 +0100

    use migration-functions

--- angelspetrochem/migrations/angelspetrochem_0.9.23.lua
+++ angelspetrochem/migrations/angelspetrochem_0.9.23.lua
@@ -1,11 +1,11 @@
+local angelsmods = require("__angelsrefining__/prototypes/migration-functions")
+
 for _, surface in pairs(game.surfaces) do
-  for _, entity in pairs(surface.find_entities_filtered({ name = { "angels-air-filter", "angels-air-filter-2" } })) do
-    entity.surface.create_entity({
-      name = entity.name == "angels-air-filter" and "angels-air-filter-2" or "angels-air-filter-3",
-      position = entity.position,
-      force = entity.force,
-      fast_replace = true,
-      spill = false,
-    })
-  end
+  angelsmods.migration.replace_entity(
+    surface.find_entities_filtered({ name = { "angels-air-filter", "angels-air-filter-2" } }),
+    {
+      ["angels-air-filter"] = "angels-air-filter-2",
+      ["angels-air-filter-2"] = "angels-air-filter-3",
+    }
+  )
 end
