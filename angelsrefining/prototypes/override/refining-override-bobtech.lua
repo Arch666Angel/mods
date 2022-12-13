1b21a5dde7a52d1f4f4865ccec006e5ebfb1fa81
lovely_santa <laurens.scheldeman@gmail.com>
2022-12-13 19:54:58 +0100

    Added support for Bob's steam power phase (#864)

--- angelsrefining/prototypes/override/refining-override-bobtech.lua
+++ angelsrefining/prototypes/override/refining-override-bobtech.lua
@@ -6,4 +6,7 @@ local OV = angelsmods.functions.OV
 if mods["bobtech"] and settings.startup["bobmods-burnerphase"].value then
   OV.add_prereq("ore-crushing", "automation-science-pack")
   OV.add_prereq("ore-crushing", "electricity")
+
+  OV.add_prereq("water-treatment", "steam-power")
+  OV.add_prereq("water-treatment", "electricity")
 end
