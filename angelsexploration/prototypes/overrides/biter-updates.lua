9169ada40a20e97acfdf2f8cc9e42aa7b8c3989f
lovely_santa <laurens.scheldeman@gmail.com>
2023-01-15 11:30:15 +0100

    updated biters to account for the super spawner

--- angelsexploration/prototypes/overrides/biter-updates.lua
+++ angelsexploration/prototypes/overrides/biter-updates.lua
@@ -2,40 +2,55 @@
 if mods["bobenemies"] then
   -- update the earliest spawn point of spitter on bobs spawner to the same evolution point of the base game spawner (with a small correction factor)
   local base_spawner = data.raw["unit-spawner"]["spitter-spawner"]
-  local bob_spawner = data.raw["unit-spawner"]["bob-spitter-spawner"]
-  if base_spawner and bob_spawner then
-    for unit, unit_correction in pairs({
-      ["small-spitter"] = 1 / 100000000,
-
-      ["bob-big-electric-spitter"] = 0.02,
-      ["bob-huge-acid-spitter"] = 0.02,
-      ["bob-huge-explosive-spitter"] = 0.02,
-    }) do
-      for bob_spawn_idx, bob_spawn_data in pairs(bob_spawner.result_units) do
-        if (bob_spawn_data.unit or bob_spawn_data[1]) == unit then
-          local first_spawn = true
-          for bob_point_idx, bob_point_data in pairs(bob_spawn_data.spawn_points or bob_spawn_data[2]) do
-            if first_spawn and (bob_point_data.spawn_weight or bob_point_data[2]) > 0 then
-              for base_spawn_idx, base_spawn_data in pairs(base_spawner.result_units) do
-                if (base_spawn_data.unit or base_spawn_data[1]) == unit then
-                  for base_point_idx, base_point_data in pairs(base_spawn_data.spawn_points or base_spawn_data[2]) do
-                    if first_spawn and (base_point_data.spawn_weight or base_point_data[2]) > 0 then
-                      local base_point = (base_spawn_data.spawn_points or base_spawn_data[2])[base_point_idx > 1 and (base_point_idx - 1) or 1]
-                      bob_spawner.result_units[bob_spawn_idx][bob_spawn_data.spawn_points and "spawn_points" or 2][bob_point_idx > 1 and (bob_point_idx - 1) or 1][bob_point_data.evolution_factor and "evolution_factor" or 1] = base_point[base_point.evolution_factor and "evolution_factor" or 1]
-                        + unit_correction -- marginally larger point (required for tips and tricks 1-2-x behaviour)
-                      first_spawn = false
+  for _, bob_spawner in pairs({
+    data.raw["unit-spawner"]["bob-spitter-spawner"],
+    data.raw["unit-spawner"]["bob-super-spawner"]
+  }) do
+    if base_spawner and bob_spawner then
+      for unit, unit_correction in pairs({
+        ["small-spitter"] = 1 / 100000000,
+      }) do
+        for bob_spawn_idx, bob_spawn_data in pairs(bob_spawner.result_units) do
+          if (bob_spawn_data.unit or bob_spawn_data[1]) == unit then
+            local first_spawn = true
+            for bob_point_idx, bob_point_data in pairs(bob_spawn_data.spawn_points or bob_spawn_data[2]) do
+              if first_spawn and (bob_point_data.spawn_weight or bob_point_data[2]) > 0 then
+                for base_spawn_idx, base_spawn_data in pairs(base_spawner.result_units) do
+                  if (base_spawn_data.unit or base_spawn_data[1]) == unit then
+                    for base_point_idx, base_point_data in pairs(base_spawn_data.spawn_points or base_spawn_data[2]) do
+                      if first_spawn and (base_point_data.spawn_weight or base_point_data[2]) > 0 then
+                        local base_point = (base_spawn_data.spawn_points or base_spawn_data[2])[base_point_idx > 1 and (base_point_idx - 1) or 1]
+                        bob_spawner.result_units[bob_spawn_idx][bob_spawn_data.spawn_points and "spawn_points" or 2][bob_point_idx > 1 and (bob_point_idx - 1) or 1][bob_point_data.evolution_factor and "evolution_factor" or 1] = base_point[base_point.evolution_factor and "evolution_factor" or 1]
+                          + unit_correction -- marginally larger point (required for tips and tricks 1-2-x behaviour)
+                        first_spawn = false
+                      end
                     end
                   end
                 end
-              end
 
-              if first_spawn and unit_correction ~= 0 then -- not found in base spawner (only adding correction factor)
-                bob_spawner.result_units[bob_spawn_idx][bob_spawn_data.spawn_points and "spawn_points" or 2][bob_point_idx > 1 and (bob_point_idx - 1) or 1][bob_point_data.evolution_factor and "evolution_factor" or 1] = bob_spawner.result_units[bob_spawn_idx][bob_spawn_data.spawn_points and "spawn_points" or 2][bob_point_idx > 1 and (bob_point_idx - 1) or 1][bob_point_data.evolution_factor and "evolution_factor" or 1]
-                  + unit_correction
+                if first_spawn and unit_correction ~= 0 then -- not found in base spawner (only adding correction factor)
+                  bob_spawner.result_units[bob_spawn_idx][bob_spawn_data.spawn_points and "spawn_points" or 2][bob_point_idx > 1 and (bob_point_idx - 1) or 1][bob_point_data.evolution_factor and "evolution_factor" or 1] = bob_spawner.result_units[bob_spawn_idx][bob_spawn_data.spawn_points and "spawn_points" or 2][bob_point_idx > 1 and (bob_point_idx - 1) or 1][bob_point_data.evolution_factor and "evolution_factor" or 1]
+                    + unit_correction
+                end
               end
             end
           end
         end
+      end
+    end
+  end
+  
+  local super_spawner = data.raw["unit-spawner"]["bob-super-spawner"]
+  if super_spawner then
+    for _, super_spawner_data in pairs(super_spawner.result_units) do
+      local super_spawner_biter = super_spawner_data[1]
+      if super_spawner_biter == "bob-big-piercing-biter" then
+        table.remove(super_spawner_data[2], 1)
+        super_spawner_data[2][1][2] = 0
+      end
+      if super_spawner_biter == "bob-big-electric-spitter" then
+        table.remove(super_spawner_data[2], 1)
+        super_spawner_data[2][1][2] = 0
       end
     end
   end
