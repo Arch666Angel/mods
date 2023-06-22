936a064fddeced6ec9a0eabf36465aaa7b318553
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-06-23 00:28:35 +0100

    Fixing Unit Test Errors
    Synthetic wood recipe unlock was not getting moved if bobplates was not enabled. Recipe can be added by either bobplates or bobelectronics.

--- angelspetrochem/prototypes/override/bobelectronics.lua
+++ angelspetrochem/prototypes/override/bobelectronics.lua
@@ -3,4 +3,9 @@ local OV = angelsmods.functions.OV
 if mods["bobelectronics"] then
   OV.add_prereq("advanced-electronics", "chlorine-processing-1")
   OV.hide_recipe("bob-oil-processing")
+
+  if not mods["bobplates"] then
+    OV.remove_unlock("plastics", "synthetic-wood")
+    OV.add_unlock("plastic-1", "synthetic-wood")
+  end
 end
