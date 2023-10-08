44ab760d17f575d0636bf1b96d7369e94f94bce5
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2023-10-08 21:05:04 +1300

    Adjust for updated prereq

--- angelsbioprocessing/prototypes/overrides/bio-processing-override-arboretum.lua
+++ angelsbioprocessing/prototypes/overrides/bio-processing-override-arboretum.lua
@@ -115,7 +115,7 @@ else
       ingredients = { { name = "paste-cellulose", amount = "solid-wood-pulp" } },
     },
   })
-  --rereq clean-out
-  OV.remove_prereq("gardens-2", "bio-paper-1")
+  --prereq clean-out
+  OV.remove_prereq("gardens-3", "bio-paper-1")
   OV.remove_unlock("bio-processing-brown", "solid-alginic-acid")
 end
