Arch666Angel/mods

commit 7768f821cf19a61574375c5095ac18ce1edd5fc9

Remove references to result & result_count #994

angelsaddons-cab/prototypes/recipes/cab-updates.lua
@@ -13,7 +13,7 @@ if angelsmods.industries and angelsmods.industries.components then
         { type = "item", name = "circuit-orange-loaded", amount = 2 },
         { type = "item", name = "angels-crawler", amount = 1 },
       },
-      result = "angels-cab",
+      results = {{ type = "item" name = "angels-cab", amount = 1 }},
     },
   })
 end

angelsaddons-liquidrobot/prototypes/recipes/cab-updates.lua
@@ -11,6 +11,6 @@ angelsmods.functions.RB.build({
       { "construction-components", 20 },
       { "circuit-orange-loaded", 2 },
     },
-    result = "angels-cab",
+    results = {{ type = "item" name = "angels-cab", amount = 1, }},
   },
 })

angelsaddons-liquidrobot/prototypes/recipes/liquid-robot-recipes.lua
@@ -10,7 +10,7 @@ data:extend({
       { "engine-unit", 1 },
       { "electronic-circuit", 2 },
     },
-    result = "empty-liquid-bot",
+    results = {{ type = "item" name = "empty-liquid-bot", amount = 1, }},
   },
   {
     type = "recipe",
@@ -18,7 +18,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    result = "dummy-a",
+    results = {{ type = "item" name = "dummy-a", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "b",
     hide_from_stats = true,
@@ -46,7 +46,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    result = "dummy-b",
+    results = {{ type = "item" name = "dummy-b", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "d",
     hide_from_stats = true,
@@ -74,7 +74,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    result = "dummy-c",
+    results = {{ type = "item" name = "dummy-c", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "f",
     hide_from_stats = true,
@@ -102,7 +102,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    result = "dummy-d",
+    results = {{ type = "item" name = "dummy-d", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "h",
     hide_from_stats = true,

angelsaddons-mobility/prototypes/recipes/recipes.lua
@@ -21,7 +21,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 40, "+10", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
     },
-    result = "crawler-locomotive",
+    results = {{ type = "item" name = "crawler-locomotive", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -44,7 +44,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 35, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 40, "+20", "+20" } },
     },
-    result = "crawler-locomotive-wagon",
+    results = {{ type = "item" name = "crawler-locomotive-wagon", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -64,7 +64,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    result = "crawler-wagon",
+    results = {{ type = "item" name = "crawler-wagon", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -87,7 +87,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    result = "crawler-bot-wagon",
+    results = {{ type = "item" name = "crawler-bot-wagon", amount = 1, }},
   }, "angels-crawler-train")
 end
 
@@ -110,7 +110,7 @@ if angelsmods.addons.mobility.petrotrain.enabled then
       { "steel-plate", { 40, "+10", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
     },
-    result = "petro-locomotive-1",
+    results = {{ type = "item" name = "petro-locomotive-1", amount = 1, }},
   }, "angels-petro-train")
 
   funcs.generate_train_recipe({
@@ -129,7 +129,7 @@ if angelsmods.addons.mobility.petrotrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    result = "petro-tank1",
+    results = {{ type = "item" name = "petro-tank1", amount = 1, }},
   }, "angels-petro-train")
 
   funcs.generate_train_recipe({
@@ -148,7 +148,7 @@ if angelsmods.addons.mobility.petrotrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    result = "petro-tank2",
+    results = {{ type = "item" name = "petro-tank2", amount = 1, }},
   }, "angels-petro-train")
 end
 
@@ -171,7 +171,7 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
       { "steel-plate", { 40, "+10", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
     },
-    result = "smelting-locomotive-1",
+    results = {{ type = "item" name = "smelting-locomotive-1", amount = 1, }},
   }, "angels-smelting-train")
 
   funcs.generate_train_recipe({
@@ -194,7 +194,7 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
       { "steel-plate", { 35, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 40, "+20", "+20" } },
     },
-    result = "smelting-locomotive-tender",
+    results = {{ type = "item" name = "smelting-locomotive-tender", amound = 1, }},
   }, "angels-smelting-train")
 
   funcs.generate_train_recipe({
@@ -214,6 +214,6 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    result = "smelting-wagon-1",
+    results = {{ type = "item" name = "smelting-wagon-1", amount = 1, }},
   }, "angels-smelting-train")
 end

angelsaddons-mobility/prototypes/train-functions.lua
@@ -144,7 +144,7 @@ local function generate_train_recipe(item, add_unlock)
       copy.localised_name = { "", { "recipe-name." .. item.name }, " MK" .. i }
       copy.localised_description = { "recipe-description." .. item.name }
       copy.ingredients = ingredients
-      copy.result = name
+      copy.results = {{ type = "item", name = name, amount  = 1 }},
 
       if copy.order then
         copy.order = copy.order .. "-" .. i

angelsaddons-cab/prototypes/recipes/cab-recipe.lua
@@ -10,7 +10,7 @@ data:extend({
       { type = "item", name = "electronic-circuit", amount = 20 },
       { type = "item", name = "iron-gear-wheel", amount = 25 },
     },
-    result = "angels-cab",
+    results = {{ type = "item", name = "angels-cab", amount = 1 }},
   },
   {
     type = "recipe",
@@ -23,7 +23,7 @@ data:extend({
       { type = "item", name = "electronic-circuit", amount = 1 },
       { type = "item", name = "iron-gear-wheel", amount = 1 },
     },
-    result = "angels-cab-deploy-charge",
+    results = {{ type = "item", name = "angels-cab-deploy-charge", amount = 1 }},
   },
   {
     type = "recipe",
@@ -36,6 +36,6 @@ data:extend({
       { type = "item", name = "electronic-circuit", amount = 1 },
       { type = "item", name = "iron-gear-wheel", amount = 1 },
     },
-    result = "angels-cab-undeploy-charge",
+    results = {{ type = "item", name = "angels-cab-undeploy-charge", amount = 1 }},
   },
 })

angelsaddons-cab/prototypes/recipes/equipment-recipe.lua
@@ -12,7 +12,7 @@ data:extend({
       { type = "item", name = "green-wire", amount = 10 },
       { type = "item", name = "electronic-circuit", amount = 20 },
     },
-    result = string.format(energyInterfaceName, "-mk1"),
+    results = {{ type = "item", name = string.format(energyInterfaceName, "-mk1"), amount = 1, }},
   },
   --[[{
     type = "recipe",
@@ -22,6 +22,6 @@ data:extend({
     ingredients ={
       {string.format(energyInterfaceName, "-mk1"), 10},
     },
-    result = string.format(energyInterfaceName, "-mk2"),
+    results = {{ type = "item", name = string.format(energyInterfaceName, "-mk2"), amount = 1, }},
   },]]
 })

commit 12a465a7ee0af258512445eda10057fc50b15bed

fix: add missing comma
Regression in 7768f821cf19a61574375c5095ac18ce1edd5fc9

angelsaddons-cab/prototypes/recipes/cab-updates.lua
@@ -13,7 +13,7 @@ if angelsmods.industries and angelsmods.industries.components then
         { type = "item", name = "circuit-orange-loaded", amount = 2 },
         { type = "item", name = "angels-crawler", amount = 1 },
       },
-      results = {{ type = "item" name = "angels-cab", amount = 1 }},
+      results = {{ type = "item", name = "angels-cab", amount = 1 }},
     },
   })
 end

angelsaddons-liquidrobot/prototypes/recipes/cab-updates.lua
@@ -11,6 +11,6 @@ angelsmods.functions.RB.build({
       { "construction-components", 20 },
       { "circuit-orange-loaded", 2 },
     },
-    results = {{ type = "item" name = "angels-cab", amount = 1, }},
+    results = {{ type = "item", name = "angels-cab", amount = 1, }},
   },
 })

angelsaddons-liquidrobot/prototypes/recipes/liquid-robot-recipes.lua
@@ -10,7 +10,7 @@ data:extend({
       { "engine-unit", 1 },
       { "electronic-circuit", 2 },
     },
-    results = {{ type = "item" name = "empty-liquid-bot", amount = 1, }},
+    results = {{ type = "item", name = "empty-liquid-bot", amount = 1, }},
   },
   {
     type = "recipe",
@@ -18,7 +18,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-a", amount = 1, }},
+    results = {{ type = "item", name = "dummy-a", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "b",
     hide_from_stats = true,
@@ -46,7 +46,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-b", amount = 1, }},
+    results = {{ type = "item", name = "dummy-b", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "d",
     hide_from_stats = true,
@@ -74,7 +74,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-c", amount = 1, }},
+    results = {{ type = "item", name = "dummy-c", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "f",
     hide_from_stats = true,
@@ -102,7 +102,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-d", amount = 1, }},
+    results = {{ type = "item", name = "dummy-d", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "h",
     hide_from_stats = true,

angelsaddons-mobility/prototypes/recipes/recipes.lua
@@ -21,7 +21,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 40, "+10", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
     },
-    results = {{ type = "item" name = "crawler-locomotive", amount = 1, }},
+    results = {{ type = "item", name = "crawler-locomotive", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -44,7 +44,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 35, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 40, "+20", "+20" } },
     },
-    results = {{ type = "item" name = "crawler-locomotive-wagon", amount = 1, }},
+    results = {{ type = "item", name = "crawler-locomotive-wagon", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -64,7 +64,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "crawler-wagon", amount = 1, }},
+    results = {{ type = "item", name = "crawler-wagon", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -87,7 +87,7 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "crawler-bot-wagon", amount = 1, }},
+    results = {{ type = "item", name = "crawler-bot-wagon", amount = 1, }},
   }, "angels-crawler-train")
 end
 
@@ -110,7 +110,7 @@ if angelsmods.addons.mobility.petrotrain.enabled then
       { "steel-plate", { 40, "+10", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
     },
-    results = {{ type = "item" name = "petro-locomotive-1", amount = 1, }},
+    results = {{ type = "item", name = "petro-locomotive-1", amount = 1, }},
   }, "angels-petro-train")
 
   funcs.generate_train_recipe({
@@ -129,7 +129,7 @@ if angelsmods.addons.mobility.petrotrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "petro-tank1", amount = 1, }},
+    results = {{ type = "item", name = "petro-tank1", amount = 1, }},
   }, "angels-petro-train")
 
   funcs.generate_train_recipe({
@@ -148,7 +148,7 @@ if angelsmods.addons.mobility.petrotrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "petro-tank2", amount = 1, }},
+    results = {{ type = "item", name = "petro-tank2", amount = 1, }},
   }, "angels-petro-train")
 end
 
@@ -171,7 +171,7 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
       { "steel-plate", { 40, "+10", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
     },
-    results = {{ type = "item" name = "smelting-locomotive-1", amount = 1, }},
+    results = {{ type = "item", name = "smelting-locomotive-1", amount = 1, }},
   }, "angels-smelting-train")
 
   funcs.generate_train_recipe({
@@ -194,7 +194,7 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
       { "steel-plate", { 35, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 40, "+20", "+20" } },
     },
-    results = {{ type = "item" name = "smelting-locomotive-tender", amound = 1, }},
+    results = {{ type = "item", name = "smelting-locomotive-tender", amound = 1, }},
   }, "angels-smelting-train")
 
   funcs.generate_train_recipe({
@@ -214,6 +214,6 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
       { "steel-plate", { 20, "+5", 0, 0, 0 } },
       { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "smelting-wagon-1", amount = 1, }},
+    results = {{ type = "item", name = "smelting-wagon-1", amount = 1, }},
   }, "angels-smelting-train")
 end

angelsaddons-mobility/prototypes/train-functions.lua
@@ -144,7 +144,7 @@ local function generate_train_recipe(item, add_unlock)
       copy.localised_name = { "", { "recipe-name." .. item.name }, " MK" .. i }
       copy.localised_description = { "recipe-description." .. item.name }
       copy.ingredients = ingredients
-      copy.results = {{ type = "item", name = name, amount  = 1 }},
+      copy.results = {{ type = "item", name = name, amount  = 1 }}
 
       if copy.order then
         copy.order = copy.order .. "-" .. i