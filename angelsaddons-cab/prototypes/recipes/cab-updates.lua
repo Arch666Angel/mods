Arch666Angel/mods

commit 7768f821cf19a61574375c5095ac18ce1edd5fc9

Remove references to result & result_count #994

angelsaddons-cab/prototypes/recipes/cab-updates.lua
@@ -13,7 +13,7 @@ if angelsmods.industries and angelsmods.industries.components then
         { type = "item", name = "circuit-orange-loaded", amount = 2 },
         { type = "item", name = "angels-crawler", amount = 1 },
       },
-      result = "angels-cab",
+      results = {{ type = "item" name = "angels-cab", amount = 1 }},
     },
   })
 end

angelsaddons-liquidrobot/prototypes/recipes/cab-updates.lua
@@ -11,6 +11,6 @@ angelsmods.functions.RB.build({
       { "construction-components", 20 },
       { "circuit-orange-loaded", 2 },
     },
-    result = "angels-cab",
+    results = {{ type = "item" name = "angels-cab", amount = 1, }},
   },
 })

commit 71b0ba370be06f0047bae12deb8d3361f4dd1d57

Fix startup issues with 2.0 Prep branch (#1002)
* fix: add missing comma

Regression in 7768f821cf19a61574375c5095ac18ce1edd5fc9

* fix: restore handling of ingredient instructions

Fixes nil to number comparison, line 113. Parses recipe amounts based on instruction set and current tier.

* fix: add missing commas

* fix: restore amount (4) of chem plants

* fix: convert techs to short-form

* fix: restore results

* fix: 4 -> 1

angelsaddons-cab/prototypes/recipes/cab-updates.lua
@@ -13,7 +13,7 @@ if angelsmods.industries and angelsmods.industries.components then
         { type = "item", name = "circuit-orange-loaded", amount = 2 },
         { type = "item", name = "angels-crawler", amount = 1 },
       },
-      results = {{ type = "item" name = "angels-cab", amount = 1 }},
+      results = {{ type = "item", name = "angels-cab", amount = 1 }},
     },
   })
 end

angelsaddons-liquidrobot/prototypes/recipes/cab-updates.lua
@@ -11,6 +11,6 @@ angelsmods.functions.RB.build({
       { "construction-components", 20 },
       { "circuit-orange-loaded", 2 },
     },
-    results = {{ type = "item" name = "angels-cab", amount = 1, }},
+    results = {{ type = "item", name = "angels-cab", amount = 1, }},
   },
 })

angelsaddons-liquidrobot/prototypes/recipes/liquid-robot-recipes.lua
@@ -10,7 +10,7 @@ data:extend({
       { "engine-unit", 1 },
       { "electronic-circuit", 2 },
     },
-    results = {{ type = "item" name = "empty-liquid-bot", amount = 1, }},
+    results = {{ type = "item", name = "empty-liquid-bot", amount = 1, }},
   },
   {
     type = "recipe",
@@ -18,7 +18,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-a", amount = 1, }},
+    results = {{ type = "item", name = "dummy-a", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "b",
     hide_from_stats = true,
@@ -46,7 +46,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-b", amount = 1, }},
+    results = {{ type = "item", name = "dummy-b", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "d",
     hide_from_stats = true,
@@ -74,7 +74,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-c", amount = 1, }},
+    results = {{ type = "item", name = "dummy-c", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "f",
     hide_from_stats = true,
@@ -102,7 +102,7 @@ data:extend({
     energy_required = 0.5,
     enabled = false,
     ingredients = {},
-    results = {{ type = "item" name = "dummy-d", amount = 1, }},
+    results = {{ type = "item", name = "dummy-d", amount = 1, }},
     subgroup = "liquid-bot-items",
     order = "h",
     hide_from_stats = true,

angelsaddons-mobility/prototypes/recipes/recipes.lua
@@ -10,18 +10,18 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "locomotive", { 1, 0, 0, 0, 0 } },
-      { "engine-unit", { 25, "+5", 0, 0, 0 } },
-      { "electric-engine-unit", { 0, 0, 40, 45, 50 } },
-      { "electronic-circuit", { 20, 0, 0, 0, 0 } },
-      { "speed-module", { 0, 2, 0, 0, 0 } },
-      { "speed-module-2", { 0, 0, 2, 0, 0 } },
-      { "speed-module-3", { 0, 0, 0, 2, 0 } },
-      { "rocket-control-unit", { 0, 0, 0, 0, 2 } },
-      { "steel-plate", { 40, "+10", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
-    },
-    results = {{ type = "item" name = "crawler-locomotive", amount = 1, }},
+      { name = "locomotive", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "engine-unit", amounts = { 25, "+5", 0, 0, 0 } },
+      { name = "electric-engine-unit", amounts = { 0, 0, 40, 45, 50 } },
+      { name = "electronic-circuit", amounts = { 20, 0, 0, 0, 0 } },
+      { name = "speed-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "speed-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "speed-module-3", amounts = { 0, 0, 0, 2, 0 } },
+      { name = "rocket-control-unit", amounts = { 0, 0, 0, 0, 2 } },
+      { name = "steel-plate", amounts = { 40, "+10", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 50, "+25", "+25" } },
+    }, 
+    results = {{ type = "item", name = "crawler-locomotive", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -30,21 +30,21 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "locomotive", { 1, 0, 0, 0, 0 } },
-      { "engine-unit", { 25, "+5", 0, 0, 0 } },
-      { "electric-engine-unit", { 0, 0, 40, 45, 50 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "speed-module", { 0, 1, 0, 0, 0 } },
-      { "speed-module-2", { 0, 0, 1, 0, 0 } },
-      { "speed-module-3", { 0, 0, 0, 1, 0 } },
-      { "effectivity-module", { 0, 1, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 1, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 1, 0 } },
-      { "rocket-control-unit", { 0, 0, 0, 0, 2 } },
-      { "steel-plate", { 35, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 40, "+20", "+20" } },
+      { name = "locomotive", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "engine-unit", amounts = { 25, "+5", 0, 0, 0 } },
+      { name = "electric-engine-unit", amounts = { 0, 0, 40, 45, 50 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "speed-module", amounts = { 0, 1, 0, 0, 0 } },
+      { name = "speed-module-2", amounts = { 0, 0, 1, 0, 0 } },
+      { name = "speed-module-3", amounts = { 0, 0, 0, 1, 0 } },
+      { name = "effectivity-module", amounts = { 0, 1, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 1, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 1, 0 } },
+      { name = "rocket-control-unit", amounts = { 0, 0, 0, 0, 2 } },
+      { name = "steel-plate", amounts = { 35, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 40, "+20", "+20" } },
     },
-    results = {{ type = "item" name = "crawler-locomotive-wagon", amount = 1, }},
+    results = {{ type = "item", name = "crawler-locomotive-wagon", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -53,18 +53,18 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "cargo-wagon", { 1, 0, 0, 0, 0 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "effectivity-module", { 0, 2, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 2, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 2, 0 } },
-      { "iron-gear-wheel", { 25, 0, 0, 0, 0 } },
-      { "iron-chest", { 0, 5, "+5", 0, 0 } },
-      { "steel-chest", { 0, 0, 0, 5, "+5" } },
-      { "steel-plate", { 20, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
+      { name = "cargo-wagon", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "effectivity-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 2, 0 } },
+      { name = "iron-gear-wheel", amounts = { 25, 0, 0, 0, 0 } },
+      { name = "iron-chest", amounts = { 0, 5, "+5", 0, 0 } },
+      { name = "steel-chest", amounts = { 0, 0, 0, 5, "+5" } },
+      { name = "steel-plate", amounts = { 20, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "crawler-wagon", amount = 1, }},
+    results = {{ type = "item", name = "crawler-wagon", amount = 1, }},
   }, "angels-crawler-train")
 
   funcs.generate_train_recipe({
@@ -73,21 +73,21 @@ if angelsmods.addons.mobility.crawlertrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "cargo-wagon", { 1, 0, 0, 0, 0 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "effectivity-module", { 0, 2, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 2, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 2, 0 } },
-      { "rocket-control-unit", { 0, 0, 0, 0, 2 } },
-      { "iron-gear-wheel", { 25, 0, 0, 0, 0 } },
-      { "iron-chest", { 0, 5, 0, 0, 0 } },
-      { "steel-chest", { 0, 0, 5, 0, 0 } },
-      { "logistic-chest-storage", { 0, 0, 0, 5, 0 } },
-      { "logistic-chest-buffer", { 0, 0, 0, 0, 5 } },
-      { "steel-plate", { 20, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
+      { name = "cargo-wagon", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "effectivity-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 2, 0 } },
+      { name = "rocket-control-unit", amounts = { 0, 0, 0, 0, 2 } },
+      { name = "iron-gear-wheel", amounts = { 25, 0, 0, 0, 0 } },
+      { name = "iron-chest", amounts = { 0, 5, 0, 0, 0 } },
+      { name = "steel-chest", amounts = { 0, 0, 5, 0, 0 } },
+      { name = "logistic-chest-storage", amounts = { 0, 0, 0, 5, 0 } },
+      { name = "logistic-chest-buffer", amounts = { 0, 0, 0, 0, 5 } },
+      { name = "steel-plate", amounts = { 20, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "crawler-bot-wagon", amount = 1, }},
+    results = {{ type = "item", name = "crawler-bot-wagon", amount = 1, }},
   }, "angels-crawler-train")
 end
 
@@ -99,18 +99,18 @@ if angelsmods.addons.mobility.petrotrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "locomotive", { 1, 0, 0, 0, 0 } },
-      { "engine-unit", { 25, "+5", 0, 0, 0 } },
-      { "electric-engine-unit", { 0, 0, 40, 45, 50 } },
-      { "electronic-circuit", { 20, 0, 0, 0, 0 } },
-      { "speed-module", { 0, 2, 0, 0, 0 } },
-      { "speed-module-2", { 0, 0, 2, 0, 0 } },
-      { "speed-module-3", { 0, 0, 0, 2, 0 } },
-      { "rocket-control-unit", { 0, 0, 0, 0, 2 } },
-      { "steel-plate", { 40, "+10", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
+      { name = "locomotive", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "engine-unit", amounts = { 25, "+5", 0, 0, 0 } },
+      { name = "electric-engine-unit", amounts = { 0, 0, 40, 45, 50 } },
+      { name = "electronic-circuit", amounts = { 20, 0, 0, 0, 0 } },
+      { name = "speed-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "speed-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "speed-module-3", amounts = { 0, 0, 0, 2, 0 } },
+      { name = "rocket-control-unit", amounts = { 0, 0, 0, 0, 2 } },
+      { name = "steel-plate", amounts = { 40, "+10", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 50, "+25", "+25" } },
     },
-    results = {{ type = "item" name = "petro-locomotive-1", amount = 1, }},
+    results = {{ type = "item", name = "petro-locomotive-1", amount = 1, }},
   }, "angels-petro-train")
 
   funcs.generate_train_recipe({
@@ -119,17 +119,17 @@ if angelsmods.addons.mobility.petrotrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "fluid-wagon", { 1, 0, 0, 0, 0 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "effectivity-module", { 0, 2, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 2, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 2, "+0" } },
-      { "pipe", { 25, 0, 0, 0, 0 } },
-      { "storage-tank", { 0, 3, "+3", "+3", "+3" } },
-      { "steel-plate", { 20, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
+      { name = "fluid-wagon", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "effectivity-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 2, "+0" } },
+      { name = "pipe", amounts = { 25, 0, 0, 0, 0 } },
+      { name = "storage-tank", amounts = { 0, 3, "+3", "+3", "+3" } },
+      { name = "steel-plate", amounts = { 20, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "petro-tank1", amount = 1, }},
+    results = {{ type = "item", name = "petro-tank1", amount = 1, }},
   }, "angels-petro-train")
 
   funcs.generate_train_recipe({
@@ -138,17 +138,17 @@ if angelsmods.addons.mobility.petrotrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "fluid-wagon", { 1, 0, 0, 0, 0 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "effectivity-module", { 0, 2, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 2, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 2, "+0" } },
-      { "pipe", { 25, 0, 0, 0, 0 } },
-      { "storage-tank", { 0, 3, "+3", "+3", "+3" } },
-      { "steel-plate", { 20, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
+      { name = "fluid-wagon", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "effectivity-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 2, "+0" } },
+      { name = "pipe", amounts = { 25, 0, 0, 0, 0 } },
+      { name = "storage-tank", amounts = { 0, 3, "+3", "+3", "+3" } },
+      { name = "steel-plate", amounts = { 20, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "petro-tank2", amount = 1, }},
+    results = {{ type = "item", name = "petro-tank2", amount = 1, }},
   }, "angels-petro-train")
 end
 
@@ -160,18 +160,18 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "locomotive", { 1, 0, 0, 0, 0 } },
-      { "engine-unit", { 25, "+5", 0, 0, 0 } },
-      { "electric-engine-unit", { 0, 0, 40, 45, 50 } },
-      { "electronic-circuit", { 20, 0, 0, 0, 0 } },
-      { "speed-module", { 0, 2, 0, 0, 0 } },
-      { "speed-module-2", { 0, 0, 2, 0, 0 } },
-      { "speed-module-3", { 0, 0, 0, 2, 0 } },
-      { "rocket-control-unit", { 0, 0, 0, 0, 2 } },
-      { "steel-plate", { 40, "+10", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 50, "+25", "+25" } },
+      { name = "locomotive", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "engine-unit", amounts = { 25, "+5", 0, 0, 0 } },
+      { name = "electric-engine-unit", amounts = { 0, 0, 40, 45, 50 } },
+      { name = "electronic-circuit", amounts = { 20, 0, 0, 0, 0 } },
+      { name = "speed-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "speed-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "speed-module-3", amounts = { 0, 0, 0, 2, 0 } },
+      { name = "rocket-control-unit", amounts = { 0, 0, 0, 0, 2 } },
+      { name = "steel-plate", amounts = { 40, "+10", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 50, "+25", "+25" } },
     },
-    results = {{ type = "item" name = "smelting-locomotive-1", amount = 1, }},
+    results = {{ type = "item", name = "smelting-locomotive-1", amount = 1, }},
   }, "angels-smelting-train")
 
   funcs.generate_train_recipe({
@@ -180,21 +180,21 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "locomotive", { 1, 0, 0, 0, 0 } },
-      { "engine-unit", { 25, "+5", 0, 0, 0 } },
-      { "electric-engine-unit", { 0, 0, 40, 45, 50 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "speed-module", { 0, 1, 0, 0, 0 } },
-      { "speed-module-2", { 0, 0, 1, 0, 0 } },
-      { "speed-module-3", { 0, 0, 0, 1, 0 } },
-      { "effectivity-module", { 0, 1, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 1, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 1, 0 } },
-      { "rocket-control-unit", { 0, 0, 0, 0, 2 } },
-      { "steel-plate", { 35, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 40, "+20", "+20" } },
+      { name = "locomotive", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "engine-unit", amounts = { 25, "+5", 0, 0, 0 } },
+      { name = "electric-engine-unit", amounts = { 0, 0, 40, 45, 50 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "speed-module", amounts = { 0, 1, 0, 0, 0 } },
+      { name = "speed-module-2", amounts = { 0, 0, 1, 0, 0 } },
+      { name = "speed-module-3", amounts = { 0, 0, 0, 1, 0 } },
+      { name = "effectivity-module", amounts = { 0, 1, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 1, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 1, 0 } },
+      { name = "rocket-control-unit", amounts = { 0, 0, 0, 0, 2 } },
+      { name = "steel-plate", amounts = { 35, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 40, "+20", "+20" } },
     },
-    results = {{ type = "item" name = "smelting-locomotive-tender", amound = 1, }},
+    results = {{ type = "item", name = "smelting-locomotive-tender", amound = 1, }},
   }, "angels-smelting-train")
 
   funcs.generate_train_recipe({
@@ -203,17 +203,17 @@ if angelsmods.addons.mobility.smeltingtrain.enabled then
     energy_required = 10,
     enabled = false,
     ingredients = {
-      { "cargo-wagon", { 1, 0, 0, 0, 0 } },
-      { "electronic-circuit", { 15, 0, 0, 0, 0 } },
-      { "effectivity-module", { 0, 2, 0, 0, 0 } },
-      { "effectivity-module-2", { 0, 0, 2, 0, 0 } },
-      { "effectivity-module-3", { 0, 0, 0, 2, "+0" } },
-      { "iron-gear-wheel", { 25, 0, 0, 0, 0 } },
-      { "iron-chest", { 0, 5, "+5", 0, 0 } },
-      { "steel-chest", { 0, 0, 0, 5, "+5" } },
-      { "steel-plate", { 20, "+5", 0, 0, 0 } },
-      { "low-density-structure", { 0, 0, 25, "+10", "+15" } },
+      { name = "cargo-wagon", amounts = { 1, 0, 0, 0, 0 } },
+      { name = "electronic-circuit", amounts = { 15, 0, 0, 0, 0 } },
+      { name = "effectivity-module", amounts = { 0, 2, 0, 0, 0 } },
+      { name = "effectivity-module-2", amounts = { 0, 0, 2, 0, 0 } },
+      { name = "effectivity-module-3", amounts = { 0, 0, 0, 2, "+0" } },
+      { name = "iron-gear-wheel", amounts = { 25, 0, 0, 0, 0 } },
+      { name = "iron-chest", amounts = { 0, 5, "+5", 0, 0 } },
+      { name = "steel-chest", amounts = { 0, 0, 0, 5, "+5" } },
+      { name = "steel-plate", amounts = { 20, "+5", 0, 0, 0 } },
+      { name = "low-density-structure", amounts = { 0, 0, 25, "+10", "+15" } },
     },
-    results = {{ type = "item" name = "smelting-wagon-1", amount = 1, }},
+    results = {{ type = "item", name = "smelting-wagon-1", amount = 1, }},
   }, "angels-smelting-train")
 end

angelsaddons-mobility/prototypes/train-functions.lua
@@ -86,7 +86,7 @@ local function generate_tiered_ingredients(tier, ingredients)
   local generated_ingredients = {}
   for _, ingredient in pairs(ingredients) do
     local ingredient_name = ingredient.name
-    local ingredient_amount = ingredient.amount
+    local ingredient_amount = ingredient.amount or ingredient.amounts
 
     if type(ingredient_amount) == "table" then
       ingredient_amount = ingredient_amount[tier] or 0
@@ -144,7 +144,7 @@ local function generate_train_recipe(item, add_unlock)
       copy.localised_name = { "", { "recipe-name." .. item.name }, " MK" .. i }
       copy.localised_description = { "recipe-description." .. item.name }
       copy.ingredients = ingredients
-      copy.results = {{ type = "item", name = name, amount  = 1 }},
+      copy.results = {{ type = "item", name = name, amount  = 1 }}
 
       if copy.order then
         copy.order = copy.order .. "-" .. i

angelsbioprocessing/prototypes/technology/bio-processing-artifacts.lua
@@ -38,7 +38,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-red-tool", amount = 1 },
+        { "alien-artifact-red-tool", 1 },
       },
       time = 30,
     },
@@ -82,7 +82,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-yellow-tool", amount = 1 },
+        { "alien-artifact-yellow-tool", 1 },
       },
       time = 30,
     },
@@ -125,7 +125,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-orange-tool", amount = 1 },
+        { "alien-artifact-orange-tool", 1 },
       },
       time = 30,
     },
@@ -168,7 +168,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-blue-tool", amount = 1 },
+        { "alien-artifact-blue-tool", 1 },
       },
       time = 30,
     },
@@ -212,7 +212,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-purple-tool", amount = 1 },
+        { "alien-artifact-purple-tool", 1 },
       },
       time = 30,
     },
@@ -256,7 +256,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-green-tool", amount = 1 },
+        { "alien-artifact-green-tool", 1 },
       },
       time = 30,
     },
@@ -292,7 +292,7 @@ data:extend({
     unit = {
       count = 50,
       ingredients = {
-        { type = "item", name = "alien-artifact-tool", amount = 1 },
+        { "alien-artifact-tool", 1 },
       },
       time = 30,
     },

angelsexploration/prototypes/overrides/artifacts-updates.lua
@@ -61,8 +61,8 @@ if alien_technology then
       unit = {
         count = 250,
         ingredients = {
-          { type = "item", name = "science-pack-gold", amount = 1 },
-          { type = "item", name = "alien-science-pack", amount = 2 },
+          { "science-pack-gold", 1 },
+          { "alien-science-pack", 2 },
         },
         time = 30,
       },
@@ -107,8 +107,8 @@ if alien_technology then
       unit = {
         count = 250,
         ingredients = {
-          { type = "item", name = "science-pack-gold", amount = 1 },
-          { type = "item", name = "alien-science-pack", amount = 2 },
+          { "science-pack-gold", 1 },
+          { "alien-science-pack", 2 },
         },
         time = 30,
       },
@@ -186,8 +186,8 @@ if alien_technology then
       unit = {
         count = 250,
         ingredients = {
-          { type = "item", name = "science-pack-gold", amount = 1 },
-          { type = "item", name = "alien-science-pack", amount = 2 },
+          { "science-pack-gold", 1 },
+          { "alien-science-pack", 2 },
         },
         time = 30,
       },
@@ -242,8 +242,8 @@ if alien_technology then
       unit = {
         count = 250,
         ingredients = {
-          { type = "item", name = "science-pack-gold", amount = 1 },
-          { type = "item", name = "alien-science-pack", amount = 2 },
+          { "science-pack-gold", 1 },
+          { "alien-science-pack", 2 },
         },
         time = 30,
       },
@@ -312,8 +312,8 @@ if alien_technology then
       unit = {
         count = 250,
         ingredients = {
-          { type = "item", name = "science-pack-gold", amount = 1 },
-          { type = "item", name = "alien-science-pack", amount = 2 },
+          { "science-pack-gold", 1 },
+          { "alien-science-pack", 2 },
         },
         time = 30,
       },
@@ -359,8 +359,8 @@ if alien_technology then
       unit = {
         count = 250,
         ingredients = {
-          { type = "item", name = "science-pack-gold", amount = 1 },
-          { type = "item", name = "alien-science-pack", amount = 2 },
+          { "science-pack-gold", 1 },
+          { "alien-science-pack", 2 },
         },
         time = 30,
       },

angelsindustries/prototypes/overrides/industries-override-functions.lua
@@ -213,7 +213,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "tech-specialised-labs")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.grey.time
+              angelsmods.industries.techtiers.grey.time,
               angelsmods.industries.techtiers.grey.amount
             )
           end
@@ -221,7 +221,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "tech-red-packs")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.red.time
+              angelsmods.industries.techtiers.red.time,
               angelsmods.industries.techtiers.red.amount
             )
           end
@@ -229,7 +229,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "tech-green-packs")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.green.time
+              angelsmods.industries.techtiers.green.time,
               angelsmods.industries.techtiers.green.amount
             )
           end
@@ -237,7 +237,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "tech-orange-packs")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.orange.time
+              angelsmods.industries.techtiers.orange.time,
               angelsmods.industries.techtiers.orange.amount
             )
           end
@@ -245,7 +245,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "tech-blue-packs")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.blue.time
+              angelsmods.industries.techtiers.blue.time,
               angelsmods.industries.techtiers.blue.amount
             )
           end
@@ -253,7 +253,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "tech-yellow-packs")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.yellow.time
+              angelsmods.industries.techtiers.yellow.time,
               angelsmods.industries.techtiers.yellow.amount
             )
           end
@@ -261,7 +261,7 @@ function angelsmods.functions.AI.tech_unlock_reset()
             OV.add_prereq(techname, "space-science-pack")
             set_research_tiers(
               techname,
-              angelsmods.industries.techtiers.white.time
+              angelsmods.industries.techtiers.white.time,
               angelsmods.industries.techtiers.white.amount
             )
           end

angelspetrochem/prototypes/recipes/petrochem-entity.lua
@@ -475,7 +475,7 @@ data:extend({
     ingredients = {
       { type = "item", name = "angels-chemical-plant-3", amount = 1 },
     },
-    results = {{ type = "item", name = "angels-chemical-plant-4", amount = }},
+    results = {{ type = "item", name = "angels-chemical-plant-4", amount = 1 }},
   },
   --ELECTRIC BOILER
   {

angelsrefining/prototypes/recipes/refining-dynamic/crystal-processing-mix.lua
@@ -54,6 +54,9 @@ data:extend({
       { type = "item", name = "angels-ore6-crystal", amount = 2 },
       { type = "item", name = "catalysator-orange", amount = 1 },
     },
+    results = {
+      { type = "item", name = "angels-void", amount = 1 },
+    },
     icon_size = 32,
     order = "m",
   },