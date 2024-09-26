8b84f77de46967fecbce79d8137dd2f7f485d592
kingarthur91 <arthurkoy_durbin@yahoo.com>
2024-09-25 22:12:39 -0400

    removed biter builder noise

--- angelsexploration/prototypes/entities/biter-builder.lua
+++ angelsexploration/prototypes/entities/biter-builder.lua
@@ -1,10 +1,5 @@
 require("util")
 
-local noise = require("noise")
-
-local tne = noise.to_noise_expression
-local litexp = noise.literal_expression
-
 local onehalf_exp = tne(1) / 2
 local onethird_exp = tne(1) / 3
 
