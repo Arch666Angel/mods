182d6bb7334051d0a2aa9a4996b960fb53e48aba
Kira <kirazy@live.com>
2024-10-23 23:50:48 -0700

    refactor: clean up control
    - Add Lua type annotations
    - Convert variable names to snake_case
    - Rename variables for clarity
    - Improve readability of filter condition

--- angelsaddons-mobility/control.lua
+++ angelsaddons-mobility/control.lua
@@ -1,49 +1,55 @@
-local sourceEntityTypes = {
+local source_entity_types = {
   ["train-stop"] = true,
   ["locomotive"] = true,
   ["cargo-wagon"] = true,
   ["fluid-wagon"] = true,
 }
 
-local destinationEntityTypes = {
+local destination_entity_types = {
   ["cargo-wagon"] = true,
   ["fluid-wagon"] = true,
 }
 
-local ptier_amount = settings.startup["angels-petrotrain-tier-amount"].value
-local stier_amount = settings.startup["angels-smeltingtrain-tier-amount"].value
-local wagonsToCopy = {
-  ["petro-tank1"] = ptier_amount >= 1,
-  ["petro-tank1-2"] = ptier_amount >= 2,
-  ["petro-tank1-3"] = ptier_amount >= 3,
-  ["petro-tank1-4"] = ptier_amount >= 4,
-  ["petro-tank1-5"] = ptier_amount >= 5,
-  ["petro-tank2"] = ptier_amount >= 1,
-  ["petro-tank2-2"] = ptier_amount >= 2,
-  ["petro-tank2-3"] = ptier_amount >= 3,
-  ["petro-tank2-4"] = ptier_amount >= 4,
-  ["petro-tank2-5"] = ptier_amount >= 5,
-  ["smelting-wagon-1"] = stier_amount >= 1,
-  ["smelting-wagon-1-2"] = stier_amount >= 2,
-  ["smelting-wagon-1-3"] = stier_amount >= 3,
-  ["smelting-wagon-1-4"] = stier_amount >= 4,
-  ["smelting-wagon-1-5"] = stier_amount >= 5,
+local petro_tier_amount = settings.startup["angels-petrotrain-tier-amount"].value
+local smelting_tier_amount = settings.startup["angels-smeltingtrain-tier-amount"].value
+local wagons_to_copy = {
+  ["petro-tank1"] = petro_tier_amount >= 1,
+  ["petro-tank1-2"] = petro_tier_amount >= 2,
+  ["petro-tank1-3"] = petro_tier_amount >= 3,
+  ["petro-tank1-4"] = petro_tier_amount >= 4,
+  ["petro-tank1-5"] = petro_tier_amount >= 5,
+
+  ["petro-tank2"] = petro_tier_amount >= 1,
+  ["petro-tank2-2"] = petro_tier_amount >= 2,
+  ["petro-tank2-3"] = petro_tier_amount >= 3,
+  ["petro-tank2-4"] = petro_tier_amount >= 4,
+  ["petro-tank2-5"] = petro_tier_amount >= 5,
+
+  ["smelting-wagon-1"] = smelting_tier_amount >= 1,
+  ["smelting-wagon-1-2"] = smelting_tier_amount >= 2,
+  ["smelting-wagon-1-3"] = smelting_tier_amount >= 3,
+  ["smelting-wagon-1-4"] = smelting_tier_amount >= 4,
+  ["smelting-wagon-1-5"] = smelting_tier_amount >= 5,
 }
 
+---Copies and pastes the color from the source entity to the destination entity, if the source and
+---destination entities are valid for the operation.
+---@param event EventData.on_entity_settings_pasted
 local on_entity_settings_pasted = function(event)
   local source = event.source
   local destination = event.destination
 
-  if
-    (wagonsToCopy[source.name] or wagonsToCopy[destination.name]) -- correct train to do behaviour for
-    and (
-      (sourceEntityTypes[source.type] and destinationEntityTypes[destination.type]) -- paste to a cargo or fluid wagon
-      or (sourceEntityTypes[destination.type] and destinationEntityTypes[source.type])
-    )
-  then -- copy from a cargo or fluid wagon
+  local is_valid_source = wagons_to_copy[source.name] or wagons_to_copy[destination.name]
+
+  local is_valid_destination =
+      (source_entity_types[source.type] and destination_entity_types[destination.type]) or
+      (source_entity_types[destination.type] and destination_entity_types[source.type])
+
+  if is_valid_source and is_valid_destination then
     local source_color = source.color or source.prototype.color
     if source_color then
       local destination_color = destination.color or destination.prototype.color
+
       destination.color = {
         r = source_color.r,
         g = source_color.g,
