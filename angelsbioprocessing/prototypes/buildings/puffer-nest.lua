d9a721e565b9b37fa3ac7e16dc3ae5479c1b24a6
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-06 15:40:29 -0700

    fix: Removed puffer-nest item https://github.com/Arch666Angel/mods/issues/1087

--- angelsbioprocessing/prototypes/buildings/puffer-nest.lua
+++ angelsbioprocessing/prototypes/buildings/puffer-nest.lua
@@ -2,16 +2,6 @@ local def_shift = { 0.75, -2 }
 
 data:extend({
   {
-    type = "item",
-    name = "puffer-nest",
-    icon = "__angelsbioprocessinggraphics__/graphics/icons/puffer-nest.png",
-    icon_size = 32,
-    subgroup = "farming-gardens",
-    order = "a",
-    stack_size = 200,
-    place_result = "puffer-nest",
-  },
-  {
     type = "tree",
     name = "puffer-nest",
     icon = "__angelsbioprocessinggraphics__/graphics/icons/puffer-nest.png",
--- angelsbioprocessing/prototypes/overrides/bio-processing-override-angel.lua
+++ angelsbioprocessing/prototypes/overrides/bio-processing-override-angel.lua
@@ -82,8 +82,6 @@ for _, item_name in pairs({ "solid-beans", "solid-corn", "solid-leafs", "solid-n
   end
 end
 
--- Puffer nests cannot be created
-angelsmods.functions.hide("puffer-nest")
 
 if angelsmods.trigger.crystals_full == true then
 else
