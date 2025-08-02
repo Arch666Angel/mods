546e194337836589b6fb48b27c4790a14097905c
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-08-02 13:36:35 +1200

    Make sure roughly even numbers of each type of fish will spawn

--- angelsbioprocessing/prototypes/buildings/alien-fish.lua
+++ angelsbioprocessing/prototypes/buildings/alien-fish.lua
@@ -1,6 +1,17 @@
-data.raw.planet.nauvis.map_gen_settings.autoplace_settings.entity.settings["alien-fish-1"] = {}
-data.raw.planet.nauvis.map_gen_settings.autoplace_settings.entity.settings["alien-fish-2"] = {}
-data.raw.planet.nauvis.map_gen_settings.autoplace_settings.entity.settings["alien-fish-3"] = {}
+data.raw.planet.nauvis.map_gen_settings.autoplace_settings.entity.settings["angels-alien-fish-1"] = {}
+data.raw.planet.nauvis.map_gen_settings.autoplace_settings.entity.settings["angels-alien-fish-2"] = {}
+data.raw.planet.nauvis.map_gen_settings.autoplace_settings.entity.settings["angels-alien-fish-3"] = {}
+
+-- We have four types of fish
+-- Make them spawn on different tiles
+-- Else they will overwrite each other
+--
+--                    |  X   |  Y
+-- ---------------------------------
+--         Levac fish | Odd  | Odd
+-- Dorflurp jellyfish | Even | Odd
+--          Santa ray | Odd  | Even
+--     Factorian fish | Even | Even
 
 data:extend({
   {
@@ -9,7 +20,7 @@ data:extend({
     icon = "__angelsbioprocessinggraphics__/graphics/icons/alien-fish-1.png",
     icon_size = 32,
     flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 1, result = "angels-alien-fish-1-raw", count = 1 },
+    minable = { mining_time = 1, result = "angels-alien-fish-1-raw", count = 5 },
     max_health = 20,
     subgroup = "creatures",
     order = "xa",
@@ -26,17 +37,7 @@ data:extend({
     },
     autoplace = {
       order = "xa",
-      --max_probability = 0.025,
-      --peaks = {
-      --  {
-      --    influence = 0.01,
-      --    min_influence = 0,
-      --    water_optimal = 0.625,
-      --    water_range = 0.125,
-      --    water_max_range = 0.125,
-      --  },
-      --},
-      probability_expression = 0.00025
+      probability_expression = "x%2*y%2*0.01"
     },
     protected_from_tile_building = false,
   },
@@ -46,7 +47,7 @@ data:extend({
     icon = "__angelsbioprocessinggraphics__/graphics/icons/alien-fish-2.png",
     icon_size = 32,
     flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 1, result = "angels-alien-fish-2-raw", count = 1 },
+    minable = { mining_time = 1, result = "angels-alien-fish-2-raw", count = 5 },
     max_health = 20,
     subgroup = "creatures",
     order = "xb",
@@ -63,17 +64,7 @@ data:extend({
     },
     autoplace = {
       order = "xb",
-      --max_probability = 0.025,
-      --peaks = {
-      --  {
-      --    influence = 0.01,
-      --    min_influence = 0,
-      --    water_optimal = 0.875,
-      --    water_range = 0.125,
-      --    water_max_range = 0.125,
-      --  },
-      --},
-      probability_expression = 0.00025
+      probability_expression = "+x%2*y%2*0.01"
     },
     protected_from_tile_building = false,
   },
@@ -83,7 +74,7 @@ data:extend({
     icon = "__angelsbioprocessinggraphics__/graphics/icons/alien-fish-3.png",
     icon_size = 32,
     flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 1, result = "angels-alien-fish-3-raw", count = 1 },
+    minable = { mining_time = 1, result = "angels-alien-fish-3-raw", count = 5 },
     max_health = 20,
     subgroup = "creatures",
     order = "xc",
@@ -100,18 +91,12 @@ data:extend({
     },
     autoplace = {
       order = "xc",
-      --max_probability = 0.025,
-      --peaks = {
-      --  {
-      --    influence = 0.01,
-      --    min_influence = 0,
-      --    water_optimal = 0.250,
-      --    water_range = 0.250,
-      --    water_max_range = 0.250,
-      --  },
-      --},
-      probability_expression = 0.00025
+      probability_expression = "x%2*+y%2*0.01"
     },
     protected_from_tile_building = false,
   },
 })
+
+if data.raw["fish"]["fish"] and data.raw["fish"]["fish"].autoplace then
+  data.raw["fish"]["fish"].autoplace.probability_expression = "+x%2*+y%2*0.01"
+end
