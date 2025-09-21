b3d5852a7759d50dbdca55fa7b27aec1e70392ea
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-09-21 13:31:01 +1200

    Prevent elevated rails from crossing cooling tower

--- angelssmelting/prototypes/buildings/cooling-tower.lua
+++ angelssmelting/prototypes/buildings/cooling-tower.lua
@@ -22,6 +22,7 @@ data:extend({
     icon = "__angelssmeltinggraphics__/graphics/icons/cooling-tower.png",
     icon_size = 64,
     flags = { "placeable-neutral", "placeable-player", "player-creation" },
+    collision_mask = angelsmods.functions.set_building_collision_mask("asm", { "elevated_rail" }),
     minable = { mining_time = 1, result = "angels-cooling-tower" },
     max_health = 100,
     fast_replaceable_group = "angels-cooling-tower",
