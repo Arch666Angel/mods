c7a9f9551d427a4c8e24d270dc0475ad7ca6c36e
Kira <kirazy@live.com>
2025-12-24 02:11:15 -0800

    feat: add petro-loco

---
+++ angelsaddons-mobility/graphics/entity/petro-locomotive/petro-locomotive-lights.lua
@@ -0,0 +1,18 @@
+return
+{
+  width = 451,
+  height = 357,
+  shift = util.by_pixel_hr(-0.5, -39.5),
+  line_length = 4,
+  filenames = {
+    "-1.png",
+    "-2.png",
+    "-3.png",
+    "-4.png",
+    "-5.png",
+    "-6.png",
+    "-7.png",
+    "-8.png"
+  },
+  lines_per_file = 8,
+}
---
+++ angelsaddons-mobility/graphics/entity/petro-locomotive/petro-locomotive-sloped-shadow.lua
@@ -0,0 +1,18 @@
+return
+{
+  width = 507,
+  height = 558,
+  shift = util.by_pixel_hr(38.5, 17.0),
+  line_length = 4,
+  filenames = {
+    "-1.png",
+    "-2.png",
+    "-3.png",
+    "-4.png",
+    "-5.png",
+    "-6.png",
+    "-7.png",
+    "-8.png"
+  },
+  lines_per_file = 5,
+}
