b0bf018c5cf407f97c7a53bdae1bcc454046128f
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-01-03 19:54:04 +1300

    Updating factorio train simulations with new entity names

--- angelsaddons-mobility/prototypes/factoriopedia-simulations.lua
+++ angelsaddons-mobility/prototypes/factoriopedia-simulations.lua
@@ -17,7 +17,7 @@ simulations.factoriopedia_crawler_locomotive =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKgRTrKUJStIyhPLvcwkbSGxSpL7Yvfck9a2vsNMjnhyZAO0VSFnjof24gqfeSD33jBwQWnCSNEQBZPb4BW0ZtwLQBAqEyXErLp0Zhx06Fogfpw/s7Y+huCEEnKxnlzUznEnFWsCF9WuG78mhSu+qKF6Yy2xmnc1cZTNX2czql6mcPGt0hbbKDjbQhH+Ay0W5fLowGrnT2Gnbkw+kfHc+EteDncj00B6k9ijAOuJTZeK8LWYABRxSFrR/Ss6MSqN0xWHElGCSscp0ZCamWHdJtkfF8fkg1Sez4zby8/qVdfbkyuzJNbnMfOQ6F5mf7yYX+d9vuJ2na9Vnx/tmUvu+dnP3rphz4iMeCypgQudviOWmrDarummaqm7q9xi/AUkkRL8=",
+            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKgRTrKUOStIyhPLvSwkbSGxSpL7Yvfck9a2vsNMjniyxh/YKpAw7aD+u4KhnqeceywGhBStJQxBAvMcvaMuwFYDsyRMmx624dDwOO7RRIH6czkdvf/TFDSHgZFx0GZ7hkVSsBVyifh3he7Ko0rsqiBfmMptZZzNX2cxVNrP6ZUruUbtCWXnWaAttlBmMpwn/4JeLcvl0b2S509hp05PzpFx3PlKsBzMR99AepHYowFiKh8vEeVvMAPI4pEho/xQgj0qjtMVhxBRkkkUVd8RTpBh7SbZHFVN0XqrPyA7bEJ/Xj62zB1hmD7DJZeYj17nI/Jg3ucj//sbtPF2jPru4dpza9+2bu3fFnFM84rGnAia07oZYbspqs6qbpqnqpn4P4RvuKEdm",
             position = {0, 0}
         }
     ]]
@@ -30,7 +30,7 @@ simulations.factoriopedia_crawler_locomotive_wagon =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKgRTLNIEJWkZQvn3uYQNJDQpUl/s3nuS+tY32OkRz45MgPYGpKzx0H7dwFNvpJ57Rg4ILThJGqIAMnv8hraMWwFoAgXC5LgX186Mww4dC8Sv0wf29sdQ3BECztazy5oZzqRiLeDK+jXD9+RQpXdVFG/MZTazzmauspmrbGb1x1ROXjS6QltlBxtowuIie/a948tFuXy5Nhq509hp25MPpHx3ORLXg53I9NAepPYowDris2XifCxmAAUcUiK0f8nPjEqjdMVhxJRjkrHKdGQmplh3TbZnxSH6INWJ2XEb+Xn/1jp7fmX2/JpcZj5ynYvMT3mTi/zvZ9zO07Xq1PHWmdR+LN/cfSjmnPiI55oKmND5O2K5KavNqm6apqqb+jPGH5WXRwg=",
+            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKgRTrKUJStIyhPLvcwkbSGxSpL7Yvfck9a2vsNMjnhyZAO0VSFnjof24gqfeSD33jBwQWnCSNEQBZPb4BW0ZtwLQBAqEyXErLp0Zhx06Fogfpw/s7Y+huCEEnKxnlzUznEnFWsCF9WuG78mhSu+qKF6Yy2xmnc1cZTNX2czqlylNj9oXysmzRldoq+xgA01YBORBuj+OKRfl8un6aOROY6dtTz6Q8t35SFwPdiLTQ3uQ2qMA64jvIBPnbTEDKOCQkqH9U45mVBqlKw4jpjyTjFWmIzMxxbpLsj0qDtMHqT6ZHbeRn9dvrrPnWGbPscll5iPXucj8tDe5yP9+yu08Xas+O94+k9r3JZy7d8WcEx/xWFcBEzp/Qyw3ZbVZ1U3TVHVTv8f4Dbs7ShU=",
             position = {0, 0}
         }
     ]]
@@ -43,7 +43,7 @@ simulations.factoriopedia_crawler_wagon =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyN09tugzAMANB/8XOoyiXQ8ivThAL1qLWQVEloVyH+fQa6i9RNymMc+8RR4glaPeLFkQlQT0CdNR7qlwk89UbpJWbUgFCDU6RhFkDmhB9Qp/OrADSBAuFWsS7ujRmHFh0niK9KH7i2P4dkJQRcrOcqaxacpaQScIc6k4yfyGG37RWzeDKzaFNGm3m0mUebxbfZOXXT6JKb6jn52dzvjnn1q1k0qtXYaNuTD9T55nYmXg/2SqaH+k1pjwKsIz5RbdB+l8nlWa4cso4hM2r9R1My+qJp9EXLWDOerGLJ+Oc4xJL//Rr+6j7Y7r3h8TBb+DElS/SRQQEHPuJnngRc0fmVyA5pcchlWZaFLOVxnj8BtbconA==",
+            string = "0eJyNk91uwjAMhd/F1ymCtinQV5mmKhQvWEsdlAQYQn33uZT9SGxSLnPi89mW7Rvs3AmPgThBewPqPUdoX24QybJxk8ZmQGghGHIwKiDe4we0q/FVAXKiRDg77o9rx6dhh0EC1JczJvHaQyruCAVHH8XleYILqVgruEJbaoHvKWA//9WjemKW2UydzayymVU2s/5mGrboYtEHc3EYit4E64uLseJ8TrBcbKv1r8qRzc5h57ylmKiP3eVA8h78mdhC+2ZcRAU+kKQ3M2i5KPU0o7NIPgiIT879UaHO7nqV3XWTy8xHrnOR+bPZ5CL/WyHZ+5h8/97JrfAsP05mUh8RlHCQFD/HpeCMId4R5WZVbyrdNE2tG70dx08ELi18",
             position = {0, 0}
         }
     ]]
@@ -56,7 +56,7 @@ simulations.factoriopedia_crawler_bot_wagon =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyN09uOgyAQBuB3mWts6gHb+iqbjUE7ayeL0ADqNsZ337F2D0l3Ey4Z+D8gMDM0esCrIxOgmoFaazxULzN46ozSa82oHqECp0jDIoDMGT+gSpdXAWgCBcItcR/cajP0DTpeIL6SPnC2u4TkTgi4Ws8pa1acpeQg4AZVJhk/k8N2mysW8WRm0aaMNvNoM482i2+zdWrS6JLGhmRSHQee3f3ulB9+HRiNajTW2nbkA7W+ni7E496OZDqo3pT2KMA64l3VBu13mVyfZuSSdQyZQes/DiajL5tGX7aMNePJQywZ/yTHWPK/n8Pf3QfbvtfcImYrPzplrT5WUMCet/jpKQEjOn8nsmNaHHNZlmUhS3lalk+WgCoO",
+            string = "0eJyNk91uwjAMhd/F1ymCtinQV5mmKi1esZY6KElhCPXd51L2I7FJucyJz2dbtm/Q2hFPnjhCfQPqHAeoX24QqGdjZ43NgFCDN2RhUkB8wA+oN9OrAuRIkXBx3B/XhsehRS8B6ssZonj7Y8zuCAUnF8TleIYLKdsquEKda4EfyGO3/JWTemLmyUydzCySmUUys/xmGu7Rhqzz5mLRZ961LmYX04vzOcF6tS+2vypHNq3FxrqeQqQuNJcjyXtwZ+Ie6jdjAypwniS9WUDrVa7nGZ1Fcl5APFr7R4U6uetNctdVKjMduU1Fps9ml4r8b4Vk70N03Xsjt8KL/DiZWX1EUMRBUvwcl4Iz+nBH5LtNuSt0VVWlrvR+mj4BOlAtlg==",
             position = {0, 0}
         }
     ]]
@@ -71,7 +71,7 @@ simulations.factoriopedia_petro_locomotive =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKgRTLNIEJWk3hPLvcwkbSGxSpL7Yvfck9a2vsNMjnh2ZAO0VSFnjof24gqfeSD33jBwQWnCSNEQBZPb4BW0ZtwLQBAqEyXErLp0Zhx06Fogfpw/s7Y+huCEEnK1nlzUznEnFWsCF9WuG78mhSu+qKF6Yy2xmnc1cZTNX2czql3nG4GyhrbKDDTRhUf4BLhfl8unCaOROY6dtTz6Q8t3nkbge7ESmh/YgtUcB1hGfKhPnbTEDKOCQsqD9U3JmVBqlKw4jpgSTjFWmIzMxxbpLsj0qjs8HqU7MjtvIz+tX1tmTK7Mn1+Qy85HrXGR+vptc5H+/4XaerlWnjvfNpPZ97ebuXTHnxEc8FlTAhM7fEMtNWW1WddM0Vd3U7zF+A1XHRFc=",
+            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKhRTLFIHJWk3hPrvcwkbSGxSpL7Yvfck9a2vsDMDnh1xgPoK1Fr2UH9cwVPH2sw91j1CDU6TgUkB8R6/oM6nrQLkQIEwOm7FpeGh36ETgfpx+iDe7hiyG0LB2XpxWZ7hQsrWCi6iXwt8Tw7b+K6Y1Atzmcwsk5mrZOYqmVn8MjV3aHx2xuBsZmxrextoxD/o+SJfPt0aWe8MNsZ25AO1vvk8ktS9HYk7qA/aeFRgHcnROnLeFjOAAvYxENo/xcdDa1C77DBgjDHKRMUN8SgU6y7R9qgkQx90exL2tJ3kef3UMnl8efL4qlRmOnKdikwPeZOK/O9f3M7Tte2pkaXj2L7v3ty9K+ac5IjHlioY0fkbYrnJi82qrKqqKKvyfZq+ASE7RqA=",
             position = {0, 0}
         }
     ]]
@@ -84,7 +84,7 @@ simulations.factoriopedia_petro_tank1 =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKhRTLFqnSlI2hPLvcwkbSGxSpL7Yvfck9a2vsOsnHC2xh/oK1Bp2UH9cwVHHup97rAeEGqymHoIC4j1+QZ2HrQJkT54wOm7FpeFp2KEVgfpxOi/e7uizG0LBaJy4DM9wIWVrBRfRrwW+J4ttfFcE9cJcJjPLZOYqmblKZha/zBG9NZnXfMr/IOaLfPl0U2S967HpTUfOU+uazyNJPZgzcQf1QfcOFRhLcpyOnLfFDCCPQwyB9k+R8dT2qG12mDBGF2Wi4ob4LBRjL9H2qCQ353V7EnbYBnleP69MHlmePLIqlZmOXKci04PdpCL/+/+283RNe2pk0Ti27/s2d++KOSc54rGZCs5o3Q2x3OTFZlVWVVWUVfkewjeGckGX",
+            string = "0eJyNk9GOgjAQRf9lnotZFNDlVzaGVBxxYpmStugaw7/vYN3VxGzShJcZ7j0tc5kb7MyIgyMOUN+AWsse6q8beOpYm7nHukeowWkyMCkg3uM31Pm0VYAcKBBGx724Njz2O3QiUL9OH8TbHUN2RygYrBeX5RkupGyt4Cr6tcD35LCN74pJvTGXycwymblKZq6SmcUfU3OHxmcDBmezTvvsojtxvcPzRb58uTSy3hlsjO3IB2p9czmS1L09E3dQH7TxqMA6kpN15HwsZgAF7GMetH9Jj8fWoHbZYcSYYpSJihvis1Csu0bbs5IIfdDtSdjTdpLn/UvL5OnlydOrUpnpyHUqMj3jTSryv19xO0/XtqdGdo5j+7F6c/ehmHOSI55LquCMzt8Ry01ebFZlVVVFWZWf0/QD5m9F4w==",
             position = {0, 0}
         }
     ]]
@@ -97,7 +97,7 @@ simulations.factoriopedia_petro_tank2 =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaS1tYf2VCVSimWLQOSlI2hPLvcwkbSGxSpL7Yvfck9a2vsB0mPFliD80VqDPsoPm4gqOe9TD3WI8IDVhNAwQFxDv8giYPGwXInjxhdNyKS8vTuEUrAvXjdF68/cFnN4SCk3HiMjzDhZStFFxEvxL4jix28V0Z1AuzSGZWycxlMnOZzCx/mSf01mRe87H4g5gv8uLppsh6O2A7mJ6cp861nweSejRn4h6avR4cKjCW5DgdOW+LGUAexxgC7Z4i46kbUNtsP2GMLspExS3xWSjGXqLtUUluzuvuKOywCfK8fl6VPLI8eWR1KjMduUpFpge7TkX+9/9t5uma7tjKonFs3/dt7t4Vc05yxGMzFZzRuhuiWOflelnVdV1WdfUewjeIykGY",
+            string = "0eJyNk9GOgjAQRf9lnotZFNDlVzaGVBxxYpmStugaw7/vYN3VxGzShJcZ7j0tc5kb7MyIgyMOUN+AWsse6q8beOpYm7nHukeowWkyMCkg3uM31Pm0VYAcKBBGx724Njz2O3QiUL9OH8TbHUN2RygYrBeX5RkupGyt4Cr6tcD35LCN74pJvTGXycwymblKZq6SmcUfU3OHxmcDBmczSya76E5c7/B8kS9fLo2sdwYbYzvygVrfXI4kdW/PxB3UB208KrCO5GQdOR+LGUAB+5gH7V/S47E1qF12GDGmGGWi4ob4LBTrrtH2rCRCH3R7Eva0neR5/9IyeXp58vSqVGY6cp2KTM94k4r871fcztO17amRnePYfqze3H0o5pzkiOeSKjij83fEcpMXm1VZVVVRVuXnNP0A+9VF7A==",
             position = {0, 0}
         }
     ]]
@@ -112,7 +112,7 @@ simulations.factoriopedia_smelting_locomotive =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaoS2sv4JQFYIpFmmCkrQbQvn3uYQJJDYpUl/s3nuS+tY32OsRL45MgPYGpKzx0G5v4Kk3Us89IweEFpwkDVEAmQN+Q1vGnQA0gQJhctyLa2fGYY+OBeLX6QN7+1Mo7ggBF+vZZc0MZ1KxFnBl/ZrhB3Ko0rsqijfmMptZZzNX2cxVNrN6MgfUgUxfaKvsYANNWJR/sMtFuXy5Mxq519hp25MPpHz3dSKuBzsxC9qj1B4FWEd8sEycj8UMoIBDioMOL+GZUWmUrjiOmEJMMlaZjszEFOuuyfasOEEfpDozO+4iP+8fWmcPr8weXpPLzEeuc5H5EW9ykf/9ibt5uladO145k9qPzZu7D8WcEx/x3FEBEzp/Ryw3ZbVZ1U3TVHVTf8b4A6r4RZA=",
+            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKhRTLFIHJWk3hPrvcwkbSGxSpLzY9T1OfeMr7MyAZ0ccoL4CtZY91B9X8NSxNnOOdY9Qg9NkYFJAvMcvqPNpqwA5UCCMiltwaXjod+ikQP0ofRBtdwzZDaHgbL2oLM9wIWVrBRepXwt8Tw7b+K2Y1Atzmcwsk5mrZOYqmVn8MjV3aHzmezSBuMuMbW1vA434R4N8kS+fLo6sdwYbYzvygVrffB5J4t6OQoL6oI1HBdaRdNeR87aYARSwj57Q/slBHlqD2mWHAaOTsUyquCEehWLdJcoekdjog25Pwp62k5zXvy2TJ5gnT7BKZaYj16nIdJ83qcj/nuN2nq5tT43sHcf0ff3m7L1i9klaPBZVwYjO3xDLTV5sVmVVVUVZle/T9A1770fZ",
             position = {0, 0}
         }
     ]]
@@ -125,7 +125,7 @@ simulations.factoriopedia_smelting_locomotive_tender =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk9FuwjAMRf/FzylaoS2sv4JQFYIpFmmCkrQbQvn3uYQJJDYpUl7sXh+nvvEN9nrEiyMToL0BKWs8tNsbeOqN1HPOyAGhBSdJQxRA5oDf0JZxJwBNoECYKu7BtTPjsEfHAvFb6QPX9qdQ3BECLtZzlTUznEnFWsCV9WuGH8ihSt+qKN6Yy2xmnc1cZTNX2czqyRxQBzJ9oa2ygw00YRGQR+j+aFAuyuXLxdHIvcZO2558IOW7rxNxPNiJgdAepfYowDri7jJxPhYzgAIOyRM6vDhoRqVRuuI4YnIyyVhlOjITU6y7prJnxDb6INWZ2XEX+bz/bZ09wTJ7gk0uMx+5zkXm+7zJRf73HHfzdK06d7x3JqUf6zdnH4rZJ27xXFQBEzp/Ryw3ZbVZ1U3TVHVTf8b4A5HcR+E=",
+            string = "0eJyNk9FuwjAMRf/FzylaoS2svzKhKhRTLFIHJWk3hPrvcwkbSGxSpL7Yvfck9a2vsDMDnh1xgPoK1Fr2UH9cwVPH2sw91j1CDU6TgUkB8R6/oM6nrQLkQIEwOm7FpeGh36ETgfpx+iDe7hiyG0LB2XpxWZ7hQsrWCi6iXwt8Tw7b+K6Y1Atzmcwsk5mrZOYqmVn8MjV3aHzmezSBuMuMbW1vA42YBZRJuj/OyRf58un+yHpnsDG2Ix+o9c3nkaTu7ShAqA/aeFRgHckldOS8LWYABexjNLR/CpKH1qB22WHAGGiUiYob4lEo1l2i7VFJmj7o9iTsaTvJ8/rRZfIg8+RBVqnMdOQ6FZke9yYV+d9fuZ2na9tTI+vHsX3fwrl7V8w5yRGPfVUwovM3xHKTF5tVWVVVUVbl+zR9A0wnSog=",
             position = {0, 0}
         }
     ]]
@@ -138,7 +138,7 @@ simulations.factoriopedia_smelting_wagon =
         game.simulation.camera_position = {1, 0.5}
         game.surfaces[1].create_entities_from_blueprint_string
         {
-            string = "0eJyNk91uwjAMhd/F1yla6Q+srzKhKhRTLFIHJSkMob77XMIEEpsUqTd2z/mS+tQ32JoRT444QHMD6ix7aL5u4KlnbeYe6wGhAafJwKSAeIff0OTTRgFyoEAYHffi2vI4bNGJQP06fRBvfwjZHaHgZL24LM9wIWUrBVfRrwS+I4ddfFdO6o25TGZWycwimVkkM8snc0ATiPvsonvLWf4HNl/ky5frIuutwdbYnnygzreXA0k92LNgoNlr41GBdSRn6sj5WMwACjjEJGj3khuPnUHtsv2IMb8oExW3xGehWHeNtmcl4fmgu6Owp80kz/s3Vslzy5PnVqcy05GrVGR6uutU5H8/4Waeru2OrWwbx/Zj6ebuQzHnJEc811PBGZ2/I5brvFwXVV3XZVVXn9P0AzSWQ2s=",
+            string = "0eJyNk91uwjAMhd/F1yla6Q+srzKhKhRTLFIHJSkMob77XMIGEpoUKTd2fT6nPvENtmbEkyMO0NyAOssemq8beOpZmznHekBowGkyMCkg3uE3NPm0UYAcKBBGxT24tjwOW3RSoH6VPoi2P4TsjlBwsl5Ulme4kLKVgqvUrwS+I4dd/FZO6o25TGZWycwimVkkM8s/puYejc/8gCYQ91mnXW+zi+5F+t4hX+TLl5sj663B1tiefKDOt5cDSTzYs6Cg2WvjUYF1JO115HwsZgAFHKIptHuxkMfOoHbZfsRoZSyTKm6Jz0Kx7hplz0h89EF3R2FPm0nO++9WySPMk0dYpzLTkatUZLrR61Tkf+9xM0/XdsdWFo9j+rF/c/ZRMfskLZ6bquCMzt8Ry3VerouqruuyqqvPafoBKKNH7Q==",
             position = {0, 0}
         }
     ]]
