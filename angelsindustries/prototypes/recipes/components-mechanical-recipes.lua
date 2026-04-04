28a7b53787d3aac9824ad8b029e597669b4dfb85
Jonas Özbay <jo.spam@gmx.net>
2026-04-04 22:22:34 +0200

    fix a few localisation links

--- angelsindustries/prototypes/recipes/components-mechanical-recipes.lua
+++ angelsindustries/prototypes/recipes/components-mechanical-recipes.lua
@@ -6,7 +6,7 @@ if angelsmods.industries.components then
     {
       type = "recipe",
       name = "angels-casing-iron-plate",
-      localised_name = { "item-name.motor-casing-1" },
+      localised_name = { "item-name.angels-motor-casing-1" },
       category = "crafting",
       subgroup = "angels-iron-casting",
       energy_required = 0.5,
@@ -148,7 +148,7 @@ if angelsmods.industries.components then
     {
       type = "recipe",
       name = "angels-casing-tungsten-1",
-      localised_name = { "item-name.motor-casing-5" },
+      localised_name = { "item-name.angels-motor-casing-5" },
       category = "angels-sintering-4",
       subgroup = "angels-tungsten-casting",
       enabled = false,
@@ -179,7 +179,7 @@ if angelsmods.industries.components then
     {
       type = "recipe",
       name = "angels-casing-tungsten-green",
-      localised_name = { "item-name.motor-casing-5-green" },
+      localised_name = { "item-name.angels-motor-casing-5-green" },
       category = "crafting",
       subgroup = "angels-tungsten-casting",
       enabled = false,
@@ -211,7 +211,7 @@ if angelsmods.industries.components then
     {
       type = "recipe",
       name = "angels-casing-tungsten-2",
-      localised_name = { "item-name.motor-casing-5" },
+      localised_name = { "item-name.angels-motor-casing-5" },
       category = "angels-sintering-5",
       subgroup = "angels-tungsten-casting",
       enabled = false,
