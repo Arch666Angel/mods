54c95dea2997e820f77b773796a6f9d7539fc050
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-06-26 19:22:11 +1200

    Module Categories Bug  #1311

--- angelsbioprocessing/prototypes/overrides/bio-processing-override-bio-productivity.lua
+++ angelsbioprocessing/prototypes/overrides/bio-processing-override-bio-productivity.lua
@@ -5,16 +5,48 @@ for _, category in pairs(data.raw["module-category"]) do
   end
 end
 
+local function add_category(entity, add_category_name)
+  local found = false
+  for _, category_name in pairs(entity.allowed_module_categories) do
+    if category_name == add_category_name then
+      found = true
+      break
+    end
+  end
+  if not found then
+    table.insert(entity.allowed_module_categories, add_category_name)
+  end
+end
+
+local function remove_category(entity, remove_category_name)
+  for i, category_name in pairs(entity.allowed_module_categories) do
+    if category_name == remove_category_name then
+      table.remove(entity.allowed_module_categories, i)
+      return
+    end
+  end
+end
+
 for _, entity_category in pairs({
   "assembling-machine",
+  "beacon",
   "furnace",
   "mining-drill",
   "lab",
   "rocket-silo",
 }) do
   for _, entity in pairs(data.raw[entity_category]) do
-    if not angelsmods.refining.productivity_exception[entity.name] and not entity.allowed_module_categories then
+    if entity.allowed_module_categories then
+      if angelsmods.refining.productivity_exception[entity.name] then
+        add_category(entity, "angels-bio-yield")
+      else
+        remove_category(entity, "angels-bio-yield")
+      end
+    else
       entity.allowed_module_categories = table.deepcopy(module_categories)
+      if angelsmods.refining.productivity_exception[entity.name] then
+        table.insert(entity.allowed_module_categories, "angels-bio-yield")
+      end
     end
   end
 end
