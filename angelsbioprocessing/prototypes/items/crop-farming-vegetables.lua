7c61ef6c3d15576fb4d97f4475b8972995594801
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2026-07-10 00:00:02 +1200

    Remove unused fields

--- angelsbioprocessing/prototypes/items/crop-farming-vegetables.lua
+++ angelsbioprocessing/prototypes/items/crop-farming-vegetables.lua
@@ -46,9 +46,6 @@ data:extend({
     subgroup = "science-pack",
     order = "c[token-bio]",
     stack_size = 200,
-    durability = 1,
-    durability_description_key = "description.science-pack-remaining-amount-key",
-    durability_description_value = "description.science-pack-remaining-amount-value",
   },
   -----------------------------------------------------------------------------
   -- UPGRADES -----------------------------------------------------------------
